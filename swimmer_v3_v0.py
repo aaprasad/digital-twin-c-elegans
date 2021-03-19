1c9da3edaae1166c9eca300706a531d2619c799f
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-03-19 16:32:55 +0800

    record video and don't render

--- swimmer_v3_v0.py
+++ swimmer_v3_v0.py
@@ -23,20 +23,20 @@ if __name__ == '__main__':
     - there's a relevant bug in gym==0.18.0, use 'pip install -e .' to install dev version instead
     - video will be named: based_path + '.mp4'
     """
-    # rec = VideoRecorder(env, base_path='swimmer_v3_v0', enabled=True)  # Create the video recorder
+    rec = VideoRecorder(env, base_path='swimmer_v3_v0', enabled=True)  # Create the video recorder
 
     """ run and record """
     observation = env.reset()
     # print(observation)
-    # rec.capture_frame()  # Capture the starting position
+    rec.capture_frame()  # Capture the starting position
     for i in range(1000):
-        env.render()  # show the current frame of visualization
+        # env.render()  # show the current frame of visualization
         action = env.action_space.sample()  # sample a random action
         observation, reward, done, info = env.step(action)
         # print(observation)
-        # rec.capture_frame()
+        rec.capture_frame()
         if done:
             print("Episode finished after {} steps".format(i + 1))
             break
-    # rec.close()  # Close the recording
+    rec.close()  # Close the recording
     env.close()
