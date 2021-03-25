7ec193e176740b4a690c31b9a4910247a9e59c06
zhongyuchen <34185896+zhongyuchen@users.noreply.github.com>
2021-03-25 10:23:33 +0800

    swimmer with n_links from dm_control

---
+++ swimmerdomain.py
@@ -0,0 +1,50 @@
+"""
+Swimmer from dm_control
+"""
+
+import os
+os.environ['DISABLE_MUJOCO_RENDERING'] = '1'
+os.environ['MUJOCO_GL'] = 'egl'  # glfw, egl, osmesa
+os.environ['EGL_DEVICE_ID'] = '0'  # GPU id for rendering
+
+import cv2
+import numpy as np
+from dm_control import suite
+
+
+def grabFrame(env):
+    # Get RGB rendering of env
+    rgbArr = env.physics.render(height=240, width=320, camera_id=0, overlays=(), depth=False, segmentation=False, scene_option=None, render_flag_overrides=None)  # camera_id=-1
+    # Convert to BGR for use with OpenCV
+    return cv2.cvtColor(rgbArr, cv2.COLOR_BGR2RGB)
+
+
+def benchmarking_task_set():
+    # Iterate over the whole task set: domain & task
+    for domain_name, task_name in suite.BENCHMARKING:
+        print(domain_name, task_name)
+        env = suite.load(domain_name, task_name)
+
+
+if __name__ == '__main__':
+    # Load one task:
+    # env = suite.load(domain_name='swimmer', task_name='swimmer6', visualize_reward=True)  # task: swimmer6, swimmer15
+    env = suite.swimmer.swimmer(n_links=3)  # n_links=3, time_limit=_DEFAULT_TIME_LIMIT, random=None, environment_kwargs=None
+
+    # Setup video writer - mp4 at 30 fps
+    frame = grabFrame(env)
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter('swimmerdomain.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30.0, (width, height))
+    video.write(frame)
+
+    # Step through an episode and print out reward, discount and observation.
+    action_spec = env.action_spec()
+    time_step = env.reset()
+    while not time_step.last():
+        action = np.random.uniform(action_spec.minimum, action_spec.maximum, size=action_spec.shape)
+        time_step = env.step(action)
+        # print(time_step.reward, time_step.discount, time_step.observation)
+        video.write(grabFrame(env))
+        
+    # End render to video file
+    video.release()
