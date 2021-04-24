f0a69d01e5058e2556a8d69d0417d5b9fd46286b
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-04-24 14:36:53 +0800

    wrap env with forage task

---
+++ src/envs/mujoco/forage.py
@@ -0,0 +1,95 @@
+""" swimmer: forage
+- wrap env with forage task
+"""
+
+from lxml import etree
+import random
+
+
+def _make_geom(mjcf, name, x, y, size, rgba):
+    """ make a sphere
+    Args:
+        x, y: position
+        size: radius
+        rgba: color and transparency, e.g. '0 1 0 1' for green, '1 0 0 1' for red
+    """
+    body = etree.Element('body', attrib={'name': name, 'pos': '{} {} {}'.format(x, y, size)})
+    geom = etree.Element('geom', attrib={'density': '0', 'size': '{}'.format(size), 'type': 'sphere', 'material': '', 'rgba': rgba})
+    body.append(geom)
+    worldbody = mjcf.find('worldbody')
+    worldbody.append(body)
+
+
+def make_perimeter(mjcf, name, width, box_width, box_height):
+    """ make a square perimeter wall
+    - the wall has mass, otherwise there is a bug because it can't compute center of mass
+    Args:
+        width: half-size of the square's width
+        box_width: half-size of the wall's width
+        box_height: half-size of the wall's height
+    """
+    body = etree.Element('body', attrib={'name': name, 'pos': '0 0 0'})
+    # 2 parallel sides
+    geom = etree.Element(
+        'geom',
+        attrib={
+            'density': '1000', 'type': 'box', 'material': '', 'rgba': '0.5 0.5 0.5 1',
+            'pos': '{} {} {}'.format(box_width, width, box_height),
+            'size': '{} {} {}'.format(width, box_width, box_height)
+        }
+    )
+    body.append(geom)
+    geom = etree.Element(
+        'geom',
+        attrib={
+            'density': '1000', 'type': 'box', 'material': '', 'rgba': '0.5 0.5 0.5 1',
+            'pos': '{} {} {}'.format(-box_width, -width, box_height),
+            'size': '{} {} {}'.format(width, box_width, box_height)
+        }
+    )
+    body.append(geom)
+    # the other 2 paralell sides
+    geom = etree.Element(
+        'geom',
+        attrib={
+            'density': '1000', 'type': 'box', 'material': '', 'rgba': '0.5 0.5 0.5 1',
+            'pos': '{} {} {}'.format(width, -box_width, box_height),
+            'size': '{} {} {}'.format(box_width, width, box_height)
+        }
+    )
+    body.append(geom)
+    geom = etree.Element(
+        'geom',
+        attrib={
+            'density': '1000', 'type': 'box', 'material': '', 'rgba': '0.5 0.5 0.5 1',
+            'pos': '{} {} {}'.format(-width, box_width, box_height),
+            'size': '{} {} {}'.format(box_width, width, box_height)
+        }
+    )
+    body.append(geom)
+    # add to worldbody
+    worldbody = mjcf.find('worldbody')
+    worldbody.append(body)
+
+
+def _make_model(xml_str, perimeter_width):
+    """ make sphere and perimeter wall
+    Args:
+        perimeter_width: half-size of square perimeter wall's width
+    """
+    # create mjcf
+    mjcf = etree.fromstring(xml_str, parser=etree.XMLParser(remove_blank_text=True))
+    # add green sphere as food, red sphere as trap
+    for i in range(5):
+        for rgba, name in zip(['0 1 0 1', '1 0 0 1'], ['food{}'.format(i + 1), 'trap{}'.format(i + 1)]):
+            x = random.randint(-perimeter_width, perimeter_width)
+            y = random.randint(-perimeter_width, perimeter_width)
+            _make_geom(mjcf=mjcf, name=name, x=x, y=y, size=0.5, rgba=rgba)
+    # make perimeter wall
+    make_perimeter(mjcf=mjcf, name='perimeter', width=perimeter_width, box_width=0.5, box_height=0.5)
+    return mjcf
+
+
+def forage(xml_str, perimeter_width):
+    mjcf = _make_model(xml_str=xml_str, perimeter_width=perimeter_width)
+    return etree.tostring(mjcf, pretty_print=True)
---
+++ swimmer_forage.py
@@ -0,0 +1,43 @@
+""" swimmer: forage
+- wrap env with forage task
+"""
+
+from src.envs.mujoco.swimmer_gym_v3_v2 import swimmer
+from src.envs.mujoco.forage import forage
+import gym
+import os
+
+
+def make_swimmer(n_bodies, joint_range, body_len, perimeter_width, camera_pos):
+    """ create swimmer env """
+    # generate xml str
+    xml_folder = 'src/envs/mujoco/assets/'
+    xml_str = swimmer(n_bodies=n_bodies, joint_range=joint_range, body_len=body_len, xml_file=os.path.join(xml_folder, 'swimmer.xml'), camera_pos=camera_pos)
+    xml_str = forage(xml_str=xml_str, perimeter_width=perimeter_width)
+    # write temp xml file, make env and delete temp file
+    xml_file = os.path.join(xml_folder, 'swimmer_temp.xml')
+    with open(xml_file, 'wb') as f:
+        f.write(xml_str)
+    env = gym.make('Swimmer-v3', xml_file=os.path.join(os.getcwd(), xml_file))
+    if os.path.exists(xml_file):
+        os.remove(xml_file)
+    return env
+
+
+def test_random():
+    """ take random actions """
+    # swimmer multibody model: set up n_bodies, joint_range and body_len
+    env = make_swimmer(n_bodies=12, joint_range='-30 30', body_len=0.26, perimeter_width=6, camera_pos='0 -6 6')
+    observation = env.reset()
+    for i in range(1000):
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} steps".format(i + 1))
+            break
+    env.close()
+
+
+if __name__ == '__main__':
+    test_random()
