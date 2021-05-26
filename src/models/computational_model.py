5375bb94ee5d191f57edc4a95f5218fee6956570
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-05-26 17:06:40 +0800

    sharp turn

--- src/models/computational_model.py
+++ src/models/computational_model.py
@@ -20,21 +20,40 @@ class SinusoidalMotion(object):
         """
         self.phi = -2 * np.pi * self.psi * np.arange(0, self.n - 1)
         self.step_b0 = None  # start of backward movement
-        """ duration """
+        self.step_omega0 = None  # start of sharp turn
+        """ backward """
         self.step_b = 100  # backward movement
+        """ sharp turn """
+        self.step_omega1 = 100  # sharp turn phase 1
+        self.step_omega2 = 100  # sharp turn phase 2
+        self.step_omega3 = self.step_omega1  # sharp turn phase 3
+        self.c_omega = 50  # phase delay for changing posture from S-shaped to omega-shaped and back
 
     def _backward(self, step):
-        """ phase for backward movement
-        Args:
-            step_0: if not None, move in the other direction
-        move in the other direction
+        """ update phase for backward movement
+        apply once at the beginning to move in the other direction
             phi = pi + 2 * omega * t_0 - phi, where t_0 is initiation time
+        apply it again at the end to switch back to original direction
         """
-        if self.step_b0 is not None and self.step_b0 <= step < self.step_b0 + self.step_b:
-            phi = np.pi + 2 * self.omega * self.step_b0 * self.dt - self.phi
-        else:
-            phi = self.phi
-        return phi
+        if self.step_b0 is not None and (step == self.step_b0 or step == self.step_b0 + self.step_b):
+            self.phi = np.pi + 2 * self.omega * self.step_b0 * self.dt - self.phi
+
+    def _sharp_turn(self, step):
+        """ update phase for sharp turn
+        it changes posture but still needs bias angle param to perform a sharp turn
+        phase 1: phi(t) = phi(t - dt) + c_omega / t_omega1 * dt, where t_omega1 is duration
+        phase 2: phi(t) = phi(t), where t_omega2 is duration
+        phase 3: phi(t) = phi(t - dt) - c_omega / t_omega3 * dt, where t_omega3 is duration
+        """
+        if self.step_omega0 is not None:
+            phase0 = self.step_omega0
+            phase1 = phase0 + self.step_omega1
+            phase2 = phase1 + self.step_omega2
+            phase3 = phase2 + self.step_omega3
+            if phase0 <= step < phase1:
+                self.phi += self.c_omega / self.step_omega1
+            elif phase2 <= step < phase3:
+                self.phi -= self.c_omega / self.step_omega3
 
     def _joint_angle(self, step):
         """ calculate joint angles
@@ -42,8 +61,9 @@ class SinusoidalMotion(object):
             backward: q = q_max * sin(omega * t - phi)
             forward: q = q_max * sin(omega * t - (pi - phi))
         """
-        phi = self._backward(step=step)
-        q = self.q_max * np.sin(self.omega * step * self.dt - (np.pi - phi))
+        self._backward(step=step)
+        self._sharp_turn(step=step)
+        q = self.q_max * np.sin(self.omega * step * self.dt - (np.pi - self.phi))
         return q
 
     def _action(self, q, q_next, q_vel):
@@ -55,7 +75,8 @@ class SinusoidalMotion(object):
         return action
 
     def step(self, step, q, q_vel):
-        self.step_b0 = 500
+        self.step_b0 = 200
+        self.step_omega0 = 500
         q_next = self._joint_angle(step=step)
         action = self._action(q=q, q_next=q_next, q_vel=q_vel)
         return action
