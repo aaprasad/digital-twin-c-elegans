7b80e63029c8b8fb48d4cb49a441a031c9d4c94d
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-05-27 14:09:30 +0800

    get center of mass, add tangential gradient

--- src/wrappers/distribution.py
+++ src/wrappers/distribution.py
@@ -5,8 +5,11 @@ class Distribution(gym.Wrapper):
     """ environmental substance distribution wrapper """
     def __init__(self, env, f, source):
         super(Distribution, self).__init__(env)
+        self.dt = 0.01  # time step
         self.f = f  # concentration function
         self.source = source  # source position
+        """ state """
+        self._com_c_past = None  # last step's concentration at the center of mass
 
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
@@ -16,14 +19,26 @@ class Distribution(gym.Wrapper):
         reward, info = self.reward(reward, info)
         return observation, reward, done, info
 
+    def _tangential_gradient(self, com):
+        """ gradient at the center of mass """
+        com_c = self.f(com, source=self.source)
+        if self._com_c_past is None:
+            gradient = 0.
+        else:
+            gradient = (com_c - self._com_c_past) / self.dt
+        self._com_c_past = com_c
+        return gradient
+
     def reward(self, reward, info):
-        position = self.env.sim.data.qpos[0:2].copy()
-        com = self.env.sim.data.subtree_com[1].copy()
-        concentration = self.f(position, source=self.source)
+        position = self.env.sim.data.qpos[0:2].copy()  # position of anterior tip
+        com = self.env.sim.data.subtree_com[1][0:2].copy()  # center of mass
+        concentration = self.f(position, source=self.source)  # position's concentration
+        tangential_gradient = self._tangential_gradient(com=com)
         # reward
         reward = concentration
         # info
         info['position'] = position.tolist()
         info['com'] = com.tolist()
         info['concentration'] = concentration
+        info['tangential_gradient'] = tangential_gradient
         return reward, info
