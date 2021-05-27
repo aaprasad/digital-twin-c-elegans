2c61c32fb38ad418aba261b7531675ee9ae1e7e6
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-05-27 14:58:21 +0800

    normal gradient

--- src/wrappers/distribution.py
+++ src/wrappers/distribution.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 
 class Distribution(gym.Wrapper):
@@ -6,10 +7,12 @@ class Distribution(gym.Wrapper):
     def __init__(self, env, f, source):
         super(Distribution, self).__init__(env)
         self.dt = 0.01  # time step
+        self.delta = 0.1  # small displacement
         self.f = f  # concentration function
         self.source = source  # source position
         """ state """
         self._com_c_past = None  # last step's concentration at the center of mass
+        self._com_past = None  # last step's center of mass
 
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
@@ -20,7 +23,7 @@ class Distribution(gym.Wrapper):
         return observation, reward, done, info
 
     def _tangential_gradient(self, com):
-        """ gradient at the center of mass """
+        """ gradient parallel to the traveling direction of the center of mass """
         com_c = self.f(com, source=self.source)
         if self._com_c_past is None:
             gradient = 0.
@@ -29,11 +32,30 @@ class Distribution(gym.Wrapper):
         self._com_c_past = com_c
         return gradient
 
+    def _normal_gradient(self, com):
+        """ gradient perpendicular to the traveling direction of the center of mass """
+        if self._com_past is None:
+            gradient = 0.
+        else:
+            # tangential direction's vector
+            normal_vector = com - self._com_past
+            # normal direction's unit vector
+            normal_vector = np.matmul(np.array([[0, 1], [-1, 0]]), normal_vector) / np.linalg.norm(normal_vector)
+            # normal vector for displacement
+            normal_vector *= self.delta
+            # gradient
+            c1 = self.f(com + normal_vector, source=self.source)
+            c2 = self.f(com - normal_vector, source=self.source)
+            gradient = (c2 - c1) / (2 * self.delta)
+        self._com_past = com
+        return gradient
+
     def reward(self, reward, info):
         position = self.env.sim.data.qpos[0:2].copy()  # position of anterior tip
         com = self.env.sim.data.subtree_com[1][0:2].copy()  # center of mass
         concentration = self.f(position, source=self.source)  # position's concentration
         tangential_gradient = self._tangential_gradient(com=com)
+        normal_gradient = self._normal_gradient(com=com)
         # reward
         reward = concentration
         # info
@@ -41,4 +63,5 @@ class Distribution(gym.Wrapper):
         info['com'] = com.tolist()
         info['concentration'] = concentration
         info['tangential_gradient'] = tangential_gradient
+        info['normal_gradient'] = normal_gradient
         return reward, info
