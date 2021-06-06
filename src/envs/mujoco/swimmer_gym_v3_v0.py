8e9acb236fc0c655bee8c73f61fc910b40ecab4c
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-06 17:53:16 +0800

    simplify swimmerenv

--- src/envs/mujoco/swimmer_gym_v3_v0.py
+++ src/envs/mujoco/swimmer_gym_v3_v0.py
@@ -1,24 +1,22 @@
-""" A Copy of Swimmer-v3 from OpenAI Gym
-- SwimmerEnv is exactly the same as Swimmer-v3, with personal xml_file path configuration.
-- swimmer.xml is unedited.
-- Reference: https://github.com/openai/gym/blob/master/gym/envs/mujoco/swimmer_v3.py
+""" OpenAI Gym's Swimmer-v3
+SwimmerEnv works the same as Swimmer-v3
+    - different default xml_file
+    - https://github.com/openai/gym/blob/master/gym/envs/mujoco/swimmer_v3.py
 """
 
-import numpy as np
 import os
-from gym.envs.mujoco import mujoco_env
+from gym.envs.mujoco import mujoco_env, swimmer_v3
 from gym import utils
 
-DEFAULT_CAMERA_CONFIG = {}
 
-
-class SwimmerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+class SwimmerEnv(swimmer_v3.SwimmerEnv):
     def __init__(self,
                  xml_file=os.path.join(os.path.dirname(__file__), 'assets', 'swimmer.xml'),
                  forward_reward_weight=1.0,
                  ctrl_cost_weight=1e-4,
                  reset_noise_scale=0.1,
                  exclude_current_positions_from_observation=True):
+        """ override gym.envs.mujoco.swimmer_v3.SwimmerEnv """
         utils.EzPickle.__init__(**locals())
 
         self._forward_reward_weight = forward_reward_weight
@@ -30,68 +28,3 @@ class SwimmerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             exclude_current_positions_from_observation)
 
         mujoco_env.MujocoEnv.__init__(self, xml_file, 4)
-
-    def control_cost(self, action):
-        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
-        return control_cost
-
-    def step(self, action):
-        xy_position_before = self.sim.data.qpos[0:2].copy()
-        self.do_simulation(action, self.frame_skip)
-        xy_position_after = self.sim.data.qpos[0:2].copy()
-
-        xy_velocity = (xy_position_after - xy_position_before) / self.dt
-        x_velocity, y_velocity = xy_velocity
-
-        forward_reward = self._forward_reward_weight * x_velocity
-
-        ctrl_cost = self.control_cost(action)
-
-        observation = self._get_obs()
-        reward = forward_reward - ctrl_cost
-        done = False
-        info = {
-            'reward_fwd': forward_reward,
-            'reward_ctrl': -ctrl_cost,
-
-            'x_position': xy_position_after[0],
-            'y_position': xy_position_after[1],
-            'distance_from_origin': np.linalg.norm(xy_position_after, ord=2),
-
-            'x_velocity': x_velocity,
-            'y_velocity': y_velocity,
-            'forward_reward': forward_reward,
-        }
-
-        return observation, reward, done, info
-
-    def _get_obs(self):
-        position = self.sim.data.qpos.flat.copy()
-        velocity = self.sim.data.qvel.flat.copy()
-
-        if self._exclude_current_positions_from_observation:
-            position = position[2:]
-
-        observation = np.concatenate([position, velocity]).ravel()
-        return observation
-
-    def reset_model(self):
-        noise_low = -self._reset_noise_scale
-        noise_high = self._reset_noise_scale
-
-        qpos = self.init_qpos + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nq)
-        qvel = self.init_qvel + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nv)
-
-        self.set_state(qpos, qvel)
-
-        observation = self._get_obs()
-        return observation
-
-    def viewer_setup(self):
-        for key, value in DEFAULT_CAMERA_CONFIG.items():
-            if isinstance(value, np.ndarray):
-                getattr(self.viewer.cam, key)[:] = value
-            else:
-                setattr(self.viewer.cam, key, value)
