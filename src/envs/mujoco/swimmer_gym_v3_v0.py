008d18708f9cf9021ece4b1fd9f2ebe1b352cd96
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-06 20:04:04 +0800

    able to load model from xml_str

--- src/envs/mujoco/swimmer_gym_v3_v0.py
+++ src/envs/mujoco/swimmer_gym_v3_v0.py
@@ -1,5 +1,6 @@
 """ OpenAI Gym's Swimmer-v3
 SwimmerEnv works the same as Swimmer-v3
+    - able to load model from xml_str
     - different default xml_file
     - https://github.com/openai/gym/blob/master/gym/envs/mujoco/swimmer_v3.py
 """
@@ -7,16 +8,58 @@ SwimmerEnv works the same as Swimmer-v3
 import os
 from gym.envs.mujoco import mujoco_env, swimmer_v3
 from gym import utils
+import mujoco_py
+import numpy as np
+
+DEFAULT_CAMERA_CONFIG = {}
 
 
-class SwimmerEnv(swimmer_v3.SwimmerEnv):
+class MujocoEnv(mujoco_env.MujocoEnv):
+    def __init__(self, model_xml=None, model_path=None, frame_skip=4):
+        """ override mujoco_env.MujocoEnv """
+        self.frame_skip = frame_skip
+        if model_xml is not None:
+            self.model = mujoco_py.load_model_from_xml(model_xml)
+        else:
+            if model_path.startswith("/"):
+                fullpath = model_path
+            else:
+                fullpath = os.path.join(os.path.dirname(__file__), "assets", model_path)
+            if not os.path.exists(fullpath):
+                raise IOError("File %s does not exist" % fullpath)
+            self.model = mujoco_py.load_model_from_path(fullpath)
+        self.sim = mujoco_py.MjSim(self.model)
+        self.data = self.sim.data
+        self.viewer = None
+        self._viewers = {}
+
+        self.metadata = {
+            'render.modes': ['human', 'rgb_array', 'depth_array'],
+            'video.frames_per_second': int(np.round(1.0 / self.dt))
+        }
+
+        self.init_qpos = self.sim.data.qpos.ravel().copy()
+        self.init_qvel = self.sim.data.qvel.ravel().copy()
+
+        self._set_action_space()
+
+        action = self.action_space.sample()
+        observation, _reward, done, _info = self.step(action)
+        assert not done
+
+        self._set_observation_space(observation)
+
+        self.seed()
+
+
+class SwimmerEnv(MujocoEnv, utils.EzPickle):
     def __init__(self,
+                 xml_str=None,
                  xml_file=os.path.join(os.path.dirname(__file__), 'assets', 'swimmer.xml'),
                  forward_reward_weight=1.0,
                  ctrl_cost_weight=1e-4,
                  reset_noise_scale=0.1,
                  exclude_current_positions_from_observation=True):
-        """ override swimmer_v3.SwimmerEnv """
         utils.EzPickle.__init__(**locals())
 
         self._forward_reward_weight = forward_reward_weight
@@ -27,4 +70,69 @@ class SwimmerEnv(swimmer_v3.SwimmerEnv):
         self._exclude_current_positions_from_observation = (
             exclude_current_positions_from_observation)
 
-        mujoco_env.MujocoEnv.__init__(self, xml_file, 4)
+        MujocoEnv.__init__(self, model_xml=xml_str, model_path=xml_file, frame_skip=4)
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
+
+    def step(self, action):
+        xy_position_before = self.sim.data.qpos[0:2].copy()
+        self.do_simulation(action, self.frame_skip)
+        xy_position_after = self.sim.data.qpos[0:2].copy()
+
+        xy_velocity = (xy_position_after - xy_position_before) / self.dt
+        x_velocity, y_velocity = xy_velocity
+
+        forward_reward = self._forward_reward_weight * x_velocity
+
+        ctrl_cost = self.control_cost(action)
+
+        observation = self._get_obs()
+        reward = forward_reward - ctrl_cost
+        done = False
+        info = {
+            'reward_fwd': forward_reward,
+            'reward_ctrl': -ctrl_cost,
+
+            'x_position': xy_position_after[0],
+            'y_position': xy_position_after[1],
+            'distance_from_origin': np.linalg.norm(xy_position_after, ord=2),
+
+            'x_velocity': x_velocity,
+            'y_velocity': y_velocity,
+            'forward_reward': forward_reward,
+        }
+
+        return observation, reward, done, info
+
+    def _get_obs(self):
+        position = self.sim.data.qpos.flat.copy()
+        velocity = self.sim.data.qvel.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        observation = np.concatenate([position, velocity]).ravel()
+        return observation
+
+    def reset_model(self):
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nv)
+
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+        return observation
+
+    def viewer_setup(self):
+        for key, value in DEFAULT_CAMERA_CONFIG.items():
+            if isinstance(value, np.ndarray):
+                getattr(self.viewer.cam, key)[:] = value
+            else:
+                setattr(self.viewer.cam, key, value)
