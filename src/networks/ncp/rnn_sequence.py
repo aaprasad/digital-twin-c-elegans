4e20a8498f8a4825c6d8aa8186c7fb9ceb4fd15a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-15 19:12:58 +0800

    neural circuit policies (NCPs) code

---
+++ src/networks/ncp/rnn_sequence.py
@@ -0,0 +1,31 @@
+""" RNN sequence: unfolds a RNN cell into a sequence
+https://colab.research.google.com/drive/1VWoGcpyqGvrUOUzH7ccppE__m-n1cAiI?usp=sharing
+"""
+
+import torch
+import torch.nn as nn
+
+
+# nn.Module that unfolds a RNN cell into a sequence
+class RNNSequence(nn.Module):
+    def __init__(
+        self,
+        rnn_cell,
+    ):
+        super(RNNSequence, self).__init__()
+        self.rnn_cell = rnn_cell
+
+    def forward(self, x):
+        device = x.device
+        batch_size = x.size(0)
+        seq_len = x.size(1)
+        hidden_state = torch.zeros(
+            (batch_size, self.rnn_cell.state_size), device=device
+        )
+        outputs = []
+        for t in range(seq_len):
+            inputs = x[:, t]
+            new_output, hidden_state = self.rnn_cell.forward(inputs, hidden_state)
+            outputs.append(new_output)
+        outputs = torch.stack(outputs, dim=1)  # return entire sequence
+        return outputs
