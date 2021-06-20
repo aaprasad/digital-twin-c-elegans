9ac3796c1467e2826a4a25b2b13331e867bef5c0
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-20 15:24:04 +0800

    create subset of TensorDataset

---
+++ src/data/subset.py
@@ -0,0 +1,11 @@
+import torch
+
+
+class SubsetDataset(torch.utils.data.TensorDataset):
+    def __init__(self, dataset, data_size, seed):
+        """ create a random subset of TensorDataset """
+        x, y = dataset.tensors
+        data_size = min(data_size, len(dataset))
+        indices = torch.randperm(len(dataset), generator=torch.Generator().manual_seed(seed))
+        indices = indices[:data_size]
+        super(SubsetDataset, self).__init__(x[indices], y[indices])
--- swimmer_chemotaxis_ncp_data.py
+++ swimmer_chemotaxis_ncp_data.py
@@ -3,15 +3,17 @@ preprocess chemotaxis dataset for NCP network training
 """
 
 from src.data.ncp import NCPDataset
+from src.data.subset import SubsetDataset
 import torch
 
 
-def preprocess_dataset():
+def preprocess_dataset(data_size=60000, seed=42):
     dataset = torch.load('data/chemotaxis.pt')
+    dataset = SubsetDataset(dataset, data_size=data_size, seed=seed)
     dataset = NCPDataset(dataset)
     print('dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
     torch.save(dataset, 'data/ncp.pt')
 
 
 if __name__ == '__main__':
-    preprocess_dataset()
+    preprocess_dataset(data_size=60000)
