abc19da3aad2b6d3442c798d795e7d06c36b1ddb
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-25 15:49:48 +0800

    split each sequence into subsequences

--- swimmer_chemotaxis_ncp_data.py
+++ swimmer_chemotaxis_ncp_data.py
@@ -2,15 +2,18 @@
 preprocess concatenated chemotaxis dataset for NCP network training
 """
 
+from src.data.subsequence import SubsequenceDataset
 from src.data.encode import EncodeDataset
 from src.data.subset import FilterSubset
 import torch
 from tqdm import tqdm
 
 
-def preprocess_dataset(data_size=12000):
+def preprocess_dataset(data_size=600, seq_len=16):
     """ preprocess chemotaxis dataset with different chemical source positions
-    data_size: the total dataset size, should be divided for each env (with different source position)
+    data_size: total amount of original chemotaxis sequences (divided equally between different envs)
+    seq_len: split each chemotaxis sequence into subsequences with seq_len
+    return: x size [93600, 16, 2], y size [93600, 16, 11]
     """
     concat_dataset = torch.load('data/concat_chemotaxis.pt')
     data_size = data_size // len(concat_dataset.datasets)
@@ -18,6 +21,7 @@ def preprocess_dataset(data_size=12000):
     for dataset in tqdm(concat_dataset.datasets):
         dataset = FilterSubset(dataset, data_size=data_size)
         dataset = EncodeDataset(dataset)
+        dataset = SubsequenceDataset(dataset, seq_len=seq_len)
         datasets.append(dataset)
     concat_dataset = torch.utils.data.ConcatDataset(datasets)
     print('dataset', len(concat_dataset), concat_dataset[0][0].size(), concat_dataset[0][1].size())
@@ -25,4 +29,4 @@ def preprocess_dataset(data_size=12000):
 
 
 if __name__ == '__main__':
-    preprocess_dataset(data_size=12000)
+    preprocess_dataset(data_size=600, seq_len=16)
