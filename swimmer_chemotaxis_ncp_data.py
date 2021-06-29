e57b2c97b98bce7a474caff274eac257ea36db2c
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-06-29 10:40:48 +0800

    mean chemotaxis index before and after filtering

--- swimmer_chemotaxis_ncp_data.py
+++ swimmer_chemotaxis_ncp_data.py
@@ -2,6 +2,7 @@
 preprocess concatenated chemotaxis dataset for NCP network training
 """
 
+import numpy as np
 from src.data.concat import ConcatDataset
 from src.data.ncp import NCPDataset
 from src.data.subset import FilterSubset
@@ -14,12 +15,16 @@ def preprocess_dataset(data_size=600, seq_len=16):
     seq_len: split each chemotaxis sequence into subsequences with seq_len
     return: x size [93600, 16, 2], y size [93600, 16, 11]
     """
-    # load, filter and concat dataset
+    # load dataset
     concat_dataset = torch.load('data/concat_chemotaxis.pt')
     print('loaded dataset', len(concat_dataset), concat_dataset[0][0].size(), concat_dataset[0][1].size())
+    print('chemotaxis index mean', np.mean([torch.mean(dataset.tensors[0]).item() for dataset in concat_dataset.datasets]))
+    # filter and concat dataset
     data_size = data_size // len(concat_dataset.datasets)
     datasets = [FilterSubset(dataset, data_size) for dataset in concat_dataset.datasets]
     dataset = ConcatDataset(datasets)
+    print('filtered dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
+    print('chemotaxis index mean', torch.mean(dataset.tensors[0]).item())
     # preprocess dataset
     dataset = NCPDataset(dataset, seq_len=seq_len)
     print('processed dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
