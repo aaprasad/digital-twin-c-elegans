525545584b9edb503508d434a8ce01c17499c8d8
zhongyuchen <34185896+zhongyuchen@users.noreply.github.com>
2021-07-11 16:01:53 +0800

    train fully_connected with smaller lr

--- swimmer_chemotaxis_ncp.py
+++ swimmer_chemotaxis_ncp.py
@@ -164,4 +164,4 @@ def offline_train_and_test(
 
 
 if __name__ == '__main__':
-    offline_train_and_test(cuda=1, batch_size=1024, epochs=100, data_name='computational_model_ncp.pt', model_name='ncp')
+    offline_train_and_test(cuda=1, batch_size=2048, epochs=100, data_name='computational_model_ncp.pt', model_name='fully_connected')
