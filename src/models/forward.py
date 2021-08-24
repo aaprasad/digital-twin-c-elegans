f0efe775813613bf3c62f89ff136f68eb5a8afa2
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-08-24 15:43:01 +0800

    swimmer forward movement simulation

---
+++ src/models/forward.py
@@ -0,0 +1,48 @@
+""" forward sinusoidal movement
+https://doi.org/10.1038/s41598-018-35157-1
+"""
+
+import numpy as np
+
+
+class Forward(object):
+    """ generate action to perform forward sinusoidal movement """
+    def __init__(self, dt, seed=None):
+        self.dt = dt  # real time per step
+        self.n = 12  # number of bodies
+        self.q_max = 40 * np.pi / 180  # max joint angle (rad)
+        """ affect sinusoidal posture and speed """
+        self.a_max = 2.  # action: [-a_max, a_max]
+        self.psi = 0.2  # body wavelength (rad)
+        self.omega = 2 * np.pi * 3  # angular velocity of bending (rad/s): 2 * pi * freq
+        """ state """
+        self.phi = -2 * np.pi * self.psi * np.arange(0, self.n - 1)
+        """ seeding """
+        self.seed(seed)
+
+    @staticmethod
+    def seed(seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+
+    def _joint_angle(self, step):
+        """ calculate joint angles
+        forward:
+            q = q_max * sin(omega * t - (pi - phi))
+            phi = -2 * pi * psi * (i - 1), where `i` is the body index (1~12)
+        """
+        q = self.q_max * np.sin(self.omega * step * self.dt - (np.pi - self.phi))
+        return q
+
+    def _action(self, q, q_next, q_vel):
+        """ delta_q: [-4 * q_max, 4 * q_max] -> action: [-a_max, a_max]
+        - consider (q_vel_next - q_vel) for action
+        """
+        delta_q = q_next - q - q_vel * self.dt
+        action = delta_q / (4 * self.q_max) * self.a_max
+        return action
+
+    def step(self, step, q, q_vel):
+        q_next = self._joint_angle(step=step)
+        action = self._action(q=q, q_next=q_next, q_vel=q_vel)
+        return action
---
+++ swimmer_forward.py
@@ -0,0 +1,43 @@
+""" swimmer: forward movement """
+
+import gym
+import os
+from src.envs.mujoco.swimmer_gym_v3_v0 import SwimmerEnv
+from src.envs.mujoco.swimmer_gym_v3_v2 import swimmer
+from src.models.forward import Forward
+from src.wrappers.recorder import Recorder
+
+
+def make_swimmer(
+    n_bodies=12, joint_range='-100 100', body_len=0.25, camera_pos='0 -6 6', camera_z=None, camera_name=None,
+    max_episode_steps=1000, video_name='swimmer_forward'
+):
+    """ create swimmer env """
+    xml_str = swimmer(n_bodies, joint_range, body_len, 'src/envs/mujoco/assets/swimmer.xml', camera_pos, camera_z)
+    env = SwimmerEnv(xml_str=xml_str.decode('utf-8'))
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = Recorder(env, stats_name=[], camera_name=camera_name)
+    if camera_name is not None:
+        env = gym.wrappers.Monitor(env, directory=os.path.join('video', video_name), force=True)
+    return env
+
+
+def test_forward(seed=None):
+    """ forward sinusoidal movement """
+    env = make_swimmer(max_episode_steps=2500, camera_z=50, camera_name=None)
+    env.seed(seed)
+    observation = env.reset()
+    model = Forward(dt=env.dt, seed=seed)
+    for i in range(10 ** 6):
+        env.render()
+        action = model.step(step=i, q=observation[1:12], q_vel=observation[15:])
+        observation, reward, done, info = env.step(action)
+        if done:
+            print('Episode finished after {} steps'.format(i + 1))
+            break
+    env.close()
+
+
+if __name__ == '__main__':
+    test_forward()
