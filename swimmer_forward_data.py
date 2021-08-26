f903002b42e2676b30ffd1256bd0482b2cf00f83
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-08-27 00:11:05 +0800

    try sine/square wave as dataset input

--- swimmer_forward_data.py
+++ swimmer_forward_data.py
@@ -54,4 +54,5 @@ def generate_dataset(data_size=100, seed=42, max_episode_steps=2500, mode='sine_
 
 
 if __name__ == '__main__':
-    generate_dataset(data_size=100, max_episode_steps=2500, mode='square_wave', save_name='forward.pt')
+    generate_dataset(data_size=100, max_episode_steps=2500, mode='sine_wave', save_name='forward.pt')
+    # generate_dataset(data_size=100, max_episode_steps=2500, mode='square_wave', save_name='forward.pt')
