f5764dace1e40ab4b7d4e70e3b9bed93f39b67cb
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-08-30 15:52:43 +0800

    online test with no reset noise

--- swimmer_forward_ncp_online.py
+++ swimmer_forward_ncp_online.py
@@ -45,7 +45,7 @@ def online_test(
     seed=42, max_episode_steps=2500, model_folder=None, model_name='fully_connected', data_size=100, mode='sine_wave',
     **kwargs
 ):
-    """ online test for at least 100 trials """
+    """ online test for at least 100 trials (if there's no randomness, 1 trial is enough) """
     np.random.seed(seed)
     torch.manual_seed(seed)
     assert model_folder is not None, 'model_folder can not be {}'.format(model_folder)
@@ -86,7 +86,7 @@ def online_test_video(
         record_kwargs = {'camera_name': 'track', 'video_name': model_folder}
     else:
         record_kwargs = {}
-    env = make_swimmer(max_episode_steps=max_episode_steps, **record_kwargs)
+    env = make_swimmer(max_episode_steps=max_episode_steps, reset_noise_scale=0., **record_kwargs)
     model = prepare_model(model_name, model_path=os.path.join('runs', model_folder, 'model.pt'), **kwargs)
     math_model = Forward(dt=env.dt, seed=seed)
     x, _ = online_test_single_simulation(env, model, math_model, mode)
@@ -99,5 +99,5 @@ if __name__ == '__main__':
     model_name = 'fully_connected'
     mode = 'sine_wave'
     kwargs = {'units': 30, 'output_dim': 11, 'in_features': 1}
-    online_test(data_size=100, model_folder=model_folder, model_name=model_name, mode=mode, **kwargs)
+    # online_test(data_size=100, model_folder=model_folder, model_name=model_name, mode=mode, **kwargs)
     online_test_video(model_folder=model_folder, model_name=model_name, mode=mode, record=True, **kwargs)
