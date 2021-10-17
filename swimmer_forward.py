71a94f4f6216bd82f9d252198555564dafa872cc
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-17 14:39:22 +0800

    fix import

--- swimmer_forward.py
+++ swimmer_forward.py
@@ -1,13 +1,12 @@
 """ swimmer: forward locomotion """
 
 import gym
+from gym_worm.envs.mujoco.swimmer_v3_v2 import swimmer
+from gym_worm.wrappers.position import Position
+from gym_worm.wrappers.recorder import Recorder
 import numpy as np
 import os
-from src.envs.mujoco.swimmer_gym_v3_v0 import SwimmerEnv
-from src.envs.mujoco.swimmer_gym_v3_v2 import swimmer
 from src.models.forward import Forward
-from src.wrappers.position import Position
-from src.wrappers.recorder import Recorder
 
 
 def make_swimmer(
@@ -15,9 +14,9 @@ def make_swimmer(
     max_episode_steps=1000, video_name='swimmer_forward', reset_noise_scale=0.1
 ):
     """ create swimmer env """
-    xml_str = swimmer(n_bodies, joint_range, body_len, 'src/envs/mujoco/assets/swimmer.xml', camera_pos, camera_z)
-    env = SwimmerEnv(xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
-    env = gym.wrappers.TimeLimit(env, max_episode_steps=max_episode_steps)
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len, camera_pos, camera_z)
+    env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
     env = Position(env)
     env = Recorder(env, stats_name=['com'], camera_name=camera_name)
@@ -33,7 +32,7 @@ def test_forward(seed=None, max_episode_steps=2500):
     observation = env.reset()
     model = Forward(dt=env.dt, seed=seed)
     for i in range(10 ** 6):
-        env.render()
+        # env.render()
         action = model.step(step=i, q=observation[1:12], q_vel=observation[15:])
         observation, reward, done, info = env.step(action)
         if done:
