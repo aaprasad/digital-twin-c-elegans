53647653fc5905f47b2a822126492ee09fcb0752
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-19 11:25:06 +0800

    add subset_and_split

--- examples/swimmer_forward/ncp_data.py
+++ examples/swimmer_forward/ncp_data.py
@@ -2,24 +2,8 @@
 preprocess forward simulation dataset for NCP network training
 """
 
-import os
-from virtual_nematode.data.split import SplitDataset
-from virtual_nematode.data.subset import RandomSubset
-import torch
-
-
-def preprocess_dataset(data_size=50, seed=42, seq_len=16, load_name='source.pt', save_name='target.pt'):
-    # load dataset
-    dataset = torch.load(os.path.join('data', load_name))
-    print('loaded dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
-    # filter dataset
-    dataset = RandomSubset(dataset, data_size, seed)
-    print('random subset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
-    # preprocess dataset
-    dataset = SplitDataset(dataset, seq_len=seq_len)
-    print('processed dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
-    torch.save(dataset, os.path.join('data', save_name))
+from virtual_nematode.data.utils import subset_and_split
 
 
 if __name__ == '__main__':
-    preprocess_dataset(data_size=1, seq_len=16, load_name='forward.pt', save_name='forward_ncp.pt')
+    subset_and_split(data_size=1, seq_len=16, load_name='forward.pt', save_name='forward_ncp.pt')
