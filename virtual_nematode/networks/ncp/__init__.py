9dc6efd105535d0e05a48260c32ff2a6d989882a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-19 14:42:59 +0800

    remove legacy import

--- virtual_nematode/networks/ncp/__init__.py
+++ virtual_nematode/networks/ncp/__init__.py
@@ -1,4 +1,3 @@
 import virtual_nematode.networks.ncp.ltc_cell
 import virtual_nematode.networks.ncp.rnn_sequence
-import virtual_nematode.networks.ncp.utils
 import virtual_nematode.networks.ncp.wirings
