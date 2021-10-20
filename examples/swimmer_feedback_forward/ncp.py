ea63d6f647ff9eed309b228c9c16df76ef97018d
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-20 14:34:13 +0800

    change batch_size=1024

--- examples/swimmer_feedback_forward/ncp.py
+++ examples/swimmer_feedback_forward/ncp.py
@@ -3,7 +3,7 @@ from virtual_nematode.trainers.ncp import train_eval_test
 
 if __name__ == '__main__':
     train_eval_test(
-        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=128,
+        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=42, cuda=0, lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
         units=50, output_dim=11, in_features=23
