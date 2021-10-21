7f92bbaa86365b272828ff745f5f19467832bbd5
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-21 11:36:22 +0800

    add 100 trials simulatations result

--- examples/swimmer_feedback_forward/sim.py
+++ examples/swimmer_feedback_forward/sim.py
@@ -4,4 +4,7 @@ from virtual_nematode.envs.swimmer_forward import simulate
 
 
 if __name__ == '__main__':
+    """ results
+    100 trials: com displacement mean 40.84 / 2500 steps
+    """
     simulate(max_episode_steps=2500, reset_noise_scale=1.745, seed=None, trials=100)
