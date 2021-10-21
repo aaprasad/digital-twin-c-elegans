f239697750cbd40db622cb83233742e7e900e403
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-21 11:08:49 +0800

    add single tester and record video

--- examples/swimmer_feedback_forward/ncp_online.py
+++ examples/swimmer_feedback_forward/ncp_online.py
@@ -1,5 +1,5 @@
 import torch
-from virtual_nematode.testers.swimmer_forward import tester
+from virtual_nematode.testers.swimmer_forward import tester, single_tester
 
 
 def encode_func(data, observation):
@@ -16,5 +16,7 @@ if __name__ == '__main__':
     data_size = 100
     reset_noise_scale = 1.745
     max_episode_steps = 2500
+    record = False
     kwargs = {'units': 50, 'output_dim': 11, 'in_features': 23}
     tester(encode_func, seed, max_episode_steps, reset_noise_scale, model_folder, model_name, data_size, mode, **kwargs)
+    single_tester(encode_func, seed, max_episode_steps, reset_noise_scale, model_folder, model_name, mode, record, **kwargs)
