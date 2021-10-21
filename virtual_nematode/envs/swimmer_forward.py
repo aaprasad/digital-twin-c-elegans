adc6564c32b33ae6d2bc43deb3c296dbdad5084d
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-21 11:30:31 +0800

    print each trial's result

--- virtual_nematode/envs/swimmer_forward.py
+++ virtual_nematode/envs/swimmer_forward.py
@@ -38,8 +38,8 @@ def simulate(seed=None, max_episode_steps=2500, trials=1):
             action = model.step(step=step, q=observation[1:12], q_vel=observation[15:])
             observation, reward, done, info = env.step(action)
             if done:
-                print('Trial {}: episode finished after {} steps'.format(i + 1, step + 1))
+                d = np.linalg.norm(np.array(env.stats['com'][-1]) - np.array(env.stats['com'][0]), ord=2)
+                displacements.append(d)
+                print('Trial {}: com displacement {:.2f} / {} steps'.format(i + 1, d, step + 1))
                 break
-        d = np.linalg.norm(np.array(env.stats['com'][-1]) - np.array(env.stats['com'][0]), ord=2)
-        displacements.append(d)
     print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(displacements), np.mean(displacements), max_episode_steps))
