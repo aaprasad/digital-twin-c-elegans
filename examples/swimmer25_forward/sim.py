2c9c92312f9c2e91bd8b4a8799eaf479412c0386
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-26 09:59:02 +0800

    edit swimmer configuration

--- examples/swimmer25_forward/sim.py
+++ examples/swimmer25_forward/sim.py
@@ -5,11 +5,17 @@ from virtual_nematode.simulation.forward import simulate
 
 
 if __name__ == '__main__':
-    n_bodies = 12
+    """ swimmer configuration
+    joint size = 0.1 <- radius = 0.04mm
+    whole body len = 2.5 <- length = 1mm
+    joint amount = 24 <- amount of muscles in one quadrant = 24
+    body len = 0.1
+    """
+    n_bodies = 25
     joint_range = '-100 100'
-    body_len = 0.25
+    body_len = 0.1
     camera_pos = '0 -6 6'
-    camera_z = None
+    camera_z = 50
     camera_name = None
     max_episode_steps = 2500
     video_name = 'swimmer_forward'
