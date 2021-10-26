f0d79288350409de7a580a6b2bfd3b3f0770050a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-10-26 11:30:40 +0800

    change n_bodies and fix observation

--- examples/swimmer25_forward/sim.py
+++ examples/swimmer25_forward/sim.py
@@ -1,16 +1,42 @@
-""" swimmer: forward locomotion """
-
-from virtual_nematode.envs.swimmer_v3_v2 import make_swimmer
-from virtual_nematode.simulation.forward import simulate
-
-
-if __name__ == '__main__':
-    """ swimmer configuration
+""" swimmer: forward locomotion
+swimmer configuration
     joint size = 0.1 <- radius = 0.04mm
     whole body len = 2.5 <- length = 1mm
     joint amount = 24 <- amount of muscles in one quadrant = 24
+    n_bodies = 25
     body len = 0.1
-    """
+observation space: (52,) including qpos[2:27], qvel[0:25],
+    qpos[0:27]: x_pos, y_pos, ?, q[0:24]
+    qvel[0:27]: x_vel, y_vel, ?, q_vel[0:24]
+"""
+
+import numpy as np
+from virtual_nematode.envs.swimmer_v3_v2 import make_swimmer
+from virtual_nematode.models.forward import Forward
+
+
+def simulate(env, seed=None, max_episode_steps=2500, trials=1):
+    """ forward sinusoidal movement """
+    displacements = []
+    if trials > 1:
+        seed = None  # ensure that seed is different in each trial
+    for i in range(trials):
+        env.seed(seed)
+        observation = env.reset()
+        model = Forward(dt=env.dt, seed=seed, n=25)
+        for step in range(10 ** 6):
+            # env.render()
+            action = model.step(step=step, q=observation[1:25], q_vel=observation[28:])
+            observation, reward, done, info = env.step(action)
+            if done:
+                d = np.linalg.norm(np.array(env.stats['com'][-1]) - np.array(env.stats['com'][0]), ord=2)
+                displacements.append(d)
+                print('Trial {}: com displacement {:.2f} / {} steps'.format(i + 1, d, step + 1))
+                break
+    print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(displacements), np.mean(displacements), max_episode_steps))
+
+
+if __name__ == '__main__':
     n_bodies = 25
     joint_range = '-100 100'
     body_len = 0.1
@@ -24,4 +50,6 @@ if __name__ == '__main__':
         n_bodies, joint_range, body_len, camera_pos, camera_z, camera_name, max_episode_steps, video_name,
         reset_noise_scale
     )
+    # print(env.action_space)
+    # print(env.observation_space)
     simulate(env, seed=None, max_episode_steps=max_episode_steps, trials=1)
