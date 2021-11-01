0bb8e5da998ec3018de5f3d0f6ae94811f1f5022
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-01 23:22:19 +0800

    fix camera and fix import

--- virtual_nematode/envs/swimmer_v3_v2.py
+++ virtual_nematode/envs/swimmer_v3_v2.py
@@ -1,5 +1,6 @@
 import gym
-from gym_worm.envs.mujoco.swimmer_v3_v2 import swimmer
+from gym_worm.envs.mujoco.swimmer_v3_v1 import swimmer
+from gym_worm.envs.mujoco.camera import camera
 from gym_worm.wrappers.position import Position
 from gym_worm.wrappers.recorder import Recorder
 import os
@@ -10,7 +11,8 @@ def make_swimmer(
     max_episode_steps=1000, video_name='swimmer', reset_noise_scale=0.1
 ):
     """ create swimmer env """
-    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len, camera_pos, camera_z)
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
+    xml_str = camera(xml_str, camera_pos, camera_z)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
