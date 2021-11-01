4b52b3effd7d606c86539b438ceb41465bdc6ac1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-01 23:22:58 +0800

    rename Muscles->MuscleAction

--- examples/swimmer_v2_forward/sim.py
+++ examples/swimmer_v2_forward/sim.py
@@ -1,6 +1,6 @@
 """ swimmer: forward locomotion """
 
-from gym_worm.wrappers.muscles import Muscles
+from gym_worm.wrappers.muscle_action import MuscleAction
 from virtual_nematode.envs.swimmer_v3_v2 import make_swimmer
 from virtual_nematode.models.forward import Forward
 from virtual_nematode.simulation.forward import simulate
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         n_bodies=25, joint_range='-100 100', body_len=0.1, camera_pos='0 -6 6', camera_z=50, camera_name=None,
         max_episode_steps=max_episode_steps, video_name='swimmer', reset_noise_scale=0.1
     )
-    env = Muscles(env)
+    env = MuscleAction(env)
     # check_wrapper(env)
     model = Forward(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2.)
     simulate(env, model, model_kwargs_func, action_func=action_func, seed=None, max_episode_steps=max_episode_steps, trials=1)
