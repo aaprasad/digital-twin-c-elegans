7d051b909f058985aa1eb049744984f7a575fd68
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-02 20:05:05 +0800

    online test

---
+++ examples/swimmer_v1_forward/ncp_online.py
@@ -0,0 +1,31 @@
+import gym
+import os
+import torch
+from virtual_nematode.envs.swimmer import make_swimmer
+from virtual_nematode.testers.forward import tester, single_tester
+from virtual_nematode.trainers.ncp import prepare_model
+
+
+def encode_func(stimuli, observation, **kwargs):
+    data = torch.tensor([stimuli] + observation[1:25].tolist() + observation[28:].tolist(), dtype=torch.float64)
+    return data
+
+
+if __name__ == '__main__':
+    model_folder = os.path.join('runs', '')
+    model_name = 'fully_connected'
+    seed = 42
+    data_size = 100
+    reset_noise_scale = 1.745
+    max_episode_steps = 2500
+    env = make_swimmer(
+        n_bodies=25, joint_range='-100 100', body_len=0.1, max_episode_steps=max_episode_steps,
+        reset_noise_scale=reset_noise_scale
+    )
+    model = prepare_model(
+        model_name, model_path=os.path.join(model_folder, 'model.pt'),
+        **{'units': 50, 'output_dim': 24, 'in_features': 49}
+    )
+    tester(env, model, encode_func, seed, max_episode_steps, model_folder, model_name, data_size)
+    env = gym.wrappers.Monitor(env, directory='video/swimmer_forward', force=True)
+    single_tester(env, model, encode_func, seed, max_episode_steps)
