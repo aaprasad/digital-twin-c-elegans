d74231efead33e855b98174b111e0f95b06026c1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-02 17:05:24 +0800

    ncp preprocess dataset

--- examples/swimmer_v1_forward/ncp_data.py
+++ examples/swimmer_v1_forward/ncp_data.py
@@ -4,10 +4,8 @@ from virtual_nematode.data.split import SplitDataset
 
 
 def preprocess_dataset(seq_len=16, load_name='source.pt', save_name='target.pt'):
-    # load dataset
     dataset = torch.load(os.path.join('data', load_name))
     print('load dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
-    # split dataset
     dataset = SplitDataset(dataset, seq_len=seq_len)
     print('split dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
     torch.save(dataset, os.path.join('data', save_name))
