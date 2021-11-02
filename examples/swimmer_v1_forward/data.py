fdf31344ebc54ff649a6b3cc2052baed866aca90
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-02 20:50:45 +0800

    data_size=5400, max_episode_steps=256

--- examples/swimmer_v1_forward/data.py
+++ examples/swimmer_v1_forward/data.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     y: torch.Tensor, (max_episode_steps, action_size)
     """
     input_size = 49  # 1 + 24 + 24
-    data_size = 6000
+    data_size = 4500
     seed = 42
-    max_episode_steps = 192
+    max_episode_steps = 256
     reset_noise_scale = 1.745
     env = make_swimmer(
         n_bodies=25, joint_range='-100 100', body_len=0.1, max_episode_steps=max_episode_steps,
