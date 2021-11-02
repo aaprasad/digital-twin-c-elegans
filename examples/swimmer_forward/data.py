34903f174ee00f98532ea3af4e74222ae0a408a1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-02 14:34:48 +0800

    swimmer forward generate data

--- examples/swimmer_forward/data.py
+++ examples/swimmer_forward/data.py
@@ -2,58 +2,34 @@
 generate forward simulation dataset
 """
 
-import os
-from virtual_nematode.data.simulation import SimulationDataset
-from virtual_nematode.models.forward import Forward
-from virtual_nematode.utils import sample_seed
 from sim import make_swimmer
-import torch
+from virtual_nematode.data.simulation import generate_dataset
+from virtual_nematode.models.forward import Forward
 
 
-def generate_sample(env, model, mode):
-    """ run a forward movement simulation
-    x: torch.Tensor, (max_episode_steps, 1)
-    y: torch.Tensor, (max_episode_steps, action_size)
-    mode: stimuli mode
-    """
-    seed = sample_seed()
-    env.seed(seed)  # seed env
-    model.seed(seed)  # seed model
-    observation = env.reset()
-    x = []
-    y = []
-    for i in range(10 ** 6):
-        action = model.step(step=i, q=observation[1:12], q_vel=observation[15:])
-        stimuli = model.stimuli(step=i, mode=mode)
-        x.append(stimuli)
-        y.append(action.tolist())
-        observation, reward, done, info = env.step(action)
-        if done:
-            break
-    env.close()
-    x = torch.tensor(x, dtype=torch.float64).unsqueeze(-1)
-    y = torch.tensor(y, dtype=torch.float64)
-    return x, y
+def model_kwargs_func(observation):
+    return {'q': observation[1:12], 'q_vel': observation[15:]}
 
 
-def generate_dataset(data_size=100, seed=42, max_episode_steps=2500, mode='sine_wave', save_name='target.pt'):
-    """ generate forward movement dataset
-    x: action sequence of the first joint in trials
-    y: action sequences in trials
-    """
-    env = make_swimmer(max_episode_steps=max_episode_steps, reset_noise_scale=0.)
-    model = Forward(dt=env.dt, seed=seed)
-    action_size = env.action_space.shape[0]
-    dataset = SimulationDataset(
-        data_size, max_episode_steps, 1, action_size, seed, generate_sample,
-        # simulation fn kwargs
-        env=env, model=model, mode=mode
-    )
-    print('dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
-    os.makedirs('data', exist_ok=True)
-    torch.save(dataset, os.path.join('data', save_name))
+def x_func(stimuli, observation):
+    return [stimuli]
 
 
 if __name__ == '__main__':
-    generate_dataset(data_size=1, max_episode_steps=5000, mode='sine_wave', save_name='data.pt')
-    # generate_dataset(data_size=1, max_episode_steps=5000, mode='square_wave', save_name='data.pt')
+    """
+    mode: stimuli mode
+    x: torch.Tensor, (max_episode_steps, input_size)
+        action sequence of the first joint in trials
+    y: torch.Tensor, (max_episode_steps, action_size)
+        action sequences in trials
+    """
+    input_size = 1
+    data_size = 1
+    reset_noise_scale = 0.
+    max_episode_steps = 5000
+    seed = 42
+    mode = 'sine_wave'  # or 'square_wave'
+    save_name = 'data.pt'
+    env = make_swimmer(max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale)
+    model = Forward(dt=env.dt, seed=seed)
+    generate_dataset(env, model, input_size, model_kwargs_func, x_func, data_size, seed, max_episode_steps, mode, save_name)
