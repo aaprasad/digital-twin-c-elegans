f057d18a71e5275f6d8d7418671f91bad22f0e02
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-03 15:33:34 +0800

    set batch_size=1024

--- examples/swimmer_v1_forward/ncp.py
+++ examples/swimmer_v1_forward/ncp.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     time: ~ 1h 50min / 300 epochs
     """
     train_eval_test(
-        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=512,
+        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=42, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
         units=100, output_dim=24, in_features=49
