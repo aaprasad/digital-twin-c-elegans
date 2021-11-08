7ae4fbc639c3dc8c7125efd04253fd5a62036afb
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-08 21:27:11 +0800

    add swimmer friction simulation

--- virtual_nematode/envs/swimmer.py
+++ virtual_nematode/envs/swimmer.py
@@ -1,6 +1,7 @@
 import gym
 from gym_worm.envs.mujoco.swimmer_v3_v1 import swimmer
 from gym_worm.envs.mujoco.camera import camera
+from gym_worm.envs.mujoco.friction import friction
 from gym_worm.wrappers.position import Position
 from gym_worm.wrappers.recorder import Recorder
 import os
@@ -13,6 +14,24 @@ def make_swimmer(
     """ create swimmer env """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
     xml_str = camera(xml_str, camera_pos, camera_z)
+    env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = Position(env, n_bodies)
+    env = Recorder(env, camera_name=camera_name)
+    if camera_name is not None:
+        env = gym.wrappers.Monitor(env, directory=os.path.join('video', video_name), force=True)
+    return env
+
+
+def make_swimmer_friction(
+    n_bodies=12, joint_range='-100 100', body_len=0.25, camera_pos='0 -6 6', camera_z=None, camera_name=None,
+    max_episode_steps=1000, video_name='swimmer', reset_noise_scale=0.1
+):
+    """ create swimmer env """
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
+    xml_str = camera(xml_str, camera_pos, camera_z)
+    xml_str = friction(xml_str, n_section=7, width=1.5, nconmax=400)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
