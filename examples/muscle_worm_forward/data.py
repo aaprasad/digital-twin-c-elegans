cb9f93e8d92a61a7949e952317c4762c581e81a4
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 12:25:12 +0800

    fix muscle worm dataset generation

--- examples/muscle_worm_forward/data.py
+++ examples/muscle_worm_forward/data.py
@@ -2,7 +2,7 @@
 
 from gym_worm.wrappers.muscle_action import joint_to_muscle_action, JointAction
 import os
-from sim import model_kwargs_func
+from sim import action_func
 import torch
 from virtual_nematode.envs.muscle_worm import make_swimmer
 from virtual_nematode.models.forward import Forward
@@ -10,9 +10,9 @@ from virtual_nematode.data.simulation import generate_dataset
 
 
 def x_func(stimuli, observation, **kwargs):
-    """ x: input_size = stimuli_size + actuator_length_size + actuator_velocity_size
+    """ x: input_size = stimuli_size + tendon_length_size + tendon_velocity_size
     external signal: first joint's target angle
-    proprioceptive observations: muscle actuator lengths and velocity
+    proprioceptive observations: tendon lengths and velocity
     """
     return [stimuli] + observation[52:148].tolist() + observation[148:244].tolist()
 
@@ -35,6 +35,6 @@ if __name__ == '__main__':
     action_size = env.action_space.shape[0]  # original muscle action size
     env = JointAction(env)  # ctrl: joint -> muscle
     model = Forward(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=1.5)
-    dataset = generate_dataset(env, model, model_kwargs_func, x_func, y_func, input_size, action_size, data_size, max_episode_steps, seed)
+    dataset = generate_dataset(env, model, action_func, x_func, y_func, input_size, action_size, data_size, max_episode_steps, seed)
     os.makedirs('data', exist_ok=True)
     torch.save(dataset, 'data/data.pt')
