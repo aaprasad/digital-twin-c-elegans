e1a332e82207d321459dae9d0359c52a8e5dfbb3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 12:22:47 +0800

    add tendon sensors in muscle worm

--- virtual_nematode/envs/muscle_worm.py
+++ virtual_nematode/envs/muscle_worm.py
@@ -2,6 +2,7 @@ import gym
 from gym_worm.envs.mujoco.camera import camera
 from gym_worm.envs.mujoco.muscle_worm_v0 import swimmer
 from gym_worm.envs.mujoco.position import position
+from gym_worm.envs.mujoco.tendon import tendon
 from gym_worm.wrappers.sensor_observation import SensorObservation
 
 
@@ -11,6 +12,7 @@ def make_swimmer(
 ):
     """ create swimmer env """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len, muscle_len, y_sidesite, z_medial, z_lateral, arrangement=None)
+    xml_str = tendon(xml_str)
     xml_str = position(xml_str)
     xml_str = camera(xml_str, camera_pos='0 -5 5', camera_z=None)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
