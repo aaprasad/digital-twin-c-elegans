821f4321be70b7a068f959cd46dd60c8e1cac36c
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 17:05:30 +0800

    add make_swimmer()

--- examples/arena/swimmer_friction/sim.py
+++ examples/arena/swimmer_friction/sim.py
@@ -1,8 +1,27 @@
 import gym
 import numpy as np
-from virtual_nematode.envs.swimmer import make_swimmer_friction
+from gym_worm.envs.mujoco.camera import camera
+from gym_worm.envs.mujoco.friction import friction
+from gym_worm.envs.mujoco.position import position
+from gym_worm.envs.mujoco.swimmer_v3_v1 import swimmer
+from gym_worm.wrappers.recorder import Recorder
+from gym_worm.wrappers.sensor_observation import SensorObservation
 from virtual_nematode.models.forward import Forward
 from virtual_nematode.simulation import simulate
+
+
+def make_swimmer_friction(n_bodies=12, joint_range='-100 100', body_len=0.25, max_episode_steps=1000, reset_noise_scale=0.1):
+    """ create swimmer env """
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
+    xml_str = position(xml_str)
+    xml_str = friction(xml_str, n_section=7, width=1.5, nconmax=400)
+    xml_str = camera(xml_str)
+    env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = SensorObservation(env)
+    env = Recorder(env, camera_name=None)
+    return env
 
 
 def action_func(model, step, observation, **kwargs):
