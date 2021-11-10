9a386be2fb9230da1e2859223bdc37954de20bed
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 22:11:02 +0800

    add timelimit, first anticlockwise and then clockwise

--- examples/arena/swimmer_tracking/sim.py
+++ examples/arena/swimmer_tracking/sim.py
@@ -28,14 +28,14 @@ def source_func(observation):
     return observation[62:64]
 
 
-def make_swimmer(x, y, n_bodies=25, joint_range='-100 100', body_len=0.1):
+def make_swimmer(x, y, n_bodies=25, joint_range='-100 100', body_len=0.1, max_episode_steps=1000):
     """ create swimmer env """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
     xml_str = position(xml_str)
     xml_str = tracking(xml_str, x, y, kp=1)
     xml_str = camera(xml_str)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=0.)
-    env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=max_episode_steps)
     env = gym.wrappers.ClipAction(env)
     env = SensorObservation(env)
     env = DistributionObservation(env, dt=env.dt, f=fick, position_func=position_func, source=None, source_func=source_func)
@@ -48,7 +48,10 @@ def action_func(model, step, observation, **kwargs):
     g_p = observation[67]
     g_w = observation[68]
     action = model.step(step, q, q_vel, g_p, g_w)
-    theta = step / 100.
+    if step < 1250:
+        theta = step / 100.  # anticlockwise
+    else:
+        theta = (2500 - step) / 100.  # clockwise
     radius = 10
     action = np.concatenate((action, [np.cos(theta) * radius, np.sin(theta) * radius]), axis=0)
     return action
@@ -67,7 +70,7 @@ def done_func(index, result, **kwargs):
 
 if __name__ == '__main__':
     max_episode_steps = 2500
-    env = make_swimmer(x=0, y=0)
+    env = make_swimmer(x=0, y=0, max_episode_steps=max_episode_steps)
     # env = gym.wrappers.Monitor(env, directory='video/swimmer', force=True)
     print(env.action_space)
     print(env.observation_space)
