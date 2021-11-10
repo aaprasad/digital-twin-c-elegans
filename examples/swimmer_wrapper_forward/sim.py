83914a10fe1b07c590456a24a5a56da249c0cd9c
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 12:12:44 +0800

    fix swimmer with wrapper simulation

--- examples/swimmer_wrapper_forward/sim.py
+++ examples/swimmer_wrapper_forward/sim.py
@@ -6,13 +6,28 @@ swimmer configuration
 """
 
 from gym_worm.wrappers.muscle_action import MuscleAction, JointAction
+import numpy as np
 from virtual_nematode.envs.swimmer import make_swimmer
 from virtual_nematode.models.forward import Forward
 from virtual_nematode.simulation import simulate
 
 
-def model_kwargs_func(observation, **kwargs):
-    return {'q': observation[1:25], 'q_vel': observation[28:52]}
+def action_func(model, step, observation, **kwargs):
+    q = observation[1:25]
+    q_vel = observation[28:52]
+    action = model.step(step, q, q_vel)
+    return action
+
+
+def step_func(observation, **kwargs):
+    com = observation[52:54]
+    return com
+
+
+def done_func(index, result, **kwargs):
+    displacement = np.linalg.norm(np.array(result[-1]) - np.array(result[0]), ord=2)
+    print('Trial {}: com displacement {:.2f}'.format(index + 1, displacement))
+    return displacement
 
 
 if __name__ == '__main__':
@@ -21,10 +36,10 @@ if __name__ == '__main__':
     """
     max_episode_steps = 2500
     env = make_swimmer(
-        n_bodies=25, joint_range='-100 100', body_len=0.1, camera_pos='0 -6 6', camera_z=50, camera_name=None,
-        max_episode_steps=max_episode_steps, reset_noise_scale=1.745
+        n_bodies=25, joint_range='-100 100', body_len=0.1, max_episode_steps=max_episode_steps, reset_noise_scale=1.745
     )
     env = MuscleAction(env)  # ctrl: muscle -> joint
     env = JointAction(env)  # ctrl: joint -> muscle
     model = Forward(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2.)
-    simulate(env, model, model_kwargs_func, seed=None, max_episode_steps=max_episode_steps, trials=100)
+    results = simulate(env, model, action_func, step_func, done_func, seed=None, trials=100, render=False)
+    print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(results), np.mean(results), max_episode_steps))
