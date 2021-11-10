e70086664d0e41f3d2c813abaa0e44fb9c1b0730
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-10 13:11:13 +0800

    change track cam pos

--- virtual_nematode/envs/swimmer.py
+++ virtual_nematode/envs/swimmer.py
@@ -11,7 +11,7 @@ def make_swimmer(n_bodies=12, joint_range='-100 100', body_len=0.25, max_episode
     """ create swimmer env """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
     xml_str = position(xml_str)
-    xml_str = camera(xml_str, camera_pos='0 -5 5', camera_z=None)
+    xml_str = camera(xml_str, camera_pos='0 -6 6', camera_z=None)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
@@ -24,7 +24,7 @@ def make_swimmer_friction(n_bodies=12, joint_range='-100 100', body_len=0.25, ma
     """ create swimmer env """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
     xml_str = position(xml_str)
-    xml_str = camera(xml_str, camera_pos='0 -5 5', camera_z=None)
+    xml_str = camera(xml_str, camera_pos='0 -6 6', camera_z=None)
     xml_str = friction(xml_str, n_section=7, width=1.5, nconmax=400)
     env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
