aa3647b3cfdd266d319d36be292bd3a2a386a6e8
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-15 18:51:26 +0800

    set units=72

--- examples/swimmer_forward/ncp.py
+++ examples/swimmer_forward/ncp.py
@@ -6,13 +6,13 @@ def fully_connected():
     units = 100
         memory: 7G * 4
         time: ~ 2h / 300 epochs
-    units = 60
+    units = 72
     """
     train_eval_test(
         data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
-        units=60, output_dim=24, in_features=49
+        units=72, output_dim=24, in_features=49
     )
 
 
@@ -21,7 +21,7 @@ def ncp():
         data_name='ncp.pt', model_name='ncp', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
-        in_features=49, inter_neurons=24, command_neurons=48, motor_neurons=24, sensory_fanout=12, inter_fanout=6,
+        in_features=49, inter_neurons=12, command_neurons=36, motor_neurons=24, sensory_fanout=6, inter_fanout=12,
         recurrent_command_synapses=6, motor_fanin=6
     )
 
