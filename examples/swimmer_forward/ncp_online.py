88d79d3632d4ffdccc114d9b5be03ad33c7f7196
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-15 19:22:43 +0800

    clean code

--- examples/swimmer_forward/ncp_online.py
+++ examples/swimmer_forward/ncp_online.py
@@ -12,16 +12,17 @@ def fully_connected():
     100 trials: com displacement mean 11.90 / 2500 steps
     1 trial: com displacement 11.58 / 2500 steps
     """
-    return prepare_model(
+    model_name = 'fully_connected'
+    model = prepare_model(
         model_name, model_path=os.path.join(model_folder, 'model.pt'),
         **{'units': 100, 'output_dim': 24, 'in_features': 49}
     )
+    return model, model_name
 
 
 if __name__ == '__main__':
     runs_folder = ''
     model_folder = os.path.join('runs', runs_folder)
-    model_name = 'fully_connected'
     seed = 42
     reset_noise_scale = 1.745
     max_episode_steps = 2500
@@ -29,7 +30,7 @@ if __name__ == '__main__':
         n_bodies=25, joint_range='-100 100', body_len=0.1, max_episode_steps=max_episode_steps,
         reset_noise_scale=reset_noise_scale
     )
-    model = fully_connected()
+    model, model_name = fully_connected()
     tester(env, model, data_func, x_func, seed, max_episode_steps, model_folder, model_name, data_size=100)
     env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder), force=True)
     single_tester(env, model, data_func, x_func, seed, max_episode_steps)
