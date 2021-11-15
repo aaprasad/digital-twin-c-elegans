135b34b1f206fe1f1ebcdb43a4665ca8579bb062
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-15 19:00:08 +0800

    update memory usage

--- examples/swimmer_forward/ncp.py
+++ examples/swimmer_forward/ncp.py
@@ -7,6 +7,7 @@ def fully_connected():
         memory: 7G * 4
         time: ~ 2h / 300 epochs
     units = 72
+        memory: 4.9G * 4
     """
     train_eval_test(
         data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
@@ -17,6 +18,9 @@ def fully_connected():
 
 
 def ncp():
+    """ results
+    memory: 4.9G * 4
+    """
     train_eval_test(
         data_name='ncp.pt', model_name='ncp', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
