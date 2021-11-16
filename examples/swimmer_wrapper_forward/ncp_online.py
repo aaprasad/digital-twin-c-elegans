50cf5ad72185d0b5e3b8b6187856d8d826bba121
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-16 19:34:19 +0800

    clean code

--- examples/swimmer_wrapper_forward/ncp_online.py
+++ examples/swimmer_wrapper_forward/ncp_online.py
@@ -8,14 +8,22 @@ from virtual_nematode.testers.forward import tester, single_tester
 from virtual_nematode.trainers.ncp import prepare_model
 
 
-if __name__ == '__main__':
+def fully_connected():
     """ results
     100 trials: com displacement mean 17.94 / 2500 steps
     1 trial: com displacement 18.43 / 2500 steps
     """
+    model_name = 'fully_connected'
+    model = prepare_model(
+        model_name, model_path=os.path.join(model_folder, 'model.pt'),
+        **{'units': 100, 'output_dim': 96, 'in_features': 49}
+    )
+    return model, model_name
+
+
+if __name__ == '__main__':
     runs_folder = ''
     model_folder = os.path.join('runs', runs_folder)
-    model_name = 'fully_connected'
     seed = 42
     reset_noise_scale = 1.745
     max_episode_steps = 2500
@@ -24,10 +32,7 @@ if __name__ == '__main__':
         reset_noise_scale=reset_noise_scale
     )
     env = MuscleAction(env)
-    model = prepare_model(
-        model_name, model_path=os.path.join(model_folder, 'model.pt'),
-        **{'units': 100, 'output_dim': 96, 'in_features': 49}
-    )
+    model, model_name = fully_connected()
     tester(env, model, data_func, x_func, seed, max_episode_steps, model_folder, model_name, data_size=100)
     env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder), force=True)
     single_tester(env, model, data_func, x_func, seed, max_episode_steps)
