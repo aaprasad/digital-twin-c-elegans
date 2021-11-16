a7ce684cdc35ad1f5de2530e8b0f5cdbb1ab7299
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-16 19:24:13 +0800

    swimmer wrapper forward with ncp 49-24-24-96

--- examples/swimmer_wrapper_forward/ncp.py
+++ examples/swimmer_wrapper_forward/ncp.py
@@ -14,5 +14,16 @@ def fully_connected():
     )
 
 
+def ncp():
+    train_eval_test(
+        data_name='ncp.pt', model_name='ncp', lengths=[48000, 12000, 12000], batch_size=1024,
+        seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
+        # model kwargs
+        in_features=49, inter_neurons=24, command_neurons=24, motor_neurons=96, sensory_fanout=24, inter_fanout=24,
+        recurrent_command_synapses=24, motor_fanin=96
+    )
+
+
 if __name__ == '__main__':
     fully_connected()
+    # ncp()
