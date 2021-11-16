21b57ad5c1cb8fa119f8e66aa7b087216b2f18ec
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-16 19:47:58 +0800

    set units=128

--- examples/swimmer_wrapper_forward/ncp.py
+++ examples/swimmer_wrapper_forward/ncp.py
@@ -3,18 +3,23 @@ from virtual_nematode.trainers.ncp import train_eval_test
 
 def fully_connected():
     """ results
-    memory: 7G * 4
-    time: 2h 40min / 300 epochs
+    units = 100
+        memory: 7G * 4
+        time: 2h / 300 epochs
+    units = 128
     """
     train_eval_test(
         data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
-        units=100, output_dim=96, in_features=49
+        units=128, output_dim=96, in_features=49
     )
 
 
 def ncp():
+    """ results
+    memory: 7.5G * 2
+    """
     train_eval_test(
         data_name='ncp.pt', model_name='ncp', lengths=[48000, 12000, 12000], batch_size=256,
         seed=11, cuda=0, device_ids=[0, 1], lr=0.001, epochs=300, early_stop=30, comment='',
