1130c9afdd065da52083a66d855577cee755f554
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-16 11:17:05 +0800

    set batch_size=150

--- examples/swimmer_forward/ncp.py
+++ examples/swimmer_forward/ncp.py
@@ -3,15 +3,15 @@ from virtual_nematode.trainers.ncp import train_eval_test
 
 def fully_connected():
     """ results
-    units = 100
+    units = 100, batch_size = 1024
         memory: 7G * 4
         time: ~ 2h / 300 epochs
-    units = 72
+    units = 72, batch_size = 1024
         memory: 4.9G * 4
         time: ~ 2h / 300 epochs
     """
     train_eval_test(
-        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
+        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=512,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
         units=150, output_dim=24, in_features=49
