e9cfda70252208f0a600c38889754370e4ae0183
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-16 11:12:13 +0800

    set units=150

--- examples/swimmer_forward/ncp.py
+++ examples/swimmer_forward/ncp.py
@@ -14,7 +14,7 @@ def fully_connected():
         data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
-        units=72, output_dim=24, in_features=49
+        units=150, output_dim=24, in_features=49
     )
 
 
