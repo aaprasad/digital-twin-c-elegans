af9b6673e398a3ecfb382e03853c28d0cef07929
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-17 19:35:01 +0800

    muscle worm forward training output_mapping='exp',units=100,batch_size=1024

--- examples/muscle_worm_forward/ncp.py
+++ examples/muscle_worm_forward/ncp.py
@@ -3,24 +3,24 @@ from virtual_nematode.trainers.ncp import train_eval_test
 
 def fully_connected():
     """ results
-    units = 200, batch_size = 256
+    units = 200, batch_size = 256, output_mapping='affine'
         memory: 7.5G * 4
         time: 7h 30min / 300 epochs
-    units = 150, batch_size = 512
+    units = 150, batch_size = 512, output_mapping='affine'
         memory: 8.6G * 4
         time: 4h 30min / 300 epochs
-    units = 100, batch_size = 1024
+    units = 100, batch_size = 1024, output_mapping='affine'
         memory: 8.5G * 4
         time: 2h 20min / 300 epochs
-    units = 128, batch_size = 512
+    units = 128, batch_size = 512, output_mapping='affine'
         memory: 6.7G * 4
         time: 3h 45min / 300 epochs
     """
     train_eval_test(
-        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=512,
+        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024,
         seed=11, cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, epochs=300, early_stop=30, comment='',
         # model kwargs
-        units=128, output_dim=96, in_features=193
+        units=100, output_dim=96, in_features=193, output_mapping='exp'
     )
 
 
