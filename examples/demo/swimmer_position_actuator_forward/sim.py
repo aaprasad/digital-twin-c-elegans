04db73f40217aaba4d47297b8b9e1f0cbd969bff
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-25 11:42:32 +0800

    modify q_max, psi, freq

--- examples/demo/swimmer_position_actuator_forward/sim.py
+++ examples/demo/swimmer_position_actuator_forward/sim.py
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     # env = gym.wrappers.Monitor(env, directory='video/swimmer', force=True)
     print(env.action_space)
     print(env.observation_space)
-    model = Forward(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., motor=False)
+    model = Forward(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., motor=False)  # q_max, psi, freq
     results = simulate(env, model, action_func, step_func, done_func, seed=None, trials=1, render=False)
     print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(results), np.mean(results), max_episode_steps))
