4fa8751ad1c2f36d3816b1825596c33df332d6c3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-11-29 17:51:05 +0800

    muscle worm forward units=100

--- examples/muscle_worm_forward/ncp.py
+++ examples/muscle_worm_forward/ncp.py
@@ -17,11 +17,11 @@ def fully_connected():
         time: 3h 45min / 300 epochs
     """
     train_eval_test(
-        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=512, seed=11,
+        data_name='ncp.pt', model_name='fully_connected', lengths=[48000, 12000, 12000], batch_size=1024, seed=11,
         cuda=0, device_ids=[0, 1, 2, 3], lr=0.001, weight_decay=0, epochs=100, early_stop=30, comment='',
         loss='MSELoss', sr=None,
         # model kwargs
-        units=150, output_dim=96, in_features=192, output_mapping='affine'
+        units=100, output_dim=96, in_features=192, output_mapping='affine'
     )
 
 
