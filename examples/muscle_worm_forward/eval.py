3e4e40d2c4ad2da31778f4b135c9e284dacc3e8a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-12-14 17:34:41 +0800

    clean muscle worm forward eval code

--- examples/muscle_worm_forward/eval.py
+++ examples/muscle_worm_forward/eval.py
@@ -9,36 +9,36 @@ from virtual_nematode.testers.forward import tester, single_tester
 from virtual_nematode.trainers.ncp import prepare_model
 
 
-def fully_connected(ckpt_name):
-    model_name = 'fully_connected'
-    model = prepare_model(
-        model_name, model_path=os.path.join(model_folder, ckpt_name),
-        **{'units': 100, 'output_dim': 96, 'in_features': 192, 'output_mapping': 'affine'}
-    )
-    return model, model_name
+def select_model(model_name, ckpt_name):
+    if model_name == 'fully_connected':
+        kwargs = {'units': 100, 'output_dim': 96, 'in_features': 192, 'output_mapping': 'affine'}
+    else:
+        raise AssertionError('{} not exist'.format(model_name))
+    model = prepare_model(model_name, model_path=os.path.join(model_folder, ckpt_name), **kwargs)
+    return model
 
 
-def evaluate(start, end):
+def evaluate(model_name, start, end):
     """ online test each checkpoint once for evaluation """
     for i in range(start, end):
         ckpt_name = 'model{}.pt'.format(i)
-        model, _ = fully_connected(ckpt_name)
+        model = select_model(model_name, ckpt_name)
         print(ckpt_name, end=' ')
         _, y = single_tester(env, model, data_func, x_func, seed, max_episode_steps)
         torch.save(y, os.path.join(data_path, ckpt_name))  # action sequence
 
 
-def test(start, end):
+def test(model_name, start, end):
     """ online test multiple trials for testing """
     for i in range(start, end):
         ckpt_name = 'model{}.pt'.format(i)
-        model, model_name = fully_connected(ckpt_name)
+        model = select_model(model_name, ckpt_name)
         tester(env, model, data_func, x_func, seed, max_episode_steps, model_folder, model_name, data_size=100)
 
 
-def record(env, ckpt_name):
+def record(model_name, env, ckpt_name):
     """ online test once for evaluation and record video """
-    model, _ = fully_connected(ckpt_name)
+    model = select_model(model_name, ckpt_name)
     env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder, ckpt_name), force=True)
     _, y = single_tester(env, model, data_func, x_func, seed, max_episode_steps)
     torch.save(y, os.path.join(data_path, ckpt_name))  # action sequence
@@ -55,6 +55,6 @@ if __name__ == '__main__':
     data_path = os.path.join('data', runs_folder)  # data folder for storing model action sequence output
     os.makedirs(data_path, exist_ok=True)
     env = make_swimmer(max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale)
-    evaluate(start=0, end=100)
-    # test(start=0, end=100)
-    # record(env, ckpt_name='model.pt')
+    evaluate('fully_connected', start=0, end=100)
+    # test('fully_connected', start=0, end=100)
+    # record('fully_connected', env, ckpt_name='model.pt')
