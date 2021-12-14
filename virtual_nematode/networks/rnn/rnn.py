5f2ad8562513d52c919d16d486d65ba359370c05
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-12-14 17:03:14 +0800

    base RNNCell on torch.nn.Module instead of torch.nn.RNNCell

--- virtual_nematode/networks/rnn/rnn.py
+++ virtual_nematode/networks/rnn/rnn.py
@@ -1,12 +1,13 @@
 import torch
 
 
-class RNNCell(torch.nn.RNNCell):
+class RNNCell(torch.nn.Module):
     def __init__(self, input_size, hidden_size, output_size, **kwargs):
         """ Vanilla RNN
         **kwargs: bias=True, nonlinearity='tanh'
         """
-        super(RNNCell, self).__init__(input_size, hidden_size, **kwargs)
+        super(RNNCell, self).__init__()
+        self.rnn_cell = torch.nn.RNNCell(input_size, hidden_size, **kwargs)
         self.hidden_size = hidden_size
         self.output_size = output_size
 
@@ -15,6 +16,6 @@ class RNNCell(torch.nn.RNNCell):
         return self.hidden_size
 
     def forward(self, inputs, states):
-        states = super(RNNCell, self).forward(inputs, states)
+        states = self.rnn_cell(inputs, states)
         outputs = states[:, 0:self.output_size]
         return outputs, states
