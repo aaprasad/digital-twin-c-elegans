d96ed25269112dd1f3c4686cdd4185034a79fdf0
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-12-20 10:23:02 +0800

    rnn based on RNNCell

--- virtual_nematode/networks/rnn/rnn.py
+++ virtual_nematode/networks/rnn/rnn.py
@@ -7,11 +7,11 @@ class RNNCell(torch.nn.Module):
         **kwargs: bias=True, nonlinearity='tanh'
         """
         super(RNNCell, self).__init__()
-        # self.rnn_cell = torch.nn.RNNCell(input_size, hidden_size, **kwargs)
-        self.linear = torch.nn.Sequential(
-            torch.nn.Linear(input_size + hidden_size, hidden_size, bias=True),
-            torch.nn.Tanh()
-        )
+        self.rnn_cell = torch.nn.RNNCell(input_size, hidden_size, **kwargs)
+        # self.linear = torch.nn.Sequential(
+        #     torch.nn.Linear(input_size + hidden_size, hidden_size, bias=True),
+        #     torch.nn.Tanh()
+        # )
         self.hidden_size = hidden_size
         self.output_size = output_size
 
@@ -20,7 +20,7 @@ class RNNCell(torch.nn.Module):
         return self.hidden_size
 
     def forward(self, inputs, states):
-        # states = self.rnn_cell(inputs, states)
-        states = self.linear(torch.cat((inputs, states), dim=-1))
+        states = self.rnn_cell(inputs, states)  # it's the same
+        # states = self.linear(torch.cat((inputs, states), dim=-1))
         outputs = states[:, 0:self.output_size]
         return outputs, states
