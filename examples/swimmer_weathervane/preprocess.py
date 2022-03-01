0b6fd32d7378fe9a06e78097176a353fc5b1dbd0
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-01 16:59:17 +0800

    preprocess weathervane dataset

---
+++ examples/swimmer_weathervane/preprocess.py
@@ -0,0 +1,28 @@
+import os
+import torch
+from virtual_nematode.data.concat import ConcatDataset
+from virtual_nematode.data.split import SplitDataset
+from virtual_nematode.data.subsequence import SubsequenceDataset
+
+
+def preprocess_dataset(seq_ranges, seq_len=16, load_name='source.pt', save_name='target.pt'):
+    """
+    x: torch.Tensor, (data_size, seq_len, input_size)
+    y: torch.Tensor, (data_size, seq_len, action_size)
+    """
+    dataset = torch.load(os.path.join('data', load_name))
+    print('load dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
+    datasets = []
+    for i, seq_range in enumerate(seq_ranges):
+        d = SubsequenceDataset(dataset, seq_range)
+        print(i, 'subsequence dataset', len(d), d[0][0].size(), d[0][1].size())
+        d = SplitDataset(d, seq_len)
+        print(i, 'split dataset', len(d), d[0][0].size(), d[0][1].size())
+        datasets.append(d)
+    dataset = ConcatDataset(datasets)
+    print('concat dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
+    torch.save(dataset, os.path.join('data', save_name))
+
+
+if __name__ == '__main__':
+    preprocess_dataset([(0, 3500)], seq_len=16, load_name='data.pt', save_name='full.pt')
