9903427361a9abdd9c8909d6dc7ff34602ef5e17
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-02 11:42:15 +0800

    weathervane dataset: output processed max gradient value

--- examples/swimmer_weathervane/preprocess.py
+++ examples/swimmer_weathervane/preprocess.py
@@ -28,6 +28,8 @@ def preprocess_dataset(seq_ranges, data_size, seq_len=16, seed=11, load_name='so
     print('random subset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
     dataset = GradientDataset(dataset, p_range=(0, 48), g_index=48, g_coef=fick())
     print('gradient dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size(), 'max abs gradient', fick())
+    x, _ = dataset.tensors
+    print('max positive gradient', x[:, :, 48], 'max negative gradient', x[:, :, 49])
     torch.save(dataset, os.path.join('data', save_name))
 
 
