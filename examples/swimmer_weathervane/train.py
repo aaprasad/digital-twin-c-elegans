e603d51d6398d0433a65aed0eadefb43aae255db
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-02 17:18:15 +0800

    weathervane: train with fully_connected ncp

---
+++ examples/swimmer_weathervane/train.py
@@ -0,0 +1,30 @@
+""" swimmer: chemotaxis based on weathervane mechanism """
+
+import torch
+from virtual_nematode.trainers.ncp import train_eval_test
+
+
+def train(model_name):
+    if model_name == 'fully_connected':
+        kwargs = {
+            'data_name': 'ncp.pt', 'model_name': model_name, 'lengths': [48000, 12000, 12000],
+            'batch_size': 1024, 'seed': 11, 'cuda': 0, 'device_ids': [0, 1, 2, 3], 'lr': 0.001, 'epochs': 100,
+            'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
+            # model kwargs
+            'units': 50, 'output_dim': 24, 'in_features': 50
+        }
+    elif model_name == 'ctrnn':
+        torch.set_default_dtype(torch.float64)
+        kwargs = {
+            'data_name': 'ncp.pt', 'model_name': model_name, 'lengths': [48000, 12000, 12000], 'batch_size': 1024, 'seed': 11,
+            'cuda': 0, 'device_ids': [0], 'lr': 0.001, 'epochs': 100, 'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
+            # model kwargs
+            'input_size': 50, 'hidden_size': 50, 'output_size': 24, 'feedback': True, 'readout': 'identity'
+        }
+    else:
+        raise AssertionError('{} not exist'.format(model_name))
+    train_eval_test(**kwargs)
+
+
+if __name__ == '__main__':
+    train(model_name='fully_connected')
