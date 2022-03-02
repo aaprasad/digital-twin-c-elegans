199aa9e237d3cff64282c88d9d9dc3e06ab72aca
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-02 17:20:00 +0800

    weathervane: train with steering data

--- examples/swimmer_weathervane/train.py
+++ examples/swimmer_weathervane/train.py
@@ -4,10 +4,10 @@ import torch
 from virtual_nematode.trainers.ncp import train_eval_test
 
 
-def train(model_name):
+def train(model_name, data_name):
     if model_name == 'fully_connected':
         kwargs = {
-            'data_name': 'ncp.pt', 'model_name': model_name, 'lengths': [48000, 12000, 12000],
+            'data_name': data_name, 'model_name': model_name, 'lengths': [48000, 12000, 12000],
             'batch_size': 1024, 'seed': 11, 'cuda': 0, 'device_ids': [0, 1, 2, 3], 'lr': 0.001, 'epochs': 100,
             'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
             # model kwargs
@@ -16,7 +16,7 @@ def train(model_name):
     elif model_name == 'ctrnn':
         torch.set_default_dtype(torch.float64)
         kwargs = {
-            'data_name': 'ncp.pt', 'model_name': model_name, 'lengths': [48000, 12000, 12000], 'batch_size': 1024, 'seed': 11,
+            'data_name': data_name, 'model_name': model_name, 'lengths': [48000, 12000, 12000], 'batch_size': 1024, 'seed': 11,
             'cuda': 0, 'device_ids': [0], 'lr': 0.001, 'epochs': 100, 'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
             # model kwargs
             'input_size': 50, 'hidden_size': 50, 'output_size': 24, 'feedback': True, 'readout': 'identity'
@@ -27,4 +27,4 @@ def train(model_name):
 
 
 if __name__ == '__main__':
-    train(model_name='fully_connected')
+    train(model_name='fully_connected', data_name='steering.pt')
