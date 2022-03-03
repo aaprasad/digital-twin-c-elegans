e6ef2de29e31b736b48d332cd80d314853e92980
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-03 16:59:17 +0800

    weathervane: evaluate network

--- examples/swimmer_weathervane/eval.py
+++ examples/swimmer_weathervane/eval.py
@@ -1,4 +1,4 @@
-from data import x_func as data_func
+from data import x_func as data_func_base
 import gym
 import os
 from sim import position_func
@@ -6,8 +6,17 @@ from sim import step_func as x_func
 import sys
 import torch
 from virtual_nematode.envs.swimmer import make_chemotaxis_swimmer
-from virtual_nematode.testers.forward import tester, single_tester
+from virtual_nematode.testers.chemotaxis import single_tester, tester
 from virtual_nematode.trainers.ncp import prepare_model
+
+
+def data_func(observation, **kwargs):
+    data = data_func_base(observation, **kwargs)
+    proprioception = data[0:48]
+    gradient = data[49]
+    gradient_positive = gradient if gradient > 0 else 0
+    gradient_negative = abs(gradient) if gradient < 0 else 0
+    return proprioception + [gradient_positive, gradient_negative]
 
 
 def select_model(model_name, ckpt_name):
@@ -20,6 +29,33 @@ def select_model(model_name, ckpt_name):
         raise AssertionError('{} not exist'.format(model_name))
     model = prepare_model(model_name, model_path=os.path.join(model_folder, ckpt_name), **kwargs)
     return model
+
+
+def evaluate(model_name, start, end):
+    """ online test once for evaluation """
+    for i in range(start, end):
+        ckpt_name = 'model{}.pt'.format(i)
+        model = select_model(model_name, ckpt_name)
+        print(ckpt_name, end=' ')
+        _, y = single_tester(envs[0], model, data_func, x_func, seed)
+        torch.save(y, os.path.join(data_path, ckpt_name))  # action sequence
+
+
+def test(model_name, start, end):
+    """ online test multiple trials for testing """
+    for i in range(start, end):
+        ckpt_name = 'model{}.pt'.format(i)
+        print(ckpt_name, end=' ')
+        model = select_model(model_name, ckpt_name)
+        tester(envs, model, data_func, x_func, seed, max_episode_steps, model_folder, model_name, data_size_per_trial, disable=True)
+
+
+def record(model_name, env, ckpt_name):
+    """ online test once for evaluation and record video """
+    model = select_model(model_name, ckpt_name)
+    env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder, ckpt_name), force=True)
+    _, y = single_tester(env, model, data_func, x_func, seed)
+    torch.save(y, os.path.join(data_path, ckpt_name))  # action sequence
 
 
 if __name__ == '__main__':
@@ -40,4 +76,4 @@ if __name__ == '__main__':
     )
     evaluate('fully_connected', start=0, end=100)
     # test('fully_connected', start=0, end=100)
-    # record('fully_connected', env, ckpt_name='model.pt')
+    # record('fully_connected', envs[0], ckpt_name='model.pt')
