20aa80390f0ba9a39e06c196082d595c8bfde8a7
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-04 10:26:15 +0800

    weathervane sim: add 100 trials chemotaxis index result

--- examples/swimmer_weathervane/sim.py
+++ examples/swimmer_weathervane/sim.py
@@ -53,6 +53,7 @@ def done_func(index, result, **kwargs):
 if __name__ == '__main__':
     """ results
     100 trials: chemotaxis index mean 0.42 / 2500 steps
+    100 trials: chemotaxis index mean 0.57, start concentration mean 0.01 / 3500 steps
     """
     # set trials = 1, camera_name = 'track' or 'fixedcam', to record video
     trails = 100
