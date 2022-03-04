a272b177cb410d0ca463482549fb63f8c5323f87
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-04 21:05:52 +0800

    weathervane data: generate data with vector env

--- examples/swimmer_weathervane/data.py
+++ examples/swimmer_weathervane/data.py
@@ -1,12 +1,15 @@
 """ swimmer: chemotaxis based on weathervane mechanism """
 
+import gym
+import numpy as np
 import os
 import torch
 from sim import position_func, action_func
 from virtual_nematode.envs.swimmer import make_chemotaxis_swimmers
-from virtual_nematode.models.computational_model import ComputationalModelChemotaxis
+from virtual_nematode.models.computational_model import ComputationalModelChemotaxis, ComputationalModelChemotaxisVector
 from virtual_nematode.data.simulation import generate_dataset
 from virtual_nematode.data.concat import ConcatDataset
+from virtual_nematode.simulation import simulate_vector
 
 
 def x_func(observation, **kwargs):
@@ -19,18 +22,56 @@ def y_func(action, **kwargs):
 
 
 def data():
+    """ generate data with torch multiprocessing """
     envs = make_chemotaxis_swimmers(
         seed=seed, trials=trials, distance=15, position_func=position_func, n_bodies=25, joint_range='-100 100', body_len=0.1,
         max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale, camera_name=None, return_func=False
     )
     action_size = envs[0].action_space.shape[0]
-    model = ComputationalModelChemotaxis(dt=envs[0].dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., **kwargs)
+    model = ComputationalModelChemotaxis(dt=envs[0].dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., n_bias=25, **kwargs)
     dataset = []
     for i, env in enumerate(envs):
         print(i, env.source.tolist(), end=' ')
         d = generate_dataset(env, model, action_func, x_func, y_func, input_size, action_size, data_size_per_trial, max_episode_steps, seed, disable=True)
         dataset.append(d)
     dataset = ConcatDataset(dataset)
+    return dataset
+
+
+def step_func(observation, action, **kwargs):
+    """ data at each time step
+    x: (batch_size, *), angle and angular velocity of the rotors, concentration, g, g_p, g_w
+    y: (batch_size, action_size), action
+    """
+    x = torch.from_numpy(np.hstack([observation[:, 1:25], observation[:, 28:52], observation[:, 58:62]]))
+    y = torch.from_numpy(action)
+    return x, y
+
+
+def done_func(result, **kwargs):
+    """ data
+    x: (batch_size, max_episode_steps, *)
+    y: (batch_size, max_episode_steps, action_size)
+    """
+    x = torch.cat([rx for rx, _ in result], dim=1)
+    y = torch.cat([ry for _, ry in result], dim=1)
+    return x, y
+
+
+def data_vector():
+    """ generate data with gym.vector.AsyncVectorEnv multiprocessing """
+    env = make_chemotaxis_swimmers(
+        seed=seed, trials=trials, distance=15, position_func=position_func, n_bodies=25, joint_range='-100 100', body_len=0.1,
+        max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale, camera_name=None, return_func=True
+    )
+    env = env * data_size_per_trial
+    env = gym.vector.AsyncVectorEnv(env)
+    model = ComputationalModelChemotaxisVector(
+        dt=env.get_attr('dt')[0], seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., n_bias=25,
+        batch_size=env.num_envs, **kwargs
+    )
+    x, y = simulate_vector(env, model, action_func, step_func, done_func, seed=11, render=False)  # (batch_size, max_episode_steps, 1)
+    dataset = torch.utils.data.TensorDataset(x, y)
     return dataset
 
 
@@ -43,6 +84,7 @@ if __name__ == '__main__':
     reset_noise_scale = 1.745
     kwargs = {'backward': False, 'omega': False, 'weathervane': True, 'random_walk': False, 'weathervane_reverse': False}
     dataset = data()
+    # dataset = data_vector()
     print('dataset', len(dataset), dataset[0][0].size(), dataset[0][1].size())
     os.makedirs('data', exist_ok=True)
     torch.save(dataset, 'data/data.pt')
