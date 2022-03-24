f948bf246b97096cc9f346aa23656b4567d349ad
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-24 13:43:53 +0800

    swimmer weathervane train: units=100

--- scripts/swimmer_weathervane/train.py
+++ scripts/swimmer_weathervane/train.py
@@ -11,7 +11,7 @@ def train(model_name, data_name):
             'batch_size': 1024, 'seed': 11, 'cuda': 0, 'device_ids': [0, 1, 2, 3], 'lr': 0.001, 'epochs': 100,
             'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
             # model kwargs
-            'units': 50, 'output_dim': 24, 'in_features': 50
+            'units': 100, 'output_dim': 24, 'in_features': 50
         }
     elif model_name == 'ctrnn':
         torch.set_default_dtype(torch.float64)
@@ -19,7 +19,7 @@ def train(model_name, data_name):
             'data_name': data_name, 'model_name': model_name, 'lengths': [48000, 12000, 12000], 'batch_size': 1024, 'seed': 11,
             'cuda': 0, 'device_ids': [0], 'lr': 0.001, 'epochs': 100, 'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
             # model kwargs
-            'input_size': 50, 'hidden_size': 50, 'output_size': 24, 'feedback': True, 'readout': 'identity'
+            'input_size': 50, 'hidden_size': 100, 'output_size': 24, 'feedback': True, 'readout': 'identity'
         }
     else:
         raise AssertionError('{} not exist'.format(model_name))
