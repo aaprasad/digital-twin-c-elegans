cf1a77086a0eb734499702350f376bf2b6d7b8b3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-24 14:47:28 +0800

    swimmer weathervane sim: set seed

--- scripts/swimmer_weathervane/sim.py
+++ scripts/swimmer_weathervane/sim.py
@@ -6,10 +6,11 @@ from virtual_nematode.simulation import simulate
 
 
 if __name__ == '__main__':
+    seed = 11
     trials = 1
     camera_name = None  # set camera_name = 'track' or 'fixedcam', to record video
     env = make_chemotaxis_swimmers(
-        seed=11, trials=trials, distance=15, position_func=position_func, n_bodies=25, joint_range='-100 100', body_len=0.1,
+        seed=seed, trials=trials, distance=15, position_func=position_func, n_bodies=25, joint_range='-100 100', body_len=0.1,
         max_episode_steps=3500, reset_noise_scale=1.745, camera_name=camera_name, return_func=False
     )
     env = env[0]
@@ -17,6 +18,6 @@ if __name__ == '__main__':
     print(env.source)
     kwargs = {'backward': False, 'omega': False, 'weathervane': True, 'random_walk': False, 'weathervane_reverse': False}
     model = ComputationalModelChemotaxis(dt=env.dt, seed=None, n=25, q_max=20., a_max=1., psi=0.1, freq=2., n_bias=25, **kwargs)
-    result = simulate(env, model, action_func, step_func, done_func, seed=11, trials=trials, render=False)  # (batch_size, max_episode_steps, 1)
+    result = simulate(env, model, action_func, step_func, done_func, seed, trials=trials, render=False)  # (batch_size, max_episode_steps, 1)
     result = np.array(result)
     print('{} trials: chemotaxis index mean {:.2f}, start concentration mean {:.2f}'.format(result.shape[0], result.mean(), result[:, 0].mean()))
