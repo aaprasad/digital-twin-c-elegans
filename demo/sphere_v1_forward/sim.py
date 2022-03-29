06a937b791134fa22da16eaec04c05d41611fcd3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-29 18:00:19 +0800

    sphere-v1 forward: include x and y pos in observation, edit observation usage

--- demo/sphere_v1_forward/sim.py
+++ demo/sphere_v1_forward/sim.py
@@ -1,7 +1,7 @@
 import gym
 from gym_worm.envs.mujoco.camera import camera
 from gym_worm.envs.mujoco.position import position
-from gym_worm.envs.mujoco.sphere_v1 import swimmer
+from gym_worm.envs.mujoco.sphere_v0 import swimmer
 from gym_worm.wrappers.recorder import Recorder
 from gym_worm.wrappers.sensor_observation import SensorObservation
 import numpy as np
@@ -14,7 +14,10 @@ def make_swimmer(n_bodies=25, joint_range='-100 100', max_episode_steps=1000, re
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
     xml_str = position(xml_str)
     xml_str = camera(xml_str)
-    env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
+    env = gym.make(
+        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
+        reset_noise_scale=reset_noise_scale
+    )
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
     env = SensorObservation(env)
@@ -27,8 +30,8 @@ def action_func(model, step, observation, **kwargs):
     model: mathematical model of controller
     return: action
     """
-    q = observation[1:25]
-    q_vel = observation[28:52]
+    q = observation[3:27]
+    q_vel = observation[30:54]
     action = model.step(step, q, q_vel)
     return action
 
@@ -37,7 +40,7 @@ def step_func(observation, **kwargs):
     """
     return: 2D center of mass
     """
-    com = observation[52:54]
+    com = observation[54:56]
     return com
 
 
