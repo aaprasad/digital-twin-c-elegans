affaa1bd5223c73b4ea935948fc616ce069184e7
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-04-07 20:00:15 +0800

    swimmer weathervane: train with ctrnn_2_stage

--- scripts/swimmer_weathervane/train.py
+++ scripts/swimmer_weathervane/train.py
@@ -21,6 +21,17 @@ def train(model_name, data_name):
             # model kwargs
             'input_size': 50, 'hidden_size': 100, 'output_size': 24, 'feedback': True, 'readout': 'identity'
         }
+    elif model_name == 'ctrnn_2_stage':
+        torch.set_default_dtype(torch.float64)
+        kwargs = {
+            'data_name': data_name, 'model_name': model_name, 'lengths': [48000, 12000, 12000], 'batch_size': 1024, 'seed': 11,
+            'cuda': 0, 'device_ids': [0], 'lr': 0.001, 'epochs': 100, 'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
+            # model kwargs
+            'input_size': 2, 'hidden_size': 50, 'output_size': 48, 'feedback': True, 'readout': 'identity',
+            'load_kwargs': {'input_size': 48, 'hidden_size': 50, 'output_size': 24, 'feedback': True, 'readout': 'identity'},
+            'load_path': '/home/imc/disk1/virtual-nematode/scripts/swimmer_forward/runs/Dec16_16-02-23_h-10-176-50-34/model47.pt',
+            'input1_range': (48, 50), 'input2_range': (0, 48)
+        }
     else:
         raise AssertionError('{} not exist'.format(model_name))
     train_eval_test(**kwargs)
@@ -28,7 +39,7 @@ def train(model_name, data_name):
 
 if __name__ == '__main__':
     """ experiments
-    model_name: 'fully_connected', 'ctrnn'
+    model_name: 'fully_connected', 'ctrnn', 'ctrnn_2_stage'
     data_name: 'steering.pt', 'steering_and_roaming.pt', 'full.pt'
     """
-    train(model_name='fully_connected', data_name='steering.pt')
+    train(model_name='ctrnn_2_stage', data_name='steering.pt')
