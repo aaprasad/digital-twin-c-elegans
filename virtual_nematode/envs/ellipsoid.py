f3d195ae8dc27711cef735efe6d5f22a6a4889e7
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-04-12 10:23:22 +0800

    make ellipsoid swimmer with servo: remove body_len

--- virtual_nematode/envs/ellipsoid.py
+++ virtual_nematode/envs/ellipsoid.py
@@ -8,12 +8,15 @@ from gym_worm.wrappers.sensor_observation import SensorObservation
 from gym_worm.wrappers.skip_frame import SkipFrame
 
 
-def make_swimmer_with_servo(n_bodies, joint_range, body_len, max_episode_steps, reset_noise_scale, kp=1, skip=10):
-    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, body_len)
+def make_swimmer_with_servo(n_bodies, joint_range, max_episode_steps, reset_noise_scale, kp=1, skip=10):
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
     xml_str = position_actuator(xml_str, joint_range, kp)
     xml_str = position(xml_str)
     xml_str = camera(xml_str)
-    env = gym.make('Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), reset_noise_scale=reset_noise_scale)
+    env = gym.make(
+        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
+        reset_noise_scale=reset_noise_scale
+    )
     env = SkipFrame(env, skip)
     env = gym.wrappers.TimeLimit(env, max_episode_steps)
     env = gym.wrappers.ClipAction(env)
