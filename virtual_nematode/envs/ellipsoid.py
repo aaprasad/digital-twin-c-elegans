fdde9ba2431894d8e9759e2b05c87d53fa47263e
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-04-22 12:04:32 +0800

    ellipsoid-v1: make swimmer with motor actuators

--- virtual_nematode/envs/ellipsoid.py
+++ virtual_nematode/envs/ellipsoid.py
@@ -14,6 +14,21 @@ from gym_worm.wrappers.sensor_observation import SensorObservation
 from gym_worm.wrappers.skip_frame import SkipFrame
 
 
+def make_swimmer(n_bodies, joint_range, max_episode_steps, reset_noise_scale):
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
+    xml_str = position(xml_str)
+    xml_str = camera(xml_str)
+    env = gym.make(
+        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
+        reset_noise_scale=reset_noise_scale
+    )
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = SensorObservation(env)
+    env = Recorder(env, camera_name=None)
+    return env
+
+
 def make_swimmer_with_servo(n_bodies, joint_range, max_episode_steps, reset_noise_scale, kp=1, skip=10):
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
     xml_str = position_actuator(xml_str, joint_range, kp, names=['rot{}y'.format(i) for i in range(2, n_bodies + 1)])
