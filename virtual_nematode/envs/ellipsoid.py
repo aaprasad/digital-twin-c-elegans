a6e00f31831408830785b2ed6291b4d09a983689
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-04-22 13:34:35 +0800

    ellipsoid: remove model with only z servo

--- virtual_nematode/envs/ellipsoid.py
+++ virtual_nematode/envs/ellipsoid.py
@@ -1,9 +1,3 @@
-"""
-action space: Box(-100.0, 100.0, (48,), float32)
-    [0:24]: angles applied on the position servos around z-axis (angle, rad)
-    [24:48]: angles applied on the position servos around y-axis (angle, rad)
-"""
-
 import gym
 from gym_worm.envs.mujoco.camera import camera
 from gym_worm.envs.mujoco.ellipsoid_v1 import swimmer
@@ -29,28 +23,12 @@ def make_swimmer(n_bodies, joint_range, max_episode_steps, reset_noise_scale):
     return env
 
 
-def make_swimmer_with_z_servo(n_bodies, joint_range, max_episode_steps, reset_noise_scale, kp=1, skip=10):
-    """ only z-axis position servos """
-    xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
-    xml_str = position_actuator(xml_str, joint_range, kp)
-    xml_str = position(xml_str)
-    xml_str = camera(xml_str)
-    # with open('swimmer.xml', 'w') as f:
-    #     f.write(xml_str.decode('utf-8'))
-    env = gym.make(
-        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
-        reset_noise_scale=reset_noise_scale
-    )
-    env = SkipFrame(env, skip)
-    env = gym.wrappers.TimeLimit(env, max_episode_steps)
-    env = gym.wrappers.ClipAction(env)
-    env = SensorObservation(env)
-    env = Recorder(env, camera_name=None)
-    return env
-
-
 def make_swimmer_with_servo(n_bodies, joint_range, max_episode_steps, reset_noise_scale, kp=1, skip=10):
-    """ z-axis and y-axis position servos """
+    """ z-axis and y-axis position servos
+    action space: Box(-100.0, 100.0, (48,), float32)
+        [0:24]: angles applied on the position servos around z-axis (angle, rad)
+        [24:48]: angles applied on the position servos around y-axis (angle, rad)
+    """
     xml_str = swimmer('swimmer.xml', n_bodies, joint_range)
     xml_str = position_actuator(xml_str, joint_range, kp, names=['rot{}y'.format(i) for i in range(2, n_bodies + 1)])
     xml_str = position(xml_str)
