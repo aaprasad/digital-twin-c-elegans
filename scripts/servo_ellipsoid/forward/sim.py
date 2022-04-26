8e5f0e4b5950eae920a458ccc288f02db97f2c82
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-04-26 16:19:52 +0800

    ellipsoid: set anisotropic friction='0.1 1'

--- scripts/servo_ellipsoid/forward/sim.py
+++ scripts/servo_ellipsoid/forward/sim.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     max_episode_steps = 2500
     env = make_swimmer_with_servo(
         n_bodies=25, joint_range='-100 100', max_episode_steps=max_episode_steps, reset_noise_scale=0.,
-        friction='1 1', kp=10, skip=1
+        friction='0.1 1', kp=10, skip=1
     )
     # env = gym.wrappers.Monitor(env, directory='video/swimmer', force=True)
     print(env.action_space)
