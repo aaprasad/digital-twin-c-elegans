be1cda8055c901cb6a7e03e4278b2a10d3c70539
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-05-12 15:51:03 +0800

    servo ellipsoid forward: add y_joint_ranges option

--- scripts/servo_ellipsoid/forward/sim.py
+++ scripts/servo_ellipsoid/forward/sim.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     max_episode_steps = 2500  # 0.04s/step, 100s in total
     # condim=1: no friction; condim=3: tangential and normal sliding friction
     env = make_swimmer_with_servo(
-        n_bodies=25, joint_range='-100 100', max_episode_steps=max_episode_steps, reset_noise_scale=0.,
-        density=4000, viscosity=0.1, condim=3, friction='0.1 1', kp=1, skip=1
+        n_bodies=25, joint_range='-100 100', y_joint_ranges=['-10 10'] * 24, max_episode_steps=max_episode_steps,
+        reset_noise_scale=0., density=4000, viscosity=0.1, condim=3, friction='0.1 1', kp=1, skip=1
     )
     # env = gym.wrappers.Monitor(env, directory='video/swimmer', force=True)
     print(env.action_space)
