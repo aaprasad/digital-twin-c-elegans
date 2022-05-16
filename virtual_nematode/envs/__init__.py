b081555c15e84772b12036db3a7597c817e6a3e3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-05-16 15:55:41 +0800

    ellipsoid2d: make motor swimmer and servo swimmer

--- virtual_nematode/envs/__init__.py
+++ virtual_nematode/envs/__init__.py
@@ -1,4 +1,5 @@
 import virtual_nematode.envs.muscle_worm
 import virtual_nematode.envs.ellipsoid
+import virtual_nematode.envs.ellipsoid2d
 import virtual_nematode.envs.muscle_ellipsoid
 import virtual_nematode.envs.swimmer
---
+++ virtual_nematode/envs/ellipsoid2d.py
@@ -0,0 +1,44 @@
+import gym
+from gym_worm.envs.mujoco.camera import camera
+from gym_worm.envs.mujoco.ellipsoid2d_v0 import swimmer
+from gym_worm.envs.mujoco.position import position
+from gym_worm.envs.mujoco.position_actuator import position_actuator
+from gym_worm.wrappers.recorder import Recorder
+from gym_worm.wrappers.sensor_observation import SensorObservation
+from gym_worm.wrappers.skip_frame import SkipFrame
+
+
+def make_swimmer(n_bodies, joint_range, max_episode_steps, reset_noise_scale, density, viscosity, condim, friction):
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, density, viscosity, condim, friction)
+    xml_str = position(xml_str)
+    xml_str = camera(xml_str)
+    # with open('swimmer.xml', 'w') as f:
+    #     f.write(xml_str.decode('utf-8'))
+    env = gym.make(
+        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
+        reset_noise_scale=reset_noise_scale
+    )
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = SensorObservation(env)
+    env = Recorder(env, camera_name=None)
+    return env
+
+
+def make_servo_swimmer(n_bodies, joint_range, max_episode_steps, reset_noise_scale, density, viscosity, condim, friction, kp=1, skip=1):
+    xml_str = swimmer('swimmer.xml', n_bodies, joint_range, density, viscosity, condim, friction)
+    xml_str = position_actuator(xml_str, joint_range, kp)
+    xml_str = position(xml_str)
+    xml_str = camera(xml_str)
+    # with open('swimmer.xml', 'w') as f:
+    #     f.write(xml_str.decode('utf-8'))
+    env = gym.make(
+        'Swimmer-v3-v0', xml_str=xml_str.decode('utf-8'), exclude_current_positions_from_observation=False,
+        reset_noise_scale=reset_noise_scale
+    )
+    env = SkipFrame(env, skip)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    env = gym.wrappers.ClipAction(env)
+    env = SensorObservation(env)
+    env = Recorder(env, camera_name=None)
+    return env
