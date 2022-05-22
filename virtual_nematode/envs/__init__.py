c5641f594e1615d1e3e25a7f0a4253f3b654bb96
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-05-22 17:22:18 +0800

    muscle ellipsoid2d forward: create gym env

--- virtual_nematode/envs/__init__.py
+++ virtual_nematode/envs/__init__.py
@@ -2,4 +2,5 @@ import virtual_nematode.envs.muscle_worm
 import virtual_nematode.envs.ellipsoid
 import virtual_nematode.envs.ellipsoid2d
 import virtual_nematode.envs.muscle_ellipsoid
+import virtual_nematode.envs.muscle_ellipsoid2d
 import virtual_nematode.envs.swimmer
