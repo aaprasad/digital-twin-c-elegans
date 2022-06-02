433b0db91a0ca8308a7ee47f55d6b9a708caccba
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-02 13:56:50 +0800

    muscle ellipsoid 2d forward: output 100s and last 10s simulation com displacement

--- scripts/muscle_ellipsoid2d/forward/sim.py
+++ scripts/muscle_ellipsoid2d/forward/sim.py
@@ -36,7 +36,7 @@ def done_func(index, result, **kwargs):
     displacement = np.linalg.norm(np.array(result[-1]) - np.array(result[0]), ord=2)
     d = np.linalg.norm(np.array(result[-1]) - np.array(result[-250]), ord=2)
     print('Trial {}: com displacement {:.2f}, last 250 steps {:.2f}'.format(index + 1, displacement, d))
-    return displacement
+    return displacement, d
 
 
 if __name__ == '__main__':
@@ -55,4 +55,6 @@ if __name__ == '__main__':
         kp=1, kd=np.array([0.15 + i * 0.002 for i in range(24)])
     )
     results = simulate(env, model, action_func, step_func, done_func, seed=None, trials=1, render=False)
-    print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(results), np.mean(results), max_episode_steps))
+    print('{} trials: com displacement mean {:.2f} / {} steps, {:.2f} / last 250 steps'.format(
+        len(results), np.mean([a for a, b in results]), max_episode_steps, np.mean([b for a, b in results])
+    ))
