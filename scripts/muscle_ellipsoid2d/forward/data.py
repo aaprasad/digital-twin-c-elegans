cd826808fc792e70be8372f0d358c00f4dd0ec94
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-06 11:33:54 +0800

    muscle ellipsoid 2d forward: generate dataset

---
+++ scripts/muscle_ellipsoid2d/forward/data.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+from sim import action_func
+import torch
+from virtual_nematode.data.simulation import generate_dataset
+from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer
+from virtual_nematode.models.muscle import ForwardPIDMuscle
+
+
+def x_func(observation, **kwargs):
+    return observation[4:28]  # joint angles
+
+
+def y_func(action, **kwargs):
+    return action.tolist()
+
+
+if __name__ == '__main__':
+    input_size = 24  # joint angles
+    data_size = 6000
+    max_episode_steps = 300
+    reset_noise_scale = 0.7
+    seed = 7
+    env = make_swimmer(
+        n_bodies=25, joint_range='-90 90', max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale,
+        density=1.2, viscosity=0.1, condim=3, friction='1 1'
+    )
+    action_size = env.action_space.shape[0]
+    print(env.action_space)
+    print(env.observation_space)
+    model = ForwardPIDMuscle(
+        dt=env.dt, n=25, a=40 * np.pi / 180, freq=0.8, psi=0.07,
+        kp=1, kd=np.array([0.15 + i * 0.002 for i in range(24)])
+    )
+    dataset = generate_dataset(env, model, action_func, x_func, y_func, input_size, action_size, data_size, max_episode_steps, seed)
+    os.makedirs('data', exist_ok=True)
+    torch.save(dataset, 'data/data.pt')
