4b8fbeaeb8d3a24d44c150befae60e2a6d0ba9f1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-11 11:41:57 +0800

    muscle ellipsoid 2d forward: train snn with longer seq

--- scripts/muscle_ellipsoid2d/forward/train.py
+++ scripts/muscle_ellipsoid2d/forward/train.py
@@ -19,7 +19,9 @@ def train():
     p = 24
     mask_c, mask_g, ex_mask_c, in_mask_c, mask_output = connectome.mask()
     mask_p = connectome.proprioception_mask(p)
-    # trainer
+    """ trainer kwargs
+    longer seq: data_name='data320.pt', lengths=[5000, 1000, 1000], batch_size=256, cuda=0, device_ids=[0, 1]
+    """
     kwargs = {
         'data_name': 'data32.pt', 'model_name': 'snn_forward', 'lengths': [50000, 10000, 10000], 'batch_size': 1024, 'seed': 11,
         'cuda': 0, 'device_ids': [0], 'lr': 0.001, 'epochs': 100, 'early_stop': 30, 'comment': '', 'loss': 'MSELoss',
