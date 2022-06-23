3f547ccf977e85bb8ab58b6a55d7ce39946980e1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-23 11:16:22 +0800

    muscle ellipsoid 2d forward: plot model weight

--- scripts/muscle_ellipsoid2d/forward/eval.py
+++ scripts/muscle_ellipsoid2d/forward/eval.py
@@ -1,8 +1,6 @@
 from data import x_func as data_func
 import gym
-from matplotlib import pyplot as plt
 import os
-import seaborn as sns
 from sim import step_func as x_func
 import sys
 import torch
@@ -14,59 +12,7 @@ from virtual_nematode.trainers.ncp import prepare_model
 import worm_assets
 
 
-def plot_weight(model, ckpt_name):
-    plt.figure(figsize=(20, 10))
-    # chemical weight
-    w_c = (
-        model.cell.w_c.abs() * model.cell.ex_mask_c -
-        model.cell.w_c.abs() * model.cell.in_mask_c +
-        model.cell.w_c * (
-            model.cell.mask_c.float() -
-            model.cell.ex_mask_c.float() -
-            model.cell.in_mask_c.float()
-        )
-    )
-    w_c = w_c.clone().detach()
-    w_c_max = w_c.abs().max().item()
-    plt.subplot(2, 3, 1)
-    plt.title('chemical weight')
-    sns.heatmap(w_c, cmap='coolwarm', vmin=-w_c_max, vmax=w_c_max)
-    # proprioception input weight
-    w_p = model.cell.w_p * model.cell.mask_p
-    w_p = w_p.clone().detach()
-    w_p_max = w_p.abs().max().item()
-    plt.subplot(2, 3, 2)
-    plt.title('proprioception input weight')
-    sns.heatmap(w_p, cmap='coolwarm', vmin=-w_p_max, vmax=w_p_max)
-    # tau
-    tau = model.cell.tau.clamp(0.01, 0.05)
-    tau = tau.clone().detach()
-    plt.subplot(2, 3, 3)
-    plt.title('tau')
-    plt.plot(tau)
-    # gap junction weight
-    w_g = model.cell.w_g.abs()
-    w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * model.cell.mask_g
-    w_g = w_g.clone().detach()
-    w_g_max = w_g.max().item()
-    plt.subplot(2, 3, 4)
-    plt.title('gap junction weight')
-    sns.heatmap(w_g, cmap='coolwarm', vmin=0, vmax=w_g_max)
-    # output scaling weight
-    w_output = model.cell.w_output.clamp(0, 1)
-    w_output = w_output.clone().detach()
-    plt.subplot(2, 3, 5)
-    plt.title('output scaling weight')
-    plt.plot(w_output)
-    # bias
-    bias = model.cell.bias.clone().detach()
-    plt.subplot(2, 3, 6)
-    plt.title('bias')
-    plt.plot(bias)
-    plt.savefig(os.path.join(data_path, '{}.png'.format(ckpt_name)))
-
-
-def select_model(model_name, ckpt_name):
+def select_model(model_folder, model_name, ckpt_name):
     if model_name == 'snn_forward':
         torch.set_default_dtype(torch.float64)
         # connectome
@@ -99,20 +45,13 @@ def select_model(model_name, ckpt_name):
     else:
         raise AssertionError('{} not exist'.format(model_name))
     model = prepare_model(model_name, model_path=os.path.join(model_folder, ckpt_name), **kwargs)
-    plot_weight(model, ckpt_name)
-    # print(torch.all(mask_c == model.cell.mask_c))
-    # print(torch.all(mask_g == model.cell.mask_g))
-    # print(torch.all(ex_mask_c == model.cell.ex_mask_c))
-    # print(torch.all(in_mask_c == model.cell.in_mask_c))
-    # print(torch.all(mask_output == model.cell.mask_output))
-    # print(torch.all(mask_p == model.cell.mask_p))
     return model
 
 
-def evaluate(model_name, start, end):
+def evaluate(model_folder, model_name, start, end):
     for i in range(start, end):
         ckpt_name = 'model{}.pt'.format(i)
-        model = select_model(model_name, ckpt_name)
+        model = select_model(model_folder, model_name, ckpt_name)
         print(ckpt_name, end=' ')
         if model_name == 'snn_forward':
             test_func = test_func2
@@ -124,12 +63,12 @@ def evaluate(model_name, start, end):
         torch.save(y, os.path.join(data_path, ckpt_name))  # action sequence
 
 
-def test(model_name, start, end):
+def test(model_folder, model_name, start, end):
     """ online test multiple trials for testing """
     for i in range(start, end):
         ckpt_name = 'model{}.pt'.format(i)
         print(ckpt_name, end=' ')
-        model = select_model(model_name, ckpt_name)
+        model = select_model(model_folder, model_name, ckpt_name)
         if model_name == 'snn_forward':
             test_func = test_func2
         elif model_name == 'ctrnn':
@@ -139,9 +78,9 @@ def test(model_name, start, end):
         tester(env, model, data_func, x_func, seed, max_episode_steps, model_folder, model_name, data_size=100, disable=True, test_func=test_func)
 
 
-def record(model_name, env, ckpt_name):
+def record(model_folder, model_name, env, ckpt_name):
     """ online test once for evaluation and record video """
-    model = select_model(model_name, ckpt_name)
+    model = select_model(model_folder, model_name, ckpt_name)
     env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder, ckpt_name), force=True)
     if model_name == 'snn_forward':
         test_func = test_func2
@@ -166,6 +105,6 @@ if __name__ == '__main__':
         n_bodies=25, joint_range='-90 90', max_episode_steps=max_episode_steps, reset_noise_scale=0.6,
         density=1.2, viscosity=0.1, condim=3, friction='1 1'
     )
-    evaluate('snn_forward', start=0, end=100)
-    # test('snn_forward', start=0, end=100)
-    # record('snn_forward', env, ckpt_name='model.pt')
+    evaluate(model_folder, 'snn_forward', start=0, end=100)
+    # test(model_folder, 'snn_forward', start=0, end=100)
+    # record(model_folder, 'snn_forward', env, ckpt_name='model.pt')
---
+++ scripts/muscle_ellipsoid2d/forward/plot.py
@@ -0,0 +1,66 @@
+from eval import select_model
+from matplotlib import pyplot as plt
+import os
+import seaborn as sns
+
+
+def plot_model_weight(model, ckpt_name):
+    plt.figure(figsize=(20, 10))
+    # chemical weight
+    w_c = (
+        model.cell.w_c.abs() * model.cell.ex_mask_c -
+        model.cell.w_c.abs() * model.cell.in_mask_c +
+        model.cell.w_c * (
+            model.cell.mask_c.float() -
+            model.cell.ex_mask_c.float() -
+            model.cell.in_mask_c.float()
+        )
+    )
+    w_c = w_c.clone().detach()
+    w_c_max = w_c.abs().max().item()
+    plt.subplot(2, 3, 1)
+    plt.title('chemical weight')
+    sns.heatmap(w_c, cmap='coolwarm', vmin=-w_c_max, vmax=w_c_max)
+    # proprioception input weight
+    w_p = model.cell.w_p * model.cell.mask_p
+    w_p = w_p.clone().detach()
+    w_p_max = w_p.abs().max().item()
+    plt.subplot(2, 3, 2)
+    plt.title('proprioception input weight')
+    sns.heatmap(w_p, cmap='coolwarm', vmin=-w_p_max, vmax=w_p_max)
+    # tau
+    tau = model.cell.tau.clamp(0.01, 0.05)
+    tau = tau.clone().detach()
+    plt.subplot(2, 3, 3)
+    plt.title('tau')
+    plt.plot(tau)
+    # gap junction weight
+    w_g = model.cell.w_g.abs()
+    w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * model.cell.mask_g
+    w_g = w_g.clone().detach()
+    w_g_max = w_g.max().item()
+    plt.subplot(2, 3, 4)
+    plt.title('gap junction weight')
+    sns.heatmap(w_g, cmap='coolwarm', vmin=0, vmax=w_g_max)
+    # output scaling weight
+    w_output = model.cell.w_output.clamp(0, 1)
+    w_output = w_output.clone().detach()
+    plt.subplot(2, 3, 5)
+    plt.title('output scaling weight')
+    plt.plot(w_output)
+    # bias
+    bias = model.cell.bias.clone().detach()
+    plt.subplot(2, 3, 6)
+    plt.title('bias')
+    plt.plot(bias)
+    plt.savefig(os.path.join(data_path, '{}.png'.format(ckpt_name)))
+
+
+if __name__ == '__main__':
+    runs_folder = ''
+    ckpt_name = 'model.pt'
+    model_name = 'snn_forward'
+    model_folder = os.path.join('runs', runs_folder)
+    data_path = os.path.join('data', runs_folder)
+    model = select_model(model_folder, 'snn_forward', ckpt_name)
+    plot_model_weight(model, ckpt_name)
