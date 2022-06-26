26f9f442765d8c3ebeec7fc28e16469d8bb8c6e7
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-26 10:21:59 +0800

    muscle ellipsoid 2d forwrad: rename dataset data_new_640.pt->data_7000_640.pt

--- scripts/muscle_ellipsoid2d/forward/data.py
+++ scripts/muscle_ellipsoid2d/forward/data.py
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     )
     dataset = generate_dataset(env, model, action_func, x_func, y_func, input_size, action_size, data_size, max_episode_steps, seed)
     os.makedirs('data', exist_ok=True)
-    torch.save(dataset, 'data/data_new_640.pt')
+    torch.save(dataset, 'data/data_7000_640.pt')
