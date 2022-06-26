959feb296a6861e453385f3194385d15e9fb09fd
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-06-26 17:46:26 +0800

    muscle ellipsoid 2d forward: split dataset into train/eval/test tensordataset

--- scripts/muscle_ellipsoid2d/forward/preprocess.py
+++ scripts/muscle_ellipsoid2d/forward/preprocess.py
@@ -90,9 +90,9 @@ if __name__ == '__main__':
     preprocess_and_split_dataset(
         load_name='data_7000_640.pt',
         save_names=[
-            'data_5000_640_64_train.pt',
-            'data_1000_640_64_eval.pt',
-            'data_1000_640_64_test.pt'
+            'data_7000_5000_640_64_train.pt',
+            'data_7000_1000_640_64_eval.pt',
+            'data_7000_1000_640_64_test.pt'
         ],
         lengths=[5000, 1000, 1000], seq_len=64, seed=33
     )
