c9a95de5d276f7321d37dbed016c42e67297c6b9
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-12 00:44:07 +0800

    weathervane: train snn with forward network model state dict

--- scripts/muscle_ellipsoid2d/weathervane/train.py
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -26,6 +26,7 @@ def train(model_name):
         """ trainer kwargs """
         data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
         device_ids, batch_size, lr = [0, 1, 2, 3], 128, 5e-2
+        model_path = '/home/imc/disk1/virtual-nematode/scripts/muscle_ellipsoid2d/forward/runs/Jul05_22-30-02_h-10-176-50-34/model125.pt'
         kwargs = {
             'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,
             'device_ids': device_ids, 'lr': lr, 'epochs': 300, 'early_stop': 300, 'loss': 'MSELoss',
@@ -33,7 +34,8 @@ def train(model_name):
             'dt': dt, 'steps': 5, 'n': n, 'm': m, 'p': p,
             'w_c_mask': w_c_mask, 'w_g_mask': w_g_mask, 'w_p_mask': w_p_mask, 'output_index': output_index,
             # 'w_c_ex_mask': w_c_ex_mask, 'w_c_in_mask': w_c_in_mask
-            'gradient_size': gradient_size, 'w_gradient_mask': w_gradient_mask
+            'gradient_size': gradient_size, 'w_gradient_mask': w_gradient_mask,
+            'model_path': model_path
         }
     else:
         raise AssertionError('{} not exist'.format(model_name))
