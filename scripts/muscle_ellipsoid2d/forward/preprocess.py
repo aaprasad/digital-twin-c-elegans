2285171026bbd0e4a45509dcca00ccafbc5a8d84
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-11 16:56:42 +0800

    forward: organize data preprocessing code

--- scripts/muscle_ellipsoid2d/forward/preprocess.py
+++ scripts/muscle_ellipsoid2d/forward/preprocess.py
@@ -4,7 +4,7 @@ import torch
 from virtual_nematode.data.clamp import ClampDataset
 from virtual_nematode.data.float import FloatDataset
 from virtual_nematode.data.split import SplitDataset
-from virtual_nematode.data.subset import random_split, Subset
+from virtual_nematode.data.utils import preprocess_and_split_dataset
 
 
 def plot_data(save_name):
@@ -42,41 +42,6 @@ def preprocess_dataset(seq_len, load_name, save_name, float_type=False):
     if float_type is True:
         dataset = FloatDataset(dataset)
     torch.save(dataset, os.path.join('data', save_name))
-
-
-def preprocess_and_split_dataset(load_name, save_names, lengths, seq_len, seed):
-    # load
-    dataset = torch.load(os.path.join('data', load_name))
-    print('load', dataset.tensors[0].shape, dataset.tensors[1].shape)
-    # subset
-    dataset = Subset(dataset, sum(lengths))
-    print('subset', dataset.tensors[0].shape, dataset.tensors[1].shape)
-    # float
-    dataset = FloatDataset(dataset)
-    print('dtype', dataset.tensors[0].dtype, dataset.tensors[1].dtype)
-    # clamp
-    dataset = ClampDataset(dataset, x_range=None, y_range=[0, 1])
-    print(
-        'clamp',
-        'x range', dataset.tensors[0].min().item(), dataset.tensors[0].max().item(),
-        'y range', dataset.tensors[1].min().item(), dataset.tensors[1].max().item()
-    )
-    # split train, eval, test
-    train_data, eval_data, test_data = random_split(dataset, lengths, seed)
-    print('train', train_data.tensors[0].shape, train_data.tensors[1].shape)
-    print('eval', eval_data.tensors[0].shape, eval_data.tensors[1].shape)
-    print('test', test_data.tensors[0].shape, test_data.tensors[1].shape)
-    # split sequence
-    train_data = SplitDataset(train_data, seq_len)
-    print('split sequence train', train_data.tensors[0].shape, train_data.tensors[1].shape)
-    eval_data = SplitDataset(eval_data, seq_len)
-    print('split sequence eval', eval_data.tensors[0].shape, eval_data.tensors[1].shape)
-    test_data = SplitDataset(test_data, seq_len)
-    print('split sequence test', test_data.tensors[0].shape, test_data.tensors[1].shape)
-    # save
-    torch.save(train_data, os.path.join('data', save_names[0]))
-    torch.save(eval_data, os.path.join('data', save_names[1]))
-    torch.save(test_data, os.path.join('data', save_names[2]))
 
 
 if __name__ == '__main__':
