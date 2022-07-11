0255d6af30f94347eb4664f78d282725f028d962
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-12 00:38:59 +0800

    weathervane: training code

---
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -0,0 +1,44 @@
+from virtual_nematode.connectomes.forward import body_wall_muscles, neuron_list2
+from virtual_nematode.networks.snn.weathervane import Connectome
+from virtual_nematode.trainers.ncp import train_eval_test
+import worm_assets
+
+
+def train(model_name):
+    if model_name == 'snn_weathervane':
+        """ connectome """
+        path = worm_assets.connectome_path(filename='SI 5 Connectome adjacency matrices, corrected July 2020.xlsx')
+        muscles = body_wall_muscles()
+        neurons = neuron_list2(path, muscles)
+        sensory_neurons = ['ASEL', 'ASER']
+        print('{} neurons, {} muscles, {} cells in total'.format(len(neurons), len(muscles), len(neurons) + len(muscles)))
+        p = 24
+        gradient_size = 1
+        connectome = Connectome(
+            sensory_neurons, gradient_size, gradient_mask=False, neurons=neurons, muscles=muscles, ex_synapses=[],
+            in_synapses=[], path=path, p=p, p_mask=True, polarity_mask=False
+        )
+        (w_c_mask, w_g_mask, w_c_ex_mask, w_c_in_mask, w_p_mask, output_index), w_gradient_mask = connectome.mask()
+        """ params """
+        dt = 0.04
+        n = len(connectome.cells)
+        m = len(connectome.muscles)
+        """ trainer kwargs """
+        data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
+        device_ids, batch_size, lr = [0, 1, 2, 3], 128, 5e-2
+        kwargs = {
+            'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,
+            'device_ids': device_ids, 'lr': lr, 'epochs': 300, 'early_stop': 300, 'loss': 'MSELoss',
+            # model kwargs
+            'dt': dt, 'steps': 5, 'n': n, 'm': m, 'p': p,
+            'w_c_mask': w_c_mask, 'w_g_mask': w_g_mask, 'w_p_mask': w_p_mask, 'output_index': output_index,
+            # 'w_c_ex_mask': w_c_ex_mask, 'w_c_in_mask': w_c_in_mask
+            'gradient_size': gradient_size, 'w_gradient_mask': w_gradient_mask
+        }
+    else:
+        raise AssertionError('{} not exist'.format(model_name))
+    train_eval_test(**kwargs)
+
+
+if __name__ == '__main__':
+    train('snn_weathervane')
