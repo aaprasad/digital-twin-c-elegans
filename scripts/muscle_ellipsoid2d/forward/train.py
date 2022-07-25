d30bf2948e19a3cc9bedb95733637efa216b14dd
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-25 11:50:36 +0800

    forward: clean code

--- scripts/muscle_ellipsoid2d/forward/train.py
+++ scripts/muscle_ellipsoid2d/forward/train.py
@@ -33,7 +33,7 @@ def plot_mask(w_c_mask, w_g_mask, w_c_ex_mask, w_c_in_mask, w_p_mask):
 
 
 def train(model_name):
-    if model_name == 'snn_forward' or model_name == 'snn1_forward' or model_name == 'snn2_forward':
+    if model_name in ['snn_forward', 'snn1_forward', 'snn2_forward']:
         """ connectome: cells and synapse polarity """
         path = worm_assets.connectome_path(filename='SI 5 Connectome adjacency matrices, corrected July 2020.xlsx')
         muscles = body_wall_muscles()
