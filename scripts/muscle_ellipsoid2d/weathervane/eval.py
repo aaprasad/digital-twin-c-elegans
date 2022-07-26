c2c3e410fa5ce5c9c748f2fdcb319f8641220682
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-26 23:06:10 +0800

    weathervane: clean code

--- scripts/muscle_ellipsoid2d/weathervane/eval.py
+++ scripts/muscle_ellipsoid2d/weathervane/eval.py
@@ -20,9 +20,8 @@ def select_model(model_folder, model_name, ckpt_name):
         path = worm_assets.connectome_path(filename='SI 5 Connectome adjacency matrices, corrected July 2020.xlsx')
         muscles = body_wall_muscles()
         neurons = neuron_list2(path, muscles)
-        # sensory = ['ASEL', 'ASER']
         sensory = sensory_neurons(path)
-        print('{} neurons, {} muscles, {} cells in total'.format(len(neurons), len(muscles), len(neurons) + len(muscles)))
+        print('{} neurons, {} muscles, {} sensory, {} cells in total'.format(len(neurons), len(muscles), len(sensory), len(neurons) + len(muscles)))
         p = 24
         gradient_size = 1
         connectome = Connectome(
@@ -39,7 +38,7 @@ def select_model(model_folder, model_name, ckpt_name):
         kwargs = {
             'dt': dt, 'steps': 5, 'n': n, 'm': m, 'p': p,
             'w_c_mask': w_c_mask, 'w_g_mask': w_g_mask, 'w_p_mask': w_p_mask, 'output_index': output_index,
-            # 'w_c_ex_mask': w_c_ex_mask, 'w_c_in_mask': w_c_in_mask
+            # 'w_c_ex_mask': w_c_ex_mask, 'w_c_in_mask': w_c_in_mask,
             'gradient_size': gradient_size, 'w_gradient_mask': w_gradient_mask,
         }
     else:
