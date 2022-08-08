f86e2dddef0dd157e6a4e77664e144eb9991753a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-08-08 16:08:44 +0800

    weathervane: update snncell model with forward's new snncell model, rename snncell

--- virtual_nematode/networks/snn/weathervane.py
+++ virtual_nematode/networks/snn/weathervane.py
@@ -1,6 +1,6 @@
 import torch
 from virtual_nematode.networks.snn.forward import SNNCell as _SNNCell
-from virtual_nematode.networks.snn.forward import SNNCell2 as _SNNCell2
+from virtual_nematode.networks.snn.forward import SNNCell3 as _SNNCell3
 
 
 class SNNCell(_SNNCell):
@@ -26,16 +26,16 @@ class SNNCell(_SNNCell):
         return external_input
 
 
-class SNNCell2(_SNNCell2):
+class SNNCell1(_SNNCell3):
     def __init__(self, gradient_size, w_gradient_mask, **kwargs):
-        super(SNNCell2, self).__init__(**kwargs)
+        super(SNNCell1, self).__init__(**kwargs)
         self.gradient_size = gradient_size
-        self.w_gradient = torch.nn.Parameter(torch.zeros((gradient_size, self.n)).uniform_(-1, 1))
-        self.w_gradient_mask = torch.nn.Parameter(w_gradient_mask, requires_grad=False)
+        self.w_gradient = torch.nn.Parameter(torch.zeros((gradient_size, self.n)).uniform_(-1, 1))  # (gradient_size, n)
+        self.w_gradient_mask = torch.nn.Parameter(w_gradient_mask, requires_grad=False)  # (gradient_size, n), bool
 
     def _external_input(self, stimuli):
         # proprioception input
-        external_input = super(SNNCell2, self)._external_input(stimuli[:, 0:self.p])
+        external_input = super(SNNCell1, self)._external_input(stimuli[:, 0:self.p])
         # gradient input
         w_gradient = self.w_gradient * self.w_gradient_mask
         external_input += torch.mm(stimuli[:, self.p:self.p+self.gradient_size], w_gradient)
