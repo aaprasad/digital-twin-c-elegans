016f092ea1cd06f65bd968930887ecda14116b5f
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-08-10 00:53:52 +0800

    forward: add new proprioception input settings

--- virtual_nematode/connectomes/forward.py
+++ virtual_nematode/connectomes/forward.py
@@ -1,33 +1,19 @@
 import numpy as np
 import pandas as pd
 import torch
-from virtual_nematode.connectomes.cells import body_wall_muscles, neuron_list2, sensory_neurons
-from virtual_nematode.connectomes.polarity import chemical_polarities
+from virtual_nematode.connectomes.cells import body_wall_muscles, neuron_list2, proprioception_neurons
+from virtual_nematode.connectomes.polarity import chemical_polarities, proprioception_polarities
 
 
 class Connectome(object):
-    def __init__(self, path, neurons, muscles, ex_synapses, in_synapses, sensory_neurons, p):
+    def __init__(self, path, neurons, muscles, ex_synapses, in_synapses):
         self.path = path
         self.neurons = neurons
         self.muscles = muscles
         self.cells = neurons + muscles  # cells to simulate
+        self.chemical, self.gap_junction = self._init()
         self.ex_synapses = self._check_synapses(ex_synapses)
         self.in_synapses = self._check_synapses(in_synapses)
-        self.sensory_neurons = self._check_cells(sensory_neurons)
-        self.p = p
-        self.chemical, self.gap_junction = self._init()
-
-    def _check_synapses(self, synapses):
-        """ remove duplicate synapses, and check if related cells in the synapses exist """
-        synapses = list(set(synapses))
-        synapse_cells = set([pre for pre, _ in synapses] + [post for _, post in synapses])
-        assert synapse_cells.issubset(set(self.cells))
-        return synapses
-
-    def _check_cells(self, cells):
-        """ check if the cells exist """
-        assert set(cells).issubset(set(self.cells))
-        return cells
 
     def _init(self):
         chemical = pd.read_excel(self.path, sheet_name='hermaphrodite chemical', header=2, index_col=2).iloc[:300, 2:456]
@@ -56,12 +42,18 @@ class Connectome(object):
         adjacency = adjacency.loc[self.cells, self.cells]
         return adjacency
 
-    def _weight(self, chemical, gap_junction):
-        chemical = chemical.replace(np.nan, 0)
-        gap_junction = gap_junction.replace(np.nan, 0)
-        w_c_mask = torch.from_numpy(chemical.to_numpy(dtype=np.bool))
-        w_g_mask = torch.from_numpy(gap_junction.to_numpy(dtype=np.bool))
-        return w_c_mask, w_g_mask
+    def _check_synapses(self, synapses):
+        """ remove duplicate synapses, and check if related cells in the synapses exist """
+        synapses = list(set(synapses))
+        synapse_cells = set([pre for pre, _ in synapses] + [post for _, post in synapses])
+        assert synapse_cells.issubset(set(self.cells))
+        return synapses
+
+    @staticmethod
+    def _weight(adjacency):
+        adjacency = adjacency.replace(np.nan, 0)
+        mask = torch.from_numpy(adjacency.to_numpy(dtype=np.bool))
+        return mask
 
     def _polarity_mask(self, synapses):
         """ chemical synaptic polarity mask
@@ -73,23 +65,6 @@ class Connectome(object):
         for pre, post in synapses:
             mask.loc[pre, post] = True
         mask = torch.from_numpy(mask.to_numpy(dtype=np.bool))
-        return mask
-
-    def _external_input_mask(self, dim):
-        """ external input bool mask
-        dim: dimension of the external input
-        flag: if True, sensory neurons receive external input; else, all neurons receive external input
-        """
-        mask = pd.DataFrame(False, index=list(range(dim)), columns=self.cells)
-        mask.loc[:, self.sensory_neurons] = True
-        mask = torch.from_numpy(mask.to_numpy(dtype=np.bool))
-        return mask
-
-    def _proprioception_mask(self):
-        """ proprioception input synapse bool mask
-        https://doi.org/10.1016/j.neuron.2012.08.039
-        """
-        mask = self._external_input_mask(dim=self.p)
         return mask
 
     def mask(self):
@@ -110,42 +85,99 @@ class Connectome(object):
             mask_pi is True -> w_pi
         """
         # chemical synapses and gap junctions
-        w_c_mask, w_g_mask = self._weight(self.chemical, self.gap_junction)
-        if torch.all(w_g_mask.tril() == w_g_mask.triu().T).item() is not True:
+        c_mask = self._weight(self.chemical)
+        g_mask = self._weight(self.gap_junction)
+        if torch.all(g_mask.tril() == g_mask.triu().T).item() is not True:
             raise AssertionError('Gap junction mask is not symmetric!')
         # chemical synapse polarity: subsets of chemical synapse mask, and no overlap between them
-        w_c_ex_mask = self._polarity_mask(self.ex_synapses)
-        w_c_in_mask = self._polarity_mask(self.in_synapses)
-        w_c_ex_mask &= w_c_mask
-        w_c_in_mask &= w_c_mask
-        if torch.any(w_c_ex_mask & w_c_in_mask).item() is True:
+        c_ex_mask = self._polarity_mask(self.ex_synapses)
+        c_in_mask = self._polarity_mask(self.in_synapses)
+        c_ex_mask &= c_mask
+        c_in_mask &= c_mask
+        if torch.any(c_ex_mask & c_in_mask).item() is True:
             raise AssertionError('There is overlap between excitatory mask and inhibitory mask!')
         # chemical synapses exclude excitatory/inhibitory chemical synapses
-        w_c_mask = w_c_mask ^ w_c_ex_mask ^ w_c_in_mask
+        c_mask = c_mask ^ c_ex_mask ^ c_in_mask
         # combined chemical synapse polarity mask, (3, n, n)
-        w_c_mask = torch.stack((w_c_mask, w_c_ex_mask, w_c_in_mask), dim=0)
-        # proprioception input
-        w_p_mask = self._proprioception_mask()
+        c_mask = torch.stack((c_mask, c_ex_mask, c_in_mask), dim=0)
         # output
         muscles = set(self.muscles)
         output_index = torch.tensor([True if cell in muscles else False for cell in self.cells])
-        return w_c_mask, w_g_mask, w_p_mask, output_index
+        return c_mask, g_mask, output_index
+
+
+class ExternalInput(object):
+    def __init__(self, neurons, muscles, dim, input_neurons, ex_synapses, in_synapses):
+        self.neurons = neurons
+        self.muscles = muscles
+        self.cells = neurons + muscles  # cells to simulate
+        self.dim = dim  # input dimension
+        self.input_neurons = self._check_cells(input_neurons)
+        self.ex_synapses = self._check_synapses(ex_synapses)
+        self.in_synapses = self._check_synapses(in_synapses)
+
+    def _check_cells(self, cells):
+        """ check if the cells exist """
+        assert set(cells).issubset(set(self.cells))
+        return cells
+
+    def _check_synapses(self, synapses):
+        """ check if related cells in the synapses exist """
+        pre_cells = set([pre for pre, _ in synapses])
+        assert pre_cells.issubset(set(range(self.dim)))
+        post_cells = set([post for _, post in synapses])
+        assert post_cells.issubset(set(self.cells))
+        return synapses
+
+    def _polarity_mask(self, synapses):
+        """ polarity mask
+        synapses: [(pre1, post1), (pre2, post2), ...]
+        excitatory mask: if True, the connection is excitatory
+        inhibitory mask: if True, the connection is inhibitory
+        """
+        mask = pd.DataFrame(False, index=list(range(self.dim)), columns=self.cells)
+        for pre, post in synapses:
+            mask.loc[pre, post] = True
+        mask = torch.from_numpy(mask.to_numpy(dtype=np.bool))
+        return mask
+
+    def _mask(self):
+        mask = pd.DataFrame(False, index=list(range(self.dim)), columns=self.cells)
+        mask.loc[:, self.input_neurons] = True
+        mask = torch.from_numpy(mask.to_numpy(dtype=np.bool))
+        return mask
+
+    def mask(self):
+        mask = self._mask()
+        ex_mask = self._polarity_mask(self.ex_synapses)
+        in_mask = self._polarity_mask(self.in_synapses)
+        ex_mask &= mask
+        in_mask &= mask
+        if torch.any(ex_mask & in_mask).item() is True:
+            raise AssertionError('There is overlap between excitatory mask and inhibitory mask!')
+        mask = mask ^ ex_mask ^ in_mask
+        mask = torch.stack((mask, ex_mask, in_mask), dim=0)  # (3, dim, n)
+        return mask
 
 
 def get_kwargs(path, polarity_path):
     """ connectome masks and params """
+    # connectome
     muscles = body_wall_muscles()
     neurons = neuron_list2(path, muscles)
-    sensory = sensory_neurons(path)
-    print('{} neurons, {} muscles, {} sensory, {} cells in total'.format(len(neurons), len(muscles), len(sensory), len(neurons) + len(muscles)))
+    print('{} neurons, {} muscles, {} cells in total'.format(len(neurons), len(muscles), len(neurons) + len(muscles)))
     ex_synapses, in_synapses = chemical_polarities(polarity_path)
     print('{} excitatory synapses, {} inhibitory synapses'.format(len(ex_synapses), len(in_synapses)))
+    connectome = Connectome(path=path, neurons=neurons, muscles=muscles, ex_synapses=ex_synapses, in_synapses=in_synapses)
+    w_c_mask, w_g_mask, output_index = connectome.mask()
+    # proprioception input
     p = 24
-    connectome = Connectome(
-        path=path, neurons=neurons, muscles=muscles, ex_synapses=ex_synapses, in_synapses=in_synapses,
-        sensory_neurons=sensory, p=p
+    p_ex_synapses, p_in_synapses = proprioception_polarities(dim=p)
+    proprioception = ExternalInput(
+        neurons=neurons, muscles=muscles, dim=p, input_neurons=proprioception_neurons(),
+        ex_synapses=p_ex_synapses, in_synapses=p_in_synapses
     )
-    w_c_mask, w_g_mask, w_p_mask, output_index = connectome.mask()
+    w_p_mask = proprioception.mask()
     return {
         'n': len(connectome.cells), 'm': len(connectome.muscles), 'p': p,
         'w_c_mask': w_c_mask, 'w_g_mask': w_g_mask, 'w_p_mask': w_p_mask, 'output_index': output_index
