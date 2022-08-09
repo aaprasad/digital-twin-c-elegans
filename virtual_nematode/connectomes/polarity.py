524b7286c82f5121f149182f808d3cdbc4a083b4
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-08-10 00:49:31 +0800

    connectome: add proprioception input polarity

--- virtual_nematode/connectomes/polarity.py
+++ virtual_nematode/connectomes/polarity.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from virtual_nematode.connectomes.cells import db_motor_neurons, vb_motor_neurons, da_motor_neurons, va_motor_neurons
 
 
 def _chemical_polarities(df):
@@ -13,4 +14,31 @@ def chemical_polarities(path):
     df = pd.read_excel(path)
     ex_synapses = _chemical_polarities(df[df['Sign'] == '+'])
     in_synapses = _chemical_polarities(df[df['Sign'] == '-'])
+    return ex_synapses, in_synapses
+
+
+def _proprioception_polarities(dim, cells):
+    """ fully connected """
+    synapses = []
+    for pre in range(dim):
+        for post in cells:
+            synapses.append((pre, post))
+    return synapses
+
+
+def proprioception_polarities(dim):
+    """ https://doi.org/10.1016/j.neuron.2012.08.039
+    The activity of the ventral motor neuron (VB9) is positively correlated with bending toward the ventral side.
+    The activity of the dorsal cholinergic neuron (DB6) is positively correlated with bending toward the dorsal side.
+    Bending the worm toward the dorsal side activated the DB motor neuron over the VB motor neuron.
+    Bending the worm toward the ventral side activated the VB motor neuron over the DB motor neuron.
+    nematode lies on its left, and thus dorsal muscles are on its left; ventral muscles are on its right
+    if dorsal muscle contracts, joint angle < 0; if ventral muscle contracts, joint angle > 0
+    """
+    db = db_motor_neurons()
+    vb = vb_motor_neurons()
+    da = da_motor_neurons()
+    va = va_motor_neurons()
+    ex_synapses = _proprioception_polarities(dim, vb + va)
+    in_synapses = _proprioception_polarities(dim, db + da)
     return ex_synapses, in_synapses
