b74950ca264a1bf5e0267a72d96cf060053648ab
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-08-19 15:33:48 +0800

    forward: organize models, re-add snncell

--- virtual_nematode/networks/snn/forward.py
+++ virtual_nematode/networks/snn/forward.py
@@ -1,5 +1,64 @@
-import math
 import torch
+
+
+class SNNCell(torch.nn.Module):
+    def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index):
+        super(SNNCell, self).__init__()
+        self.dt = dt  # env dt
+        self.steps = steps  # ode steps
+        self.n = n  # number of cells
+        self.m = m  # number of muscles
+        self.p = p  # proprioception input size
+        self.tau = torch.nn.Parameter(torch.zeros(n).uniform_(0.01, 0.05))  # (n, )
+        self.bias = torch.nn.Parameter(torch.zeros(n).uniform_(-1, 1))  # (n, )
+        self.w_c = torch.nn.Parameter(torch.zeros((n, n)).uniform_(-1, 1))  # (n, n)
+        self.w_c_mask = torch.nn.Parameter(w_c_mask, requires_grad=False)  # (3, n, n), bool
+        self.w_g = torch.nn.Parameter(torch.zeros((n, n)).uniform_(0, 1))  # (n, n)
+        self.w_g_mask = torch.nn.Parameter(w_g_mask, requires_grad=False)  # (n, n), bool
+        self.w_p = torch.nn.Parameter(torch.zeros((p, n)).uniform_(-1, 1))  # (p, n)
+        self.w_p_mask = torch.nn.Parameter(w_p_mask, requires_grad=False)  # (3, p, n), bool
+        self.output_index = torch.nn.Parameter(output_index, requires_grad=False)  # (n, ), bool
+        self.activation_func = torch.nn.Sigmoid()
+
+    @property
+    def init_state(self):
+        """ initial state and activation """
+        state = self.bias.clone().detach()  # (n, )
+        activation = self.activation_func(state)  # (n, )
+        return state, activation
+
+    def _external_input(self, stimuli):
+        """ proprioception input """
+        w_p_abs = self.w_p.abs()
+        w_p = self.w_p * self.w_p_mask[0] + w_p_abs * self.w_p_mask[1] - w_p_abs * self.w_p_mask[2]
+        external_input = torch.mm(stimuli, w_p)  # (n, )
+        return external_input
+
+    def forward(self, state, activation, stimuli):
+        # chemical synapse weight
+        w_c_abs = self.w_c.abs()
+        w_c = self.w_c * self.w_c_mask[0] + w_c_abs * self.w_c_mask[1] - w_c_abs * self.w_c_mask[2]
+        # gap junction weight
+        w_g = self.w_g.abs()
+        w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * self.w_g_mask
+        # external input + bias
+        external_input = self._external_input(stimuli) + self.bias
+        # dt / tau
+        dt_tau = self.dt / self.steps / self.tau.clamp(0.01, 0.05)
+        for i in range(self.steps):
+            # chemical synapse input
+            synapse_input = torch.mm(activation, w_c)
+            # gap junction input
+            delta_state = state.unsqueeze(dim=2).repeat(1, 1, self.n) - state.unsqueeze(dim=1).repeat(1, self.n, 1)
+            gap_input = torch.sum(delta_state * w_g, dim=1)
+            # total input
+            total_input = synapse_input + gap_input + external_input
+            # cell state and activation
+            state = (1 - dt_tau) * state + dt_tau * total_input
+            activation = self.activation_func(state)
+        # muscle output
+        action = activation[:, self.output_index]
+        return state, activation, action
 
 
 class SNNCell4(torch.nn.Module):
