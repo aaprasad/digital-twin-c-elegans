979b4e6db1d7bc2ce27b25177d712f0c926b3df7
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-08-30 20:45:06 +0800

    weathervane: train with snncell3

--- scripts/muscle_ellipsoid2d/weathervane/train.py
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -4,14 +4,14 @@ import worm_assets
 
 
 def train(model_name):
-    if model_name in ['snn_weathervane', 'snn_weathervane1']:
+    if model_name.startswith('snn_weathervane'):
         data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
-        device_ids, batch_size, lr = [0, 1, 2, 3], 128, 5e-2
+        device_ids, batch_size, lr, epochs = [0, 1, 2, 3], 128, 5e-2, 1000
         # strict, model_path = True, None
         strict, model_path = False, '/home/imc/disk1/virtual-nematode/scripts/muscle_ellipsoid2d/forward/runs/Jul25_12-07-58_h-10-176-50-34/model119.pt'
         kwargs = {
             'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,
-            'device_ids': device_ids, 'lr': lr, 'epochs': 300, 'early_stop': 300, 'loss': 'MSELoss',
+            'device_ids': device_ids, 'lr': lr, 'epochs': epochs, 'early_stop': epochs, 'loss': 'MSELoss',
             # model kwargs
             'model_path': model_path, 'strict': strict,
             'dt': 0.04, 'steps': 5,
@@ -26,4 +26,4 @@ def train(model_name):
 
 
 if __name__ == '__main__':
-    train('snn_weathervane1')
+    train('snn_weathervane3')
