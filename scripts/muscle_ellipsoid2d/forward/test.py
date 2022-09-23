8316bff7b76e21089b02f0ec4ae2e2dc60a14e45
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-09-23 17:44:55 +0800

    forward: simulate and record trap experiment

--- scripts/muscle_ellipsoid2d/forward/test.py
+++ scripts/muscle_ellipsoid2d/forward/test.py
@@ -5,7 +5,7 @@ from sim import step_func as x_func
 import sys
 import torch
 from virtual_nematode.connectomes.forward import get_kwargs
-from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer
+from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer, make_swimmer_trapped
 from virtual_nematode.testers.forward import tester, single_tester, test_func2
 from virtual_nematode.trainers.ncp import prepare_model
 import worm_assets
@@ -61,6 +61,19 @@ def record(env, model_folder, model_name, ckpt_name):
     single_test(env, model_folder, model_name, ckpt_name, save_folder)
 
 
+def record_trapped_experiment(model_folder, model_name, ckpt_name, body_index):
+    """ trap experiment
+    reset_noise_scale = 0.
+    """
+    save_folder = os.path.join('video', runs_folder, ckpt_name)
+    env = make_swimmer_trapped(
+        n_bodies=25, joint_range='-90 90', max_episode_steps=max_episode_steps, reset_noise_scale=0.,
+        density=1.2, viscosity=0.1, condim=3, friction='1 1', body_index=body_index
+    )
+    env = gym.wrappers.Monitor(env, directory=save_folder, force=True)
+    single_test(env, model_folder, model_name, ckpt_name, save_folder)
+
+
 if __name__ == '__main__':
     runs_folder = sys.argv[1]
     model_folder = os.path.join('runs', runs_folder)
@@ -75,3 +88,4 @@ if __name__ == '__main__':
     test(model_folder, 'snn_forward3', ckpt_name='model.pt')
     # single_test(env, model_folder, 'snn_forward3', ckpt_name='model.pt', save_folder=os.path.join('data', runs_folder))
     # record(env, model_folder, 'snn_forward3', ckpt_name='model.pt')
+    # record_trapped_experiment(model_folder, 'snn_forward3', ckpt_name='model.pt', body_index=[10, 11, 12, 13, 14, 15, 16])
