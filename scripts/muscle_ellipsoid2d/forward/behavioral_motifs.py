34e1d8963f64bdda39ff145c56e44b2629713950
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-07 10:09:46 +0800

    behavioral motifs: recreate forward movement

---
+++ scripts/muscle_ellipsoid2d/forward/behavioral_motifs.py
@@ -0,0 +1,77 @@
+import gym
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import seaborn as sns
+from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer
+from virtual_nematode.models.muscle import ForwardPIDMuscle
+
+
+def simulate(env, model, seed=None, render=False):
+    data = {'observation': [], 'action': []}
+    env.seed(seed)
+    observation = env.reset()
+    model.reset()
+    for step in range(10 ** 6):
+        if render is True:
+            env.render()
+        action = model.step(step, q=observation[4:28])
+        observation, reward, done, info = env.step(action)
+        data['observation'].append(observation)
+        data['action'].append(action)
+        if done:
+            break
+    data = {key: np.array(value) for key, value in data.items()}
+    return data
+
+
+def forward_crawl(env, render):
+    folder = os.path.join('video', 'forward_crawl')
+    env = gym.wrappers.Monitor(env, directory=folder, force=True)
+    model = ForwardPIDMuscle(
+        dt=env.dt, n=25, a=0.6, freq=0.8, psi=0.07,
+        kp=np.concatenate(([1 + i * 0.2 for i in range(12)], [3.2 - i * 0.2 for i in range(12)])),
+        kd=0.15
+    )
+    data = simulate(env, model, seed=None, render=render)
+    np.savez(os.path.join(folder, 'data.npz'), **data)
+
+
+def process_data(path):
+    data = np.load(path)
+    plt.figure()
+    plt.title('Joint Angle')
+    sns.heatmap(data['observation'][:, 4:28], cmap='coolwarm', vmin=-1, vmax=1)
+    plt.xlabel('Time (steps)')
+    plt.ylabel('Joint ID')
+    plt.figure()
+    plt.title('Muscle Action')
+    sns.heatmap(data['action'][:, :], cmap='coolwarm', vmin=0, vmax=1)
+    plt.xlabel('Time (steps)')
+    plt.ylabel('Muscle ID')
+
+
+def shallow_type_I_turn():
+    pass
+
+
+def shallow_type_II_turn():
+    pass
+
+
+def gradual_turn():
+    pass
+
+
+def omega_turn():
+    pass
+
+
+if __name__ == '__main__':
+    env = make_swimmer(
+        n_bodies=25, joint_range='-90 90', max_episode_steps=1000, reset_noise_scale=0.,
+        density=1.2, viscosity=0.1, condim=3, friction='1 1'
+    )
+    print(env.action_space)
+    print(env.observation_space)
+    forward_crawl(env, render=False)
