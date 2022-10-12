3f82ea17a1f66cac636e71358fe3ebaf9171a952
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-12 16:23:57 +0800

    weathervane: train weathervane from random init

--- scripts/muscle_ellipsoid2d/weathervane/train.py
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -6,14 +6,12 @@ import worm_assets
 def train(model_name):
     if model_name.startswith('snn_weathervane'):
         data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
-        device_ids, batch_size, lr, epochs = [0, 1, 2, 3], 128, 5e-2, 1000
-        # strict, model_path = True, None
-        strict, model_path = False, '/home/imc/disk1/virtual-nematode/scripts/muscle_ellipsoid2d/forward/runs/Aug28_11-18-28_h-10-176-50-34/model389.pt'
+        device_ids, batch_size, lr, epochs = [0, 1, 2, 3], 128, 5e-2, 10000
         kwargs = {
             'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,
-            'device_ids': device_ids, 'lr': lr, 'epochs': epochs, 'early_stop': epochs,
+            'device_ids': device_ids, 'lr': lr, 'weight_decay': 0, 'epochs': epochs, 'early_stop': epochs,
             # model kwargs
-            'model_path': model_path, 'strict': strict,
+            'model_path': None, 'strict': True, 'optimizer_path': None,
             'dt': 0.04, 'steps': 5,
             **get_kwargs(
                 path=worm_assets.connectome_path(),
