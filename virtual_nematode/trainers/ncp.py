a3bb463a4fbe1750b09876bb1cdaa61e7ea697ce
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-18 23:16:39 +0800

    forward: add li_current model choice

--- virtual_nematode/trainers/ncp.py
+++ virtual_nematode/trainers/ncp.py
@@ -10,6 +10,7 @@ from virtual_nematode.networks.ncp.wirings import FullyConnected, NCP
 from virtual_nematode.networks.rnn.ctrnn_cell import CTRNNCell
 from virtual_nematode.networks.rnn.rnn import RNNCell
 from virtual_nematode.networks.snn.forward import SNNCell, SNN, SNNCell1, SNNCell2, SNNCell3, SNNCell4
+from virtual_nematode.networks.snn.lif import LeakyIntegratorCurrentBased
 from virtual_nematode.networks.snn.weathervane import SNNCell as SNNCellW
 from virtual_nematode.networks.snn.weathervane import SNNCell1 as SNNCellW1
 from virtual_nematode.networks.snn.weathervane import SNNCell3 as SNNCellW3
@@ -42,6 +43,8 @@ def prepare_model(model_name, device=None, device_ids=None, model_path=None, str
         model = snn_forward3(**kwargs)
     elif model_name == 'snn_forward4':
         model = snn_forward4(**kwargs)
+    elif model_name == 'li_current':
+        model = li_current(**kwargs)
     elif model_name == 'snn_weathervane':
         model = snn_weathervane(**kwargs)
     elif model_name == 'snn_weathervane1':
@@ -145,6 +148,12 @@ def snn_forward3(**kwargs):
 
 def snn_forward4(**kwargs):
     cell = SNNCell4(**kwargs)
+    model = SNN(cell)
+    return model
+
+
+def li_current(**kwargs):
+    cell = LeakyIntegratorCurrentBased(**kwargs)
     model = SNN(cell)
     return model
 
