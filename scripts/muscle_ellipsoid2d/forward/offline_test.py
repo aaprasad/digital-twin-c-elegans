63e830796c1ea3f8db5d6236458463df49fc96e6
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-21 11:00:43 +0800

    forward: li current based model, offline test

--- scripts/muscle_ellipsoid2d/forward/offline_test.py
+++ scripts/muscle_ellipsoid2d/forward/offline_test.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
             polarity_path=worm_assets.polarity_path('Cook et al connectome.xls')
         )
     }
-    mse = offline_test(data_name, 'snn_forward3', model_folder, ckpt_name, batch_size=128, device_ids=0, **kwargs)
+    mse = offline_test(data_name, 'li_current', model_folder, ckpt_name, batch_size=128, device_ids=0, **kwargs)
     # print('{:.4e}'.format(mse))
     print('{:.4e}'.format(mse.mean().item()))
     save_path = os.path.join('data', runs_folder, ckpt_name)
