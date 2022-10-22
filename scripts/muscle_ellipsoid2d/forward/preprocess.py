6f6b679f40c68cd392d964e82ee33dcd703e5bbd
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-22 11:21:56 +0800

    forward: preprocess data, 3500 samples with 1280 steps each, split 2500-500-500 samples, split 1280 steps into 64 steps a piece

--- scripts/muscle_ellipsoid2d/forward/preprocess.py
+++ scripts/muscle_ellipsoid2d/forward/preprocess.py
@@ -2,6 +2,7 @@ from virtual_nematode.data.utils import preprocess_and_split_dataset
 
 
 if __name__ == '__main__':
+    """
     preprocess_and_split_dataset(
         load_name='data_7000_640.pt',
         save_names=[
@@ -11,12 +12,13 @@ if __name__ == '__main__':
         ],
         lengths=[5000, 1000, 1000], seq_len=64, seed=33
     )
-    # preprocess_and_split_dataset(
-    #     load_name='data_14000_640.pt',
-    #     save_names=[
-    #         'data_14000_10000_640_64_train.pt',
-    #         'data_14000_2000_640_64_eval.pt',
-    #         'data_14000_2000_640_64_test.pt'
-    #     ],
-    #     lengths=[10000, 2000, 2000], seq_len=64, seed=33
-    # )
+    """
+    preprocess_and_split_dataset(
+        load_name='data_3500_1280.pt',
+        save_names=[
+            'data_3500_2500_1280_64_train.pt',
+            'data_3500_500_1280_64_eval.pt',
+            'data_3500_500_1280_64_test.pt'
+        ],
+        lengths=[2500, 500, 500], seq_len=64, seed=33
+    )
