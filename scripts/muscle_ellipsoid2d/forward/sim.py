307df181980c5be2190928b5a136eb55f92944f8
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-10-26 18:48:58 +0800

    forward: simulation, step_func() returns all observation

--- scripts/muscle_ellipsoid2d/forward/sim.py
+++ scripts/muscle_ellipsoid2d/forward/sim.py
@@ -22,7 +22,7 @@ def action_func(model, step, observation, **kwargs):
 
 def step_func(observation, **kwargs):
     com = observation[56:58]  # 2D center of mass
-    return com
+    return observation
 
 
 def done_func(index, result, **kwargs):
@@ -30,10 +30,9 @@ def done_func(index, result, **kwargs):
     result: list of com in 1 simulation
     return: displacement in this simulation
     """
-    displacement = np.linalg.norm(np.array(result[-1]) - np.array(result[0]), ord=2)
-    d = np.linalg.norm(np.array(result[-1]) - np.array(result[-250]), ord=2)
-    print('Trial {}: com displacement {:.2f}, last 250 steps {:.2f}'.format(index + 1, displacement, d))
-    return displacement, d
+    displacement = np.linalg.norm(np.array(result[-1, 56:58]) - np.array(result[0, 56:58]), ord=2)
+    print('Trial {}: com displacement {:.2f}'.format(index + 1, displacement))
+    return displacement
 
 
 if __name__ == '__main__':
@@ -57,6 +56,4 @@ if __name__ == '__main__':
         kd=0.15
     )
     results = simulate(env, model, action_func, step_func, done_func, seed=None, trials=1, render=False)
-    print('{} trials: com displacement mean {:.2f} / {} steps, {:.2f} / last 250 steps'.format(
-        len(results), np.mean([a for a, b in results]), max_episode_steps, np.mean([b for a, b in results])
-    ))
+    print('{} trials: com displacement mean {:.2f} / {} steps'.format(len(results), np.mean(results), max_episode_steps))
