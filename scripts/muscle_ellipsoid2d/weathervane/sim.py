4b994daa6b1addd62abcbaa67648ef51353018cd
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-03 10:14:06 +0800

    weathervane: simulate, add max_episode_steps=2500

--- scripts/muscle_ellipsoid2d/weathervane/sim.py
+++ scripts/muscle_ellipsoid2d/weathervane/sim.py
@@ -50,8 +50,9 @@ def done_func(result, index=None, **kwargs):
 
 
 if __name__ == '__main__':
+    max_episode_steps = 2500
     env = make_swimmer_weathervane(
-        n_bodies=25, joint_range='-90 90', max_episode_steps=2500, reset_noise_scale=0.6, distance=15,
+        n_bodies=25, joint_range='-90 90', max_episode_steps=max_episode_steps, reset_noise_scale=0.6, distance=15,
         position_func=position_func, density=1.2, viscosity=0.1, condim=3, friction='1 1', source=(0, 0)
     )  # distance = 3 * sigma
     # env = gym.wrappers.Monitor(env, directory='video/swimmer', force=True)
