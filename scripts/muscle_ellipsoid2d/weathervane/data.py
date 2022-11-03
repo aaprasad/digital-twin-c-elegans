9678b3f2cc27e34fb7c31b81fe2c0ddd68cba776
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-03 11:44:32 +0800

    weathervane: generate data, record all observation and action

--- scripts/muscle_ellipsoid2d/weathervane/data.py
+++ scripts/muscle_ellipsoid2d/weathervane/data.py
@@ -8,12 +8,12 @@ from virtual_nematode.models.muscle import WeathervanePIDMuscle
 
 
 def x_func(observation, **kwargs):
-    q = observation[4:28]
-    # c = [observation[62]]
-    g = [observation[63]]
-    # g_p = [observation[64]]
-    # g_w = [observation[65]]
-    return np.concatenate((q, g))
+    # q = observation[4:28]
+    # c = observation[62:63]
+    # g = observation[63:64]
+    # g_p = observation[64:65]
+    # g_w = observation[65:66]
+    return observation
 
 
 def y_func(action, **kwargs):
@@ -21,19 +21,19 @@ def y_func(action, **kwargs):
 
 
 if __name__ == '__main__':
-    input_size = 24 + 1
+    input_size = 66  # x_func() size
     data_size = 7000
     max_episode_steps = 640
+    reset_noise_scale = 0.6
     seed = 7
     env = make_swimmer_weathervane(
         n_bodies=25, joint_range=['-70 70'] + ['-100 100'] * 22 + ['-70 70'],
-        max_episode_steps=max_episode_steps, reset_noise_scale=0.6,
+        max_episode_steps=max_episode_steps, reset_noise_scale=reset_noise_scale,
         distance=15, source=(0, 0), position_func=position_func,  # distance = 3 * sigma
         density=1.2, viscosity=0.1, condim=3, friction='1 1 0.005 0.0001 0.0001', cone='elliptic'
     )
     action_size = env.action_space.shape[0]
-    print(env.action_space)
-    print(env.observation_space)
+    print(env.action_space, env.observation_space)
     print(env.source)
     model = WeathervanePIDMuscle(
         k_w=1, dt=env.dt, n=25, a=0.6, freq=0.8, psi=0.07,
