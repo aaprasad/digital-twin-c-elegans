e764c65e16dd1470e4b5d4f7e4d6cbd7ba62e6b5
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-09 13:38:50 +0800

    weathervane: testing

--- scripts/muscle_ellipsoid2d/weathervane/analysis.py
+++ scripts/muscle_ellipsoid2d/weathervane/analysis.py
@@ -9,9 +9,9 @@ def get_results_numpy(x, y, **kwargs):
     distance_mean = np.linalg.norm(x[:, 1:, 56:58] - x[:, 0:-1, 56:58], ord=2, axis=2).sum(axis=1).mean()
     print('{} trial(s), com distance mean {:.2f} / {} steps'.format(x.shape[0], distance_mean, max_episode_steps))
     chemotaxis_index_mean = x[:, :, 62].mean()
-    print('{} trials: chemotaxis index mean {:.2f}'.format(x.shape[0], chemotaxis_index_mean))
+    print('{} trial(s), chemotaxis index mean {:.2f}'.format(x.shape[0], chemotaxis_index_mean))
     initial_concentration_mean = x[:, 0, 62].mean()
-    print('{} trials: initial concentration mean {:.2f}'.format(x.shape[0], initial_concentration_mean))
+    print('{} trial(s), initial concentration mean {:.2f}'.format(x.shape[0], initial_concentration_mean))
 
 
 def get_results_torch(x, y, **kwargs):
@@ -21,6 +21,18 @@ def get_results_torch(x, y, **kwargs):
     distance_mean = torch.linalg.norm(x[:, 1:, 56:58] - x[:, 0:-1, 56:58], ord=2, dim=2).sum(dim=1).mean().item()
     print('{} trial(s), com distance mean {:.2f} / {} steps'.format(x.shape[0], distance_mean, max_episode_steps))
     chemotaxis_index_mean = x[:, :, 62].mean().item()
-    print('{} trials: chemotaxis index mean {:.2f}'.format(x.shape[0], chemotaxis_index_mean))
+    print('{} trial(s), chemotaxis index mean {:.2f}'.format(x.shape[0], chemotaxis_index_mean))
     initial_concentration_mean = x[:, 0, 62].mean().item()
-    print('{} trials: initial concentration mean {:.2f}'.format(x.shape[0], initial_concentration_mean))
+    print('{} trial(s), initial concentration mean {:.2f}'.format(x.shape[0], initial_concentration_mean))
+
+
+def get_result_torch(x, y, **kwargs):
+    max_episode_steps = kwargs.get('max_episode_steps')
+    displacement = torch.linalg.norm(x[-1, 56:58] - x[0, 56:58], ord=2).item()
+    print('com displacement {:.2f} / {} steps'.format(displacement, max_episode_steps))
+    distance = torch.linalg.norm(x[1:, 56:58] - x[0:-1, 56:58], ord=2, dim=1).sum(dim=1).mean().item()
+    print('com distance {:.2f} / {} steps'.format(distance, max_episode_steps))
+    chemotaxis_index = x[:, 62].mean().item()
+    print('chemotaxis index {:.2f}'.format(chemotaxis_index))
+    initial_concentration = x[0, 62].item()
+    print('initial concentration {:.2f}'.format(initial_concentration))
--- scripts/muscle_ellipsoid2d/weathervane/test.py
+++ scripts/muscle_ellipsoid2d/weathervane/test.py
@@ -1,16 +1,33 @@
-from data import x_func as data_func
-from data import y_func
+from analysis import get_results_torch, get_result_torch
 import gym
+import numpy as np
 import os
-from sim import position_func
-from sim import step_func as x_func
+from sim import x_func, position_func
 import sys
 import torch
 from virtual_nematode.connectomes.weathervane import get_kwargs
 from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer_weathervane
-from virtual_nematode.testers.weathervane import single_tester, tester
+from virtual_nematode.testers.tester import tester, single_tester
 from virtual_nematode.trainers.ncp import prepare_model
 import worm_assets
+
+
+def data_func(observation, **kwargs):
+    q = observation[4:28]
+    # c = observation[62:63]
+    g = observation[63:64]
+    # g_p = observation[64:65]
+    # g_w = observation[65:66]
+    return np.concatenate((q, g))
+
+
+def y_func(**kwargs):
+    # state = kwargs.get('state')
+    # activation = kwargs.get('activation')
+    action = kwargs.get('action')
+    data = action.tolist()
+    # data = state.squeeze(dim=0).tolist() + activation.squeeze(dim=0).tolist() + action.tolist()
+    return data
 
 
 def select_model(model_folder, model_name, ckpt_name):
@@ -28,24 +45,38 @@ def select_model(model_folder, model_name, ckpt_name):
     return model
 
 
-def test(model_folder, model_name, ckpt_name):
+def test(model_folder, model_name, ckpt_name, save_folder):
     print(ckpt_name)
     model = select_model(model_folder, model_name, ckpt_name)
-    tester(env, model, data_func, x_func, y_func, seed, max_episode_steps, model_folder, model_name, data_size=100)
+    x_func_size = env.observation_space.shape[0]
+    y_func_size = 95
+    x, y = tester(env, model, data_func, x_func, y_func, x_func_size, y_func_size, seed, max_episode_steps, data_size=100)
+    torch.save((x, y), os.path.join(save_folder, 'test.pt'))
+    get_results_torch(x, y, max_episode_steps=max_episode_steps)
 
 
-def record(env, model_folder, model_name, ckpt_name):
+def single_test(env, model_folder, model_name, ckpt_name, save_folder):
     print(ckpt_name)
     model = select_model(model_folder, model_name, ckpt_name)
-    env = gym.wrappers.Monitor(env, directory=os.path.join('video', runs_folder, ckpt_name), force=True)
-    x, y = single_tester(env, model, data_func, x_func, y_func, seed, max_episode_steps)
-    torch.save((x, y), os.path.join(data_path, ckpt_name))  # x_func's return, y_func's return (action)
+    x, y = single_tester(env, model, data_func, x_func, y_func, seed)
+    torch.save((x, y), os.path.join(save_folder, 'single_test.pt'))
+    get_result_torch(x, y, max_episode_steps=max_episode_steps)
+
+
+def record(env, model_folder, model_name, ckpt_name, video_folder):
+    env = gym.wrappers.Monitor(env, directory=video_folder, force=True)
+    single_test(env, model_folder, model_name, ckpt_name, save_folder=video_folder)
 
 
 if __name__ == '__main__':
     runs_folder = sys.argv[1]
     ckpt_name = sys.argv[2]  # 'model.pt'
     model_folder = os.path.join('runs', runs_folder)
+    save_folder = os.path.join('data', runs_folder, ckpt_name)
+    video_folder = os.path.join('video', runs_folder, ckpt_name)
+    assert os.path.exists(model_folder)
+    os.makedirs(save_folder, exist_ok=True)
+    os.makedirs(video_folder, exist_ok=True)
     print(model_folder, ckpt_name)
     seed = 43
     max_episode_steps = 2500
@@ -57,5 +88,8 @@ if __name__ == '__main__':
         distance=15, source=(0, 0), position_func=position_func,  # distance = 3 * sigma
         density=1.2, viscosity=0.1, condim=3, friction='1 1 0.005 0.0001 0.0001', cone='elliptic'
     )
-    test(model_folder, 'li_conductance_gradient', ckpt_name)
-    # record(env, model_folder, 'li_conductance_gradient', ckpt_name)
+    """ testing """
+    model_name = 'li_conductance_gradient'
+    test(model_folder, model_name, ckpt_name, save_folder)
+    # single_test(env, model_folder, model_name, ckpt_name, save_folder)
+    # record(env, model_folder, model_name, ckpt_name, video_folder)
