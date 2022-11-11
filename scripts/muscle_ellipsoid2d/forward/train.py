165af02aa373e4cf81c410b0a5c2378ece3b41ed
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-11 09:30:35 +0800

    forward: train model, seed=42

--- scripts/muscle_ellipsoid2d/forward/train.py
+++ scripts/muscle_ellipsoid2d/forward/train.py
@@ -9,11 +9,9 @@ def train(model_name):
         # data_name = ['data_3500_2500_1280_64_train.pt', 'data_3500_500_1280_64_eval.pt', 'data_3500_500_1280_64_test.pt']
         device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000
         kwargs = {
-            'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,
+            'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 42,  # 11, 33, 42
             'device_ids': device_ids, 'lr': lr, 'weight_decay': weight_decay, 'epochs': epochs, 'early_stop': epochs,
-            # 'model_path': '/home/imc/disk1/virtual-nematode/scripts/muscle_ellipsoid2d/forward/runs/Sep06_10-08-29_h-10-176-50-34/model999.pt',
-            # 'strict': True,
-            # 'optimizer_path': '/home/imc/disk1/virtual-nematode/scripts/muscle_ellipsoid2d/forward/runs/Sep06_10-08-29_h-10-176-50-34/model999.optim.pt',
+            # 'model_path': None, 'strict': True, 'optimizer_path': None,
             'dt': 0.04, 'steps': 5, **get_kwargs(
                 path=worm_assets.connectome_path(),
                 polarity_path=worm_assets.polarity_path('Cook et al connectome.xls')  # 'NT+R method prediction.xls'
