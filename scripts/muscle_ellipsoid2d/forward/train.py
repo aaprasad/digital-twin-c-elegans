5d19f823ff8cc26bc730ac34f6a9a5ecf75fb49d
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-23 00:41:04 +0800

    forward: train with stride=8 data

--- scripts/muscle_ellipsoid2d/forward/train.py
+++ scripts/muscle_ellipsoid2d/forward/train.py
@@ -6,8 +6,8 @@ import worm_assets
 def train(model_name):
     if model_name.startswith('snn_forward') or model_name.startswith('li'):
         # data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
-        # data_name = ['data_7000_5000_640_64_stride8_n10_train.pt', 'data_7000_1000_640_64_stride8_n10_eval.pt', 'data_7000_1000_640_64_stride8_n10_test.pt']
-        data_name = ['data_7000_5000_640_64_stride4_n10_train.pt', 'data_7000_1000_640_64_stride4_n10_eval.pt', 'data_7000_1000_640_64_stride4_n10_test.pt']
+        data_name = ['data_7000_5000_640_64_stride8_n10_train.pt', 'data_7000_1000_640_64_stride8_n10_eval.pt', 'data_7000_1000_640_64_stride8_n10_test.pt']
+        # data_name = ['data_7000_5000_640_64_stride4_n10_train.pt', 'data_7000_1000_640_64_stride4_n10_eval.pt', 'data_7000_1000_640_64_stride4_n10_test.pt']
         device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000
         kwargs = {
             'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 11,  # 11, 33, 42
