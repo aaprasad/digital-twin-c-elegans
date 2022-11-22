c0287db8d4eea6d3b44a51f7b25ac1660cb355b3
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-11-23 00:39:12 +0800

    forward: preprocess data, stride=8, first 136 steps of data

--- scripts/muscle_ellipsoid2d/forward/preprocess.py
+++ scripts/muscle_ellipsoid2d/forward/preprocess.py
@@ -7,23 +7,23 @@ if __name__ == '__main__':
     #     save_names=['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt'],
     #     lengths=[5000, 1000, 1000], x_index=(4, 28), seq_len=64, seed=33
     # )
-    # preprocess_and_split_slide_dataset(
-    #     load_name='data_7000_640.pt',
-    #     save_names=[
-    #         'data_7000_5000_640_64_stride8_n10_train.pt',
-    #         'data_7000_1000_640_64_stride8_n10_eval.pt',
-    #         'data_7000_1000_640_64_stride8_n10_test.pt'
-    #     ],
-    #     lengths=[5000, 1000, 1000], x_index=(4, 28),
-    #     seq_len=64, seq_amount=10, stride=8, seed=33
-    # )
     preprocess_and_split_slide_dataset(
         load_name='data_7000_640.pt',
         save_names=[
-            'data_7000_5000_640_64_stride4_n10_train.pt',
-            'data_7000_1000_640_64_stride4_n10_eval.pt',
-            'data_7000_1000_640_64_stride4_n10_test.pt'
+            'data_7000_5000_640_64_stride8_n10_train.pt',
+            'data_7000_1000_640_64_stride8_n10_eval.pt',
+            'data_7000_1000_640_64_stride8_n10_test.pt'
         ],
         lengths=[5000, 1000, 1000], x_index=(4, 28),
-        seq_len=64, seq_amount=10, stride=4, seed=33
+        seq_len=64, seq_amount=10, stride=8, seed=33
     )
+    # preprocess_and_split_slide_dataset(
+    #     load_name='data_7000_640.pt',
+    #     save_names=[
+    #         'data_7000_5000_640_64_stride4_n10_train.pt',
+    #         'data_7000_1000_640_64_stride4_n10_eval.pt',
+    #         'data_7000_1000_640_64_stride4_n10_test.pt'
+    #     ],
+    #     lengths=[5000, 1000, 1000], x_index=(4, 28),
+    #     seq_len=64, seq_amount=10, stride=4, seed=33
+    # )
