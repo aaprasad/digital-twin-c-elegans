e5be7e0b53c6d50f23f5bfbe4638444f3c0d17f1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-05 11:49:33 +0800

    forward: add tempory train file

--- scripts/muscle_ellipsoid2d/forward/train.temp.py
+++ scripts/muscle_ellipsoid2d/forward/train.temp.py
@@ -4,9 +4,9 @@ from virtual_nematode.trainers.ncp import train_eval_test
 
 def train():
     data_name = ['data_7000_5000_640_64_stride8_n10_train.pt', 'data_7000_1000_640_64_stride8_n10_eval.pt', 'data_7000_1000_640_64_stride8_n10_test.pt']
-    device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000
+    device_ids, batch_size, lr, weight_decay, epochs = 0, 128, 5e-2, 0, 1000
     kwargs = {
-        'data_name': data_name, 'model_name': 'li_conductance', 'batch_size': batch_size, 'seed': 42,
+        'data_name': data_name, 'model_name': 'li_conductance', 'batch_size': batch_size, 'seed': 0,
         'device_ids': device_ids, 'lr': lr, 'weight_decay': weight_decay, 'epochs': epochs, 'early_stop': epochs,
         'dt': 0.04, 'steps': 5, **get_kwargs(
             path='data/SI 5 Connectome adjacency matrices, corrected July 2020.xlsx',
