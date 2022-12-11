dfd9ea57fb7208b52e713e038a93a39002817db4
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-11 17:29:53 +0800

    forward: sample data from a given range

--- virtual_nematode/data/split.py
+++ virtual_nematode/data/split.py
@@ -44,3 +44,32 @@ class SlidingWindowDataset(torch.utils.data.TensorDataset):
         tensor = [tensor[:, i*self.stride:i*self.stride+self.seq_len, :] for i in range(self.seq_amount)]
         tensor = torch.cat(tensor, dim=0)
         return tensor
+
+
+class SlidingWindowSampleDataset(torch.utils.data.TensorDataset):
+    """ split sequence in dataset
+    x: torch.Tensor, (data_size, seq_len, 1)
+    y: torch.Tensor, (data_size, seq_len, action_size)
+    """
+    def __init__(self, dataset, seq_len, seq_range, seq_amount, stride, seed):
+        x, y = dataset.tensors
+        self.seq_len = seq_len
+        self.seq_amount = seq_amount
+        self.seq_range = seq_range
+        self.stride = stride
+        x = self.subsequence(x)
+        y = self.subsequence(y)
+        assert x.shape[0] == y.shape[0]
+        index = torch.randperm(x.shape[0], generator=torch.Generator().manual_seed(seed))
+        print('index full', index.shape)
+        index = index[0:len(dataset)*seq_amount]
+        print('index selected', index.shape)
+        x, y = x[index, :, :], y[index, :, :]
+        super(SlidingWindowSampleDataset, self).__init__(x, y)
+
+    def subsequence(self, tensor):
+        seq_range = min(self.seq_range, tensor.shape[1])
+        seq_index_range = int((seq_range - self.seq_len) / self.stride) + 1
+        tensor = [tensor[:, i*self.stride:i*self.stride+self.seq_len, :] for i in range(seq_index_range)]
+        tensor = torch.cat(tensor, dim=0)
+        return tensor
