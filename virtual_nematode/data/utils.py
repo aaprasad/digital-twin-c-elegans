91df1060c8c2fe66251acfad627e8bc0fe81ccb8
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-11 17:33:21 +0800

    forward: preprocess data, only split train data, don't split eval and test data

--- virtual_nematode/data/utils.py
+++ virtual_nematode/data/utils.py
@@ -3,7 +3,7 @@ import os
 import torch
 from virtual_nematode.data.clamp import ClampDataset
 from virtual_nematode.data.float import FloatDataset
-from virtual_nematode.data.split import SplitDataset, SlidingWindowDataset
+from virtual_nematode.data.split import SplitDataset, SlidingWindowDataset, SlidingWindowSampleDataset
 from virtual_nematode.data.subset import random_split, Subset
 
 
@@ -71,6 +71,41 @@ def preprocess_and_split_slide_dataset(load_name, save_names, lengths, x_index, 
     print('split sequence eval', eval_data.tensors[0].shape, eval_data.tensors[1].shape)
     test_data = SlidingWindowDataset(test_data, seq_len, seq_amount, stride)
     print('split sequence test', test_data.tensors[0].shape, test_data.tensors[1].shape)
+    # save
+    torch.save(train_data, os.path.join('data', save_names[0]))
+    torch.save(eval_data, os.path.join('data', save_names[1]))
+    torch.save(test_data, os.path.join('data', save_names[2]))
+
+
+def preprocess_and_split_slide_train_dataset(load_name, save_names, lengths, x_index, seq_len, seq_amount, stride, seed):
+    # load
+    dataset = torch.load(os.path.join('data', load_name))
+    print('load', dataset.tensors[0].shape, dataset.tensors[1].shape)
+    # subset
+    dataset = Subset(dataset, sum(lengths), x_index)
+    print('subset', dataset.tensors[0].shape, dataset.tensors[1].shape)
+    # float
+    dataset = FloatDataset(dataset)
+    print('dtype', dataset.tensors[0].dtype, dataset.tensors[1].dtype)
+    # clamp
+    dataset = ClampDataset(dataset, x_range=None, y_range=[0, 1])
+    print(
+        'clamp',
+        'x range', dataset.tensors[0].min().item(), dataset.tensors[0].max().item(),
+        'y range', dataset.tensors[1].min().item(), dataset.tensors[1].max().item()
+    )
+    # split train, eval, test
+    train_data, eval_data, test_data = random_split(dataset, lengths, seed)
+    print('train', train_data.tensors[0].shape, train_data.tensors[1].shape)
+    print('eval', eval_data.tensors[0].shape, eval_data.tensors[1].shape)
+    print('test', test_data.tensors[0].shape, test_data.tensors[1].shape)
+    # split sequence
+    train_data = SlidingWindowDataset(train_data, seq_len, seq_amount, stride)
+    print('split sequence train', train_data.tensors[0].shape, train_data.tensors[1].shape)
+    # eval_data = SlidingWindowDataset(eval_data, seq_len, seq_amount, stride)
+    # print('split sequence eval', eval_data.tensors[0].shape, eval_data.tensors[1].shape)
+    # test_data = SlidingWindowDataset(test_data, seq_len, seq_amount, stride)
+    # print('split sequence test', test_data.tensors[0].shape, test_data.tensors[1].shape)
     # save
     torch.save(train_data, os.path.join('data', save_names[0]))
     torch.save(eval_data, os.path.join('data', save_names[1]))
