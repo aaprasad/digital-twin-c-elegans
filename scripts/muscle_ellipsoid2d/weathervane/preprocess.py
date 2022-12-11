01157f9c4b76f0694db7870c51b1606469406f57
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-12 00:45:28 +0800

    weathervane: try different ways to preprocess data

--- scripts/muscle_ellipsoid2d/weathervane/preprocess.py
+++ scripts/muscle_ellipsoid2d/weathervane/preprocess.py
@@ -1,4 +1,6 @@
-from virtual_nematode.data.utils import preprocess_and_split_dataset, preprocess_and_split_slide_dataset
+from virtual_nematode.data.utils import (
+    preprocess_and_split_dataset, preprocess_and_split_slide_dataset, preprocess_and_split_slide_sample_dataset
+)
 
 
 if __name__ == '__main__':
@@ -17,23 +19,43 @@ if __name__ == '__main__':
     #     lengths=[5000, 1000, 1000], x_index=[(4, 28), (63, 64)],
     #     seq_len=64, seq_amount=10, stride=4, seed=33
     # )
-    # preprocess_and_split_slide_dataset(
+    preprocess_and_split_slide_dataset(
+        load_name='data_7000_640.pt',
+        save_names=[
+            'data_7000_5000_640_64_stride8_n10_train.pt',
+            'data_7000_1000_640_64_stride8_n10_eval.pt',
+            'data_7000_1000_640_64_stride8_n10_test.pt'
+        ],
+        lengths=[5000, 1000, 1000], x_index=[(4, 28), (63, 64)],  # proprioception, gradient
+        seq_len=64, seq_amount=10, stride=8, seed=33
+    )
+    # preprocess_and_split_slide_sample_dataset(
     #     load_name='data_7000_640.pt',
     #     save_names=[
-    #         'data_7000_5000_640_64_stride8_n10_train.pt',
-    #         'data_7000_1000_640_64_stride8_n10_eval.pt',
-    #         'data_7000_1000_640_64_stride8_n10_test.pt'
+    #         'data_7000_5000_640_64_stride8_50000_train.pt',
+    #         'data_7000_1000_640_64_stride8_10000_eval.pt',
+    #         'data_7000_1000_640_64_stride8_10000_test.pt'
     #     ],
     #     lengths=[5000, 1000, 1000], x_index=[(4, 28), (63, 64)],  # proprioception, gradient
-    #     seq_len=64, seq_amount=10, stride=8, seed=33
+    #     seq_len=64, seq_range=640, seq_amount=10, stride=8, seed=33
     # )
-    preprocess_and_split_dataset(
-        load_name='data_7000_640_delay100.pt',
-        save_names=[
-            'data_7000_5000_640_64_delay100_train.pt',
-            'data_7000_1000_640_64_delay100_eval.pt',
-            'data_7000_1000_640_64_delay100_test.pt'
-        ],
-        lengths=[5000, 1000, 1000], x_index=[(4, 28), (62, 63)],  # proprioception, concentration
-        seq_len=64, seed=33
-    )
+    # preprocess_and_split_slide_sample_dataset(
+    #     load_name='data_7000_640.pt',
+    #     save_names=[
+    #         'data_7000_5000_320_64_stride8_50000_train.pt',
+    #         'data_7000_1000_320_64_stride8_10000_eval.pt',
+    #         'data_7000_1000_320_64_stride8_10000_test.pt'
+    #     ],
+    #     lengths=[5000, 1000, 1000], x_index=[(4, 28), (63, 64)],  # proprioception, gradient
+    #     seq_len=64, seq_range=320, seq_amount=10, stride=8, seed=33
+    # )
+    # preprocess_and_split_dataset(
+    #     load_name='data_7000_640_delay100.pt',
+    #     save_names=[
+    #         'data_7000_5000_640_64_delay100_train.pt',
+    #         'data_7000_1000_640_64_delay100_eval.pt',
+    #         'data_7000_1000_640_64_delay100_test.pt'
+    #     ],
+    #     lengths=[5000, 1000, 1000], x_index=[(4, 28), (62, 63)],  # proprioception, concentration
+    #     seq_len=64, seed=33
+    # )
