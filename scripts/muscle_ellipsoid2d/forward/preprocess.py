c791d614288dc5fb0807ee9a8220b15f7395225c
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-12 00:48:29 +0800

    forward: try different ways to preprocess data

--- scripts/muscle_ellipsoid2d/forward/preprocess.py
+++ scripts/muscle_ellipsoid2d/forward/preprocess.py
@@ -1,4 +1,7 @@
-from virtual_nematode.data.utils import preprocess_and_split_dataset, preprocess_and_split_slide_dataset
+from virtual_nematode.data.utils import (
+    preprocess_and_split_dataset, preprocess_and_split_slide_dataset,
+    preprocess_and_split_slide_train_dataset
+)
 
 
 if __name__ == '__main__':
@@ -17,6 +20,16 @@ if __name__ == '__main__':
         lengths=[5000, 1000, 1000], x_index=(4, 28),
         seq_len=64, seq_amount=10, stride=8, seed=33
     )
+    # preprocess_and_split_slide_train_dataset(
+    #     load_name='data_7000_640.pt',
+    #     save_names=[
+    #         'data_7000_5000_640_64_stride8_n10_train.1.pt',
+    #         'data_7000_1000_640_eval.1.pt',
+    #         'data_7000_1000_640_test.1.pt'
+    #     ],
+    #     lengths=[5000, 1000, 1000], x_index=(4, 28),
+    #     seq_len=64, seq_amount=10, stride=8, seed=33
+    # )
     # preprocess_and_split_slide_dataset(
     #     load_name='data_7000_640.pt',
     #     save_names=[
