69fdb8ae506eb8e5838013eb7f4ac533ee6a9cab
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-12 00:43:44 +0800

    weathervane: train with linear encoding gradient input, on servers

--- scripts/muscle_ellipsoid2d/forward/train.py
+++ scripts/muscle_ellipsoid2d/forward/train.py
@@ -6,12 +6,14 @@ import worm_assets
 def train(model_name):
     if model_name.startswith('snn_forward') or model_name.startswith('li'):
         # data_name = ['data_7000_5000_640_64_train.pt', 'data_7000_1000_640_64_eval.pt', 'data_7000_1000_640_64_test.pt']
-        data_name = ['data_7000_5000_640_64_stride8_n10_train.pt', 'data_7000_1000_640_64_stride8_n10_eval.pt', 'data_7000_1000_640_64_stride8_n10_test.pt']
+        # data_name = ['data_7000_5000_640_64_stride8_n10_train.pt', 'data_7000_1000_640_64_stride8_n10_eval.pt', 'data_7000_1000_640_64_stride8_n10_test.pt']
         # data_name = ['data_7000_5000_640_64_stride4_n10_train.pt', 'data_7000_1000_640_64_stride4_n10_eval.pt', 'data_7000_1000_640_64_stride4_n10_test.pt']
-        device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000  # lr = 5e-2, 1e-2
+        data_name = ['data_7000_320_64_stride8_n10_train.pt', 'data_7000_320_64_stride8_n10_eval.pt', 'data_7000_320_64_stride8_n10_test.pt']
+        # device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000
+        device_ids, batch_size, lr, weight_decay, epochs = [0, 1], 128, 5e-2, 0, 1000
         kwargs = {
-            'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 42,  # 11, 42
-            'device_ids': device_ids, 'lr': lr, 'weight_decay': weight_decay, 'epochs': epochs, 'early_stop': epochs,
+            'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 42,
+            'device_ids': device_ids, 'lr': lr, 'weight_decay': weight_decay, 'epochs': epochs, 'early_stop': 30,
             # 'model_path': None, 'strict': True, 'optimizer_path': None,
             'dt': 0.04, 'steps': 5, **get_kwargs(
                 path=worm_assets.connectome_path(),
