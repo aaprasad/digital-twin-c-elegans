87a2aa44a16d9468c1a0affef0da768fb6d02492
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-14 14:54:30 +0800

    weathervane: online test model in a uniform concentration distribution env

---
+++ scripts/muscle_ellipsoid2d/weathervane/test_forward.py
@@ -0,0 +1,32 @@
+import os
+from sim import position_func
+import sys
+from test import test
+from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer_weathervane
+from virtual_nematode.envs.swimmer import fick_uniform
+
+
+if __name__ == '__main__':
+    runs_folder = sys.argv[1]
+    ckpt_name = sys.argv[2]  # 'model.pt'
+    model_folder = os.path.join('runs', runs_folder)
+    save_folder = os.path.join('data', runs_folder, ckpt_name)
+    video_folder = os.path.join('video', runs_folder, ckpt_name)
+    assert os.path.exists(model_folder)
+    os.makedirs(save_folder, exist_ok=True)
+    os.makedirs(video_folder, exist_ok=True)
+    print(model_folder, ckpt_name)
+    seed = 1024
+    max_episode_steps = 2500
+    data_path = os.path.join('data', runs_folder)
+    os.makedirs(data_path, exist_ok=True)
+    env = make_swimmer_weathervane(
+        n_bodies=25, joint_range=['-70 70'] + ['-100 100'] * 22 + ['-70 70'],
+        max_episode_steps=max_episode_steps, reset_noise_scale=0.6,
+        distance=15, source=(0, 0), position_func=position_func,  # distance = 3 * sigma
+        density=1.2, viscosity=0.1, condim=3, friction='1 1 0.005 0.0001 0.0001', cone='elliptic',
+        distribution_func=fick_uniform  # uniform distribution of concentration=0
+    )
+    """ testing """
+    model_name = 'li_conductance_gradient2'
+    test(model_folder, model_name, ckpt_name, save_folder)
