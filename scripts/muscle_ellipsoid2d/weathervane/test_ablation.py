fc4832da5cfdde8fef076f38af45205764eaa978
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-12-16 18:43:40 +0800

    weathervane: abltion experiments, testing with random start pos and fixed start pos

--- scripts/muscle_ellipsoid2d/weathervane/test_ablation.py
+++ scripts/muscle_ellipsoid2d/weathervane/test_ablation.py
@@ -8,7 +8,9 @@ import sys
 from test import select_model
 from test_vector_env import data_func, x_func, y_func
 import torch
-from virtual_nematode.envs.muscle_ellipsoid2d import make_swimmer_weathervane_xml, make_swimmer_weathervane_fn
+from virtual_nematode.envs.muscle_ellipsoid2d import (
+    make_swimmer_weathervane_xml, make_swimmer_weathervane_fn, make_swimmer_weathervane_fixed_fn
+)
 from virtual_nematode.testers.tester import test_vector_env_func
 
 
@@ -35,12 +37,12 @@ def modify_electrical_mask(model, index):
     return model
 
 
-def test(model_folder, model_name, ckpt_name, save_folder, ablation_type, device_id=None, id_list=None):
+def test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type, suffix='', device_id=None, id_list=None):
     print(ckpt_name)
     model = select_model(model_folder, model_name, ckpt_name)
     device = torch.device('cuda:{}'.format(device_id) if device_id is not None and torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    save_folder_temp = os.path.join(save_folder, ablation_type)
+    save_folder_temp = os.path.join(save_folder, ablation_type + suffix)
     os.makedirs(save_folder_temp, exist_ok=True)
     for i in id_list:
         file_temp = os.path.join(save_folder_temp, 'test100.{}.npz'.format(i))
@@ -61,6 +63,53 @@ def test(model_folder, model_name, ckpt_name, save_folder, ablation_type, device
         get_results_numpy(x, y, max_episode_steps=max_episode_steps, sigma=5)
 
 
+def main_random():
+    """ env """
+    env = gym.vector.AsyncVectorEnv(
+        env_fns=[
+            make_swimmer_weathervane_fn(
+                xml_str, reset_noise_scale=0.6, distance=15, max_episode_steps=max_episode_steps,
+                source=(0, 0), position_func=position_func
+            ) for _ in range(num_envs)
+        ]
+    )
+    print(env.action_space, env.observation_space)
+    """ testing """
+    test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=0, id_list=list(range(0, 160)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=1, id_list=list(range(160, 320)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=2, id_list=list(range(320, 469)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=3, id_list=list(range(0, 160)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=4, id_list=list(range(160, 320)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=5, id_list=list(range(320, 469)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', device_id=6, id_list=list(range(0, 235)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', device_id=7, id_list=list(range(235, 469)))
+
+
+def main_fixed():
+    """ env """
+    pos = (10, 0)
+    env = gym.vector.AsyncVectorEnv(
+        env_fns=[
+            make_swimmer_weathervane_fixed_fn(
+                xml_str, reset_noise_scale=0.6, pos=pos, max_episode_steps=max_episode_steps,
+                source=(0, 0), position_func=position_func
+            ) for _ in range(num_envs)
+        ]
+    )
+    print(env.action_space, env.observation_space)
+    """ testing """
+    distance = int(np.sqrt(pos[0] ** 2 + pos[1] ** 2))
+    suffix = '_d{}'.format(distance)
+    test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', suffix=suffix, device_id=0, id_list=list(range(0, 160)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', suffix=suffix, device_id=1, id_list=list(range(160, 320)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='all', suffix=suffix, device_id=2, id_list=list(range(320, 469)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', suffix=suffix, device_id=3, id_list=list(range(0, 160)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', suffix=suffix, device_id=4, id_list=list(range(160, 320)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', suffix=suffix, device_id=5, id_list=list(range(320, 469)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', suffix=suffix, device_id=6, id_list=list(range(0, 235)))
+    # test(env, model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', suffix=suffix, device_id=7, id_list=list(range(235, 469)))
+
+
 if __name__ == '__main__':
     runs_folder = sys.argv[1]
     ckpt_name = sys.argv[2]  # 'model.pt'
@@ -79,22 +128,7 @@ if __name__ == '__main__':
         n_bodies=25, joint_range=['-70 70'] + ['-100 100'] * 22 + ['-70 70'],
         source=(0, 0), density=1.2, viscosity=0.1, condim=3, friction='1 1 0.005 0.0001 0.0001', cone='elliptic'
     )
-    env = gym.vector.AsyncVectorEnv(
-        env_fns=[
-            make_swimmer_weathervane_fn(
-                xml_str, reset_noise_scale=0.6, distance=15, max_episode_steps=max_episode_steps,
-                source=(0, 0), position_func=position_func
-            ) for _ in range(num_envs)
-        ]
-    )
-    print(env.action_space, env.observation_space)
-    """ testing """
+    """ test """
     model_name = 'li_conductance_gradient2'
-    test(model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=0, id_list=list(range(0, 160)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=1, id_list=list(range(160, 320)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='all', device_id=2, id_list=list(range(320, 469)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=3, id_list=list(range(0, 160)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=4, id_list=list(range(160, 320)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='chemical', device_id=5, id_list=list(range(320, 469)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', device_id=6, id_list=list(range(0, 235)))
-    # test(model_folder, model_name, ckpt_name, save_folder, ablation_type='electrical', device_id=7, id_list=list(range(235, 469)))
+    main_random()
+    # main_fixed()
