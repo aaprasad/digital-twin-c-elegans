801e48248f778aab27989c939f20814b5f950a41
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-01-03 23:22:53 +0800

    weathervane: train model with suffix, suffix_params options

--- scripts/muscle_ellipsoid2d/weathervane/train.py
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -14,9 +14,12 @@ def train(model_name):
         # data_name = ['data_7000_640_512_stride8_n10_train.pt', 'data_7000_640_512_stride8_n10_eval.pt', 'data_7000_640_512_stride8_n10_test.pt']
         # device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 128, 5e-2, 0, 1000
         device_ids, batch_size, lr, weight_decay, epochs = [0, 1], 128, 5e-2, 0, 1000
+        suffix, suffix_params = None, None
+        # suffix, suffix_params = ['e_c'], {'lr': 5e-4}
         kwargs = {
             'data_name': data_name, 'model_name': model_name, 'batch_size': batch_size, 'seed': 42,
             'device_ids': device_ids, 'lr': lr, 'weight_decay': weight_decay, 'epochs': epochs, 'early_stop': epochs,
+            'suffix': suffix, 'suffix_params': suffix_params,
             # model kwargs
             # 'model_path': None, 'strict': True, 'optimizer_path': None,
             'dt': 0.04, 'steps': 5,
