73269c99dbf30edf1dda1383aab640845e12aef5
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-01-04 16:55:44 +0800

    forward: li current based model, with w_c init in U[-1,1]

--- virtual_nematode/networks/snn/lif.py
+++ virtual_nematode/networks/snn/lif.py
@@ -46,12 +46,12 @@ class LeakyIntegratorCurrentBased(torch.nn.Module):
         # self.bias = torch.nn.Parameter(bias)  # (n, )
         self.bias = torch.nn.Parameter(torch.zeros(n).uniform_(-1, 1))  # (n, )
         """ chemical synapse init with dedicated excitatory/inhibitory ratio """
-        w_c = torch.zeros((n, n)).uniform_(-1, 1)
-        polarity = torch.zeros((n, n)).bernoulli_(p=(w_c_mask.sum() * 0.8 - w_c_mask[1].sum()) / w_c_mask[0].sum())
-        polarity[polarity == 0.] = -1.
-        w_c = w_c.abs() * polarity
-        self.w_c = torch.nn.Parameter(w_c)  # (n, n)
-        # self.w_c = torch.nn.Parameter(torch.zeros((n, n)).uniform_(-1, 1))  # (n, n)
+        # w_c = torch.zeros((n, n)).uniform_(-1, 1)
+        # polarity = torch.zeros((n, n)).bernoulli_(p=(w_c_mask.sum() * 0.8 - w_c_mask[1].sum()) / w_c_mask[0].sum())
+        # polarity[polarity == 0.] = -1.
+        # w_c = w_c.abs() * polarity
+        # self.w_c = torch.nn.Parameter(w_c)  # (n, n)
+        self.w_c = torch.nn.Parameter(torch.zeros((n, n)).uniform_(-1, 1))  # (n, n)
         """"""
         self.w_c_mask = torch.nn.Parameter(w_c_mask, requires_grad=False)  # (3, n, n), bool
         w_c_n = w_c_mask.sum(dim=[0, 1])
