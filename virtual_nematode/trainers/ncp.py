47edb99a86256bfa0819b67b623d78424c885ee1
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-01-06 21:59:25 +0800

    weathervane: add li_conductance_mixed_gradient model

--- virtual_nematode/trainers/ncp.py
+++ virtual_nematode/trainers/ncp.py
@@ -20,7 +20,8 @@ from virtual_nematode.networks.snn.lif_chemotaxis import (
     LeakyIntegratorConductanceBasedGradientInput,
     LeakyIntegratorConductanceBasedGradientInput1,
     LeakyIntegratorConductanceBasedGradientInput2,
-    LeakyIntegratorConductanceBasedGradientInput3
+    LeakyIntegratorConductanceBasedGradientInput3,
+    LeakyIntegratorConductanceBasedMixedGradientInput
 )
 from virtual_nematode.networks.snn.weathervane import SNNCell as SNNCellW
 from virtual_nematode.networks.snn.weathervane import SNNCell1 as SNNCellW1
@@ -72,6 +73,8 @@ def prepare_model(model_name, device=None, device_ids=None, model_path=None, str
         model = li_conductance_gradient2(**kwargs)
     elif model_name == 'li_conductance_gradient3':
         model = li_conductance_gradient3(**kwargs)
+    elif model_name == 'li_conductance_mixed_gradient':
+        model = li_conductance_mixed_gradient(**kwargs)
     elif model_name == 'snn_weathervane':
         model = snn_weathervane(**kwargs)
     elif model_name == 'snn_weathervane1':
@@ -229,6 +232,12 @@ def li_conductance_gradient2(**kwargs):
 
 def li_conductance_gradient3(**kwargs):
     cell = LeakyIntegratorConductanceBasedGradientInput3(**kwargs)
+    model = SNN(cell)
+    return model
+
+
+def li_conductance_mixed_gradient(**kwargs):
+    cell = LeakyIntegratorConductanceBasedMixedGradientInput(**kwargs)
     model = SNN(cell)
     return model
 
