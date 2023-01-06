6a1033f901617dc054bfe32eba53eb7befccd3a8
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-01-06 21:52:24 +0800

    forward: add li conductance based model with mixed e_c

--- virtual_nematode/networks/snn/lif.py
+++ virtual_nematode/networks/snn/lif.py
@@ -282,6 +282,89 @@ class LeakyIntegratorConductanceBased(torch.nn.Module):
         return state, activation, action
 
 
+class LeakyIntegratorConductanceBasedMixed(torch.nn.Module):
+    def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index):
+        super(LeakyIntegratorConductanceBasedMixed, self).__init__()
+        self.dt = dt
+        self.steps = steps
+        self.n = n
+        self.m = m
+        self.p = p
+        self.tau = torch.nn.Parameter(torch.zeros(n).uniform_(0.01, 0.2))  # (n, )
+        self.bias = torch.nn.Parameter(torch.zeros(n).uniform_(-1, 1))  # (n, )
+        w_c = torch.zeros((2, n, n)).uniform_(0, 1)
+        w_c_mask_mixed = torch.stack((w_c_mask[0] | w_c_mask[1], w_c_mask[0] | w_c_mask[2]), dim=0)
+        w_c *= w_c_mask_mixed
+        w_c[0][w_c_mask[0]] /= 2
+        w_c[1][w_c_mask[0]] /= 2
+        self.w_c = torch.nn.Parameter(w_c)  # (2, n, n)
+        self.w_c_mask = torch.nn.Parameter(w_c_mask_mixed, requires_grad=False)  # (2, n, n), bool
+        w_c_n = w_c_mask.sum(dim=[0, 1])
+        w_c_n[w_c_n == 0] = 1
+        self.w_c_n = torch.nn.Parameter(w_c_n, requires_grad=False)  # (n, )
+        w_g = torch.zeros((n, n)).uniform_(0, 1)
+        w_g *= w_g_mask
+        self.w_g = torch.nn.Parameter(w_g)  # (n, n)
+        self.w_g_mask = torch.nn.Parameter(w_g_mask, requires_grad=False)  # (n, n), bool
+        w_g_n = w_g_mask.sum(dim=0)
+        w_g_n[w_g_n == 0] = 1
+        self.w_g_n = torch.nn.Parameter(w_g_n, requires_grad=False)  # (n, )
+        w_p = torch.zeros((p, n)).uniform_(-1, 1)
+        w_p *= w_p_mask.any(dim=0)
+        self.w_p = torch.nn.Parameter(w_p)  # (p, n)
+        self.w_p_mask = torch.nn.Parameter(w_p_mask.any(dim=0), requires_grad=False)  # (p, n), bool
+        w_p_n = w_p_mask.sum(dim=[0, 1])
+        w_p_n[w_p_n == 0] = 1
+        self.w_p_n = torch.nn.Parameter(w_p_n, requires_grad=False)  # (n, )
+        self.output_index = torch.nn.Parameter(output_index, requires_grad=False)  # (n, ), bool
+        self.state_func = torch.nn.Hardtanh(-1, 1)
+        self.activation_func = torch.nn.Sigmoid()
+        self.activation_scaling = torch.sigmoid(torch.tensor([-1, 1])).tolist()
+
+    @property
+    def init_state(self):
+        """ initial state and activation """
+        bias = self.bias.clone().detach()
+        state = self.state_func(bias)
+        activation = self.activation_func(state)
+        activation = (activation - self.activation_scaling[0]) / (self.activation_scaling[1] - self.activation_scaling[0])
+        return state, activation  # (n, ), (n, )
+
+    def _external_input(self, stimuli):
+        """ proprioception input """
+        w_p = self.w_p * self.w_p_mask
+        external_input = torch.mm(stimuli, w_p) / self.w_p_n
+        return external_input
+
+    def forward(self, state, activation, stimuli):
+        # chemical synapse weight
+        w_c = self.w_c.abs() * self.w_c_mask  # (2, n, n)
+        # gap junction weight
+        w_g = self.w_g.abs()
+        w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * self.w_g_mask
+        # external input + bias
+        external_input = self._external_input(stimuli) + self.bias
+        # dt / tau
+        dt_tau = self.dt / self.steps / self.tau.clamp(0.01, 0.2)
+        for i in range(self.steps):
+            # chemical synapse input
+            synapse_input = (torch.mm(activation, w_c[0] - w_c[1]) - torch.mm(activation, w_c[0] + w_c[1]) * state) / self.w_c_n
+            # gap junction input
+            delta_state = state.unsqueeze(dim=2).repeat(1, 1, self.n) - state.unsqueeze(dim=1).repeat(1, self.n, 1)
+            gap_input = torch.sum(delta_state * w_g, dim=1) / self.w_g_n
+            # total input
+            total_input = synapse_input + gap_input + external_input
+            # cell state and activation
+            state = (1 - dt_tau) * state + dt_tau * total_input
+            state = self.state_func(state)
+            activation = self.activation_func(state)
+            activation = (activation - self.activation_scaling[0]) / (self.activation_scaling[1] - self.activation_scaling[0])
+        # muscle output
+        action = activation[:, self.output_index]
+        # action = action * self.alpha_m.clamp(0, 1) + self.beta_m.clamp(0, 1)
+        return state, activation, action
+
+
 class LeakyIntegratorConductanceBasedPolarity(torch.nn.Module):
     def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index):
         super(LeakyIntegratorConductanceBasedPolarity, self).__init__()
