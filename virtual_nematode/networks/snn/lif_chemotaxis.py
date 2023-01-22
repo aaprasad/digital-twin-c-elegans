weathervane: li conductance based model restrained, fix bias=-1, add state_func to clamp state and activation scaling

virtual_nematode/networks/snn/lif_chemotaxis.py
@@ -4,7 +4,8 @@ from virtual_nematode.networks.snn.lif import (
     LeakyIntegratorConductanceBased, LeakyIntegratorConductanceBasedPolarity,
     LeakyIntegratorConductanceBased1,
     LeakyIntegratorConductanceBasedMixed, LeakyIntegratorConductanceBasedMixed1, LeakyIntegratorConductanceBasedMixed2,
-    LeakyIntegratorConductanceBasedUnrestrained, LeakyIntegratorConductanceBasedUnrestrained1, LeakyIntegratorConductanceBasedUnrestrained2
+    LeakyIntegratorConductanceBasedUnrestrained, LeakyIntegratorConductanceBasedUnrestrained1, LeakyIntegratorConductanceBasedUnrestrained2,
+    LeakyIntegratorConductanceBasedRestrained, LeakyIntegratorConductanceBasedRestrained1, LeakyIntegratorConductanceBasedRestrained2
 )
 
 
@@ -362,6 +363,93 @@ class LeakyIntegratorConductanceBasedUnrestrainedGradientInput2(LeakyIntegratorC
         """
         # proprioception input
         external_input = super(LeakyIntegratorConductanceBasedUnrestrainedGradientInput2, self)._external_input(stimuli[:, 0:self.p])
+        # sensory input
+        gradient = stimuli[:, self.p:self.p+1]  # (batch_size, 1)
+        up_step_index = gradient > 0
+        down_step_index = gradient <= 0
+        w_s = self.w_s.abs()
+        asel_input = torch.zeros_like(gradient)
+        asel_input[up_step_index] = w_s[0] * gradient[up_step_index]
+        aser_input = torch.zeros_like(gradient)
+        aser_input[up_step_index] = -w_s[1] * gradient[up_step_index]
+        aser_input[down_step_index] = -w_s[2] * gradient[down_step_index]
+        sensory_input = torch.cat((asel_input, aser_input), dim=1)  # ASEL/ASER, (batch_size, 2)
+        external_input[:, self.w_s_mask] += sensory_input
+        return external_input
+
+
+class LeakyIntegratorConductanceBasedRestrainedGradientInput(LeakyIntegratorConductanceBasedRestrained):
+    def __init__(self, s, w_s_mask, **kwargs):
+        super(LeakyIntegratorConductanceBasedRestrainedGradientInput, self).__init__(**kwargs)
+        self.s = s  # sensory size
+        self.w_s = torch.nn.Parameter(torch.ones(s))  # (3, )
+        self.w_s_mask = torch.nn.Parameter(w_s_mask, requires_grad=False)  # (2, ), long
+
+    def _external_input(self, stimuli):
+        """ receive proprioception input and sensory input
+        https://doi.org/10.1038/nature06927
+        https://doi.org/10.1038/s41598-018-35157-1
+        """
+        # proprioception input
+        external_input = super(LeakyIntegratorConductanceBasedRestrainedGradientInput, self)._external_input(stimuli[:, 0:self.p])
+        # sensory input
+        gradient = stimuli[:, self.p:self.p+1]  # (batch_size, 1)
+        up_step_index = gradient > 0
+        down_step_index = gradient <= 0
+        w_s = self.w_s.abs()
+        asel_input = torch.zeros_like(gradient)
+        asel_input[up_step_index] = w_s[0] * gradient[up_step_index]
+        aser_input = torch.zeros_like(gradient)
+        aser_input[up_step_index] = -w_s[1] * gradient[up_step_index]
+        aser_input[down_step_index] = -w_s[2] * gradient[down_step_index]
+        sensory_input = torch.cat((asel_input, aser_input), dim=1)  # ASEL/ASER, (batch_size, 2)
+        external_input[:, self.w_s_mask] += sensory_input
+        return external_input
+
+
+class LeakyIntegratorConductanceBasedRestrainedGradientInput1(LeakyIntegratorConductanceBasedRestrained1):
+    def __init__(self, s, w_s_mask, **kwargs):
+        super(LeakyIntegratorConductanceBasedRestrainedGradientInput1, self).__init__(**kwargs)
+        self.s = s  # sensory size
+        self.w_s = torch.nn.Parameter(torch.ones(s))  # (3, )
+        self.w_s_mask = torch.nn.Parameter(w_s_mask, requires_grad=False)  # (2, ), long
+
+    def _external_input(self, stimuli):
+        """ receive proprioception input and sensory input
+        https://doi.org/10.1038/nature06927
+        https://doi.org/10.1038/s41598-018-35157-1
+        """
+        # proprioception input
+        external_input = super(LeakyIntegratorConductanceBasedRestrainedGradientInput1, self)._external_input(stimuli[:, 0:self.p])
+        # sensory input
+        gradient = stimuli[:, self.p:self.p+1]  # (batch_size, 1)
+        up_step_index = gradient > 0
+        down_step_index = gradient <= 0
+        w_s = self.w_s.abs()
+        asel_input = torch.zeros_like(gradient)
+        asel_input[up_step_index] = w_s[0] * gradient[up_step_index]
+        aser_input = torch.zeros_like(gradient)
+        aser_input[up_step_index] = -w_s[1] * gradient[up_step_index]
+        aser_input[down_step_index] = -w_s[2] * gradient[down_step_index]
+        sensory_input = torch.cat((asel_input, aser_input), dim=1)  # ASEL/ASER, (batch_size, 2)
+        external_input[:, self.w_s_mask] += sensory_input
+        return external_input
+
+
+class LeakyIntegratorConductanceBasedRestrainedGradientInput2(LeakyIntegratorConductanceBasedRestrained2):
+    def __init__(self, s, w_s_mask, **kwargs):
+        super(LeakyIntegratorConductanceBasedRestrainedGradientInput2, self).__init__(**kwargs)
+        self.s = s  # sensory size
+        self.w_s = torch.nn.Parameter(torch.ones(s))  # (3, )
+        self.w_s_mask = torch.nn.Parameter(w_s_mask, requires_grad=False)  # (2, ), long
+
+    def _external_input(self, stimuli):
+        """ receive proprioception input and sensory input
+        https://doi.org/10.1038/nature06927
+        https://doi.org/10.1038/s41598-018-35157-1
+        """
+        # proprioception input
+        external_input = super(LeakyIntegratorConductanceBasedRestrainedGradientInput2, self)._external_input(stimuli[:, 0:self.p])
         # sensory input
         gradient = stimuli[:, self.p:self.p+1]  # (batch_size, 1)
         up_step_index = gradient > 0