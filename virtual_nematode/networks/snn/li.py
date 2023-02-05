50b39071e4b079bd38df276a13a4fdbc68464990
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-02-06 01:06:01 +0800

    weathervane: li model, copy li2

--- virtual_nematode/networks/snn/li.py
+++ virtual_nematode/networks/snn/li.py
@@ -102,9 +102,12 @@ class LI0(torch.nn.Module):
 
 
 class LI1(torch.nn.Module):
-    def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index, v_range, scaling):
-        """ partially fixed reversal potential
-        v_range: (v_min, v_max)
+    def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index, init_type, beta, v_range, e_range):
+        """ trainable reversal potential
+        init_type: 'random' or 'polarity'
+        beta: the width of the sigmoid activation function
+        v_range: range of membrane potential (v_min, v_max)
+        e_range: range of reversal potential (e_min, e_max)
         """
         super(LI1, self).__init__()
         self.dt = dt  # 0.04
@@ -113,16 +116,23 @@ class LI1(torch.nn.Module):
         self.m = m
         self.p = p
         self.tau = torch.nn.Parameter(torch.zeros(n).uniform_(0.01, 0.2))  # (n, )
-        self.bias = torch.nn.Parameter(torch.zeros(n).normal_(0, 1))  # (n, )
+        bias = torch.zeros(n)
+        torch.nn.init.trunc_normal_(bias, mean=0, std=1, a=e_range[0], b=e_range[1])
+        self.bias = torch.nn.Parameter(bias)  # (n, )
         w_c = torch.zeros((n, n)).normal_(0, 1)
         w_c = w_c.abs() * w_c_mask.any(dim=0)
         self.w_c = torch.nn.Parameter(w_c)  # (n, n)
         self.w_c_mask = torch.nn.Parameter(w_c_mask.any(dim=0), requires_grad=False)  # (n, n), bool
         e_c = torch.zeros((n, n))
-        torch.nn.init.trunc_normal_(e_c, mean=0, std=1, a=v_range[0], b=v_range[1])
-        e_c = e_c * w_c_mask[0] + v_range[1] * w_c_mask[1] + v_range[0] * w_c_mask[2]
+        torch.nn.init.trunc_normal_(e_c, mean=0, std=1, a=e_range[0], b=e_range[1])
+        if init_type == 'random':
+            e_c *= w_c_mask.any(dim=0)
+        elif init_type == 'polarity':
+            error = torch.zeros((n, n)).normal_(0, 1)
+            e_c = e_c * w_c_mask[0] + (e_range[1] - error.abs()) * w_c_mask[1] + (e_range[0] + error.abs()) * w_c_mask[2]
+        else:
+            raise AssertionError
         self.e_c = torch.nn.Parameter(e_c)  # (n, n)
-        self.e_c_mask = torch.nn.Parameter(w_c_mask, requires_grad=False)  # (n, n), bool
         w_c_n = w_c_mask.sum(dim=[0, 1])
         w_c_n[w_c_n == 0] = 1
         self.w_c_n = torch.nn.Parameter(w_c_n, requires_grad=False)  # (n, )
@@ -144,18 +154,15 @@ class LI1(torch.nn.Module):
         self.output_index = torch.nn.Parameter(output_index, requires_grad=False)  # (n, ), bool
         self.state_func = torch.nn.Hardtanh(v_range[0], v_range[1])
         self.activation_func = torch.nn.Sigmoid()
-        self.v_range = v_range
-        self.scaling = scaling
-        self.activation_scaling = torch.sigmoid(torch.tensor(v_range)).tolist()
+        self.beta = beta
+        self.reversal_potential_func = torch.nn.Hardtanh(e_range[0], e_range[1])
 
     @property
     def init_state(self):
         """ initial state and activation """
         bias = self.bias.clone().detach()
         state = self.state_func(bias)
-        activation = self.activation_func(state)
-        if self.scaling is True:
-            activation = (activation - self.activation_scaling[0]) / (self.activation_scaling[1] - self.activation_scaling[0])
+        activation = self.activation_func(self.beta * state)
         return state, activation  # (n, ), (n, )
 
     def _external_input(self, stimuli):
@@ -167,7 +174,7 @@ class LI1(torch.nn.Module):
     def forward(self, state, activation, stimuli):
         # chemical synapse weight
         w_c = self.w_c.abs() * self.w_c_mask
-        e_c = self.e_c.clamp(self.v_range[0], self.v_range[1]) * self.e_c_mask[0] + self.v_range[1] * self.e_c_mask[1] + self.v_range[0] * self.e_c_mask[2]
+        e_c = self.reversal_potential_func(self.e_c)
         # gap junction weight
         w_g = self.w_g.abs()
         w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * self.w_g_mask
@@ -186,9 +193,7 @@ class LI1(torch.nn.Module):
             # cell state and activation
             state = (1 - dt_tau) * state + dt_tau * total_input
             state = self.state_func(state)
-            activation = self.activation_func(state)
-            if self.scaling is True:
-                activation = (activation - self.activation_scaling[0]) / (self.activation_scaling[1] - self.activation_scaling[0])
+            activation = self.activation_func(self.beta * state)
         # muscle output
         action = activation[:, self.output_index]
         return state, activation, action
