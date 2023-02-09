27d0314955e02f8bf4aea79bd8134308f483164a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-02-09 15:25:04 +0800

    weathervane: muscle arrangement bug fix

--- virtual_nematode/networks/snn/output.py
+++ virtual_nematode/networks/snn/output.py
@@ -16,25 +16,24 @@ def muscle_band_arrangement(band_length, kernel_size):
 class MuscleArrangement(torch.nn.Module):
     def __init__(self, band_lengths, kernel_size):
         super(MuscleArrangement, self).__init__()
-        weight, bias, mask = [], [], []
-        for band_length in band_lengths:
+        self.band_lengths = band_lengths
+        self.band_range = torch.cumsum(torch.tensor([0] + band_lengths), dim=0)
+        for i, band_length in enumerate(band_lengths):
             w, b, m = muscle_band_arrangement(band_length, kernel_size)
             w = torch.nn.Parameter(w)
             b = torch.nn.Parameter(b)
             m = torch.nn.Parameter(m, requires_grad=False)
-            weight.append(w)
-            bias.append(b)
-            mask.append(m)
-        self.band_lengths = band_lengths
-        self.band_range = torch.cumsum(torch.tensor([0] + band_lengths), dim=0)
-        self.weight = weight
-        self.bias = bias
-        self.mask = mask
+            self.register_parameter('weight{}'.format(i), w)
+            self.register_parameter('bias{}'.format(i), b)
+            self.register_parameter('mask{}'.format(i), m)
 
     def forward(self, action):  # (batch_size, m)
         action_split = [action[:, self.band_range[i]:self.band_range[i+1]] for i in range(len(self.band_lengths))]
         action_split = [
-            torch.mm(a, w * m) + b for a, w, b, m in zip(action_split, self.weight, self.bias, self.mask)
+            torch.mm(
+                a, self._parameters['weight{}'.format(i)] * self._parameters['mask{}'.format(i)]
+            ) + self._parameters['bias{}'.format(i)]
+            for i, a in enumerate(action_split)
         ]
         action = torch.cat(action_split, dim=1)
         action = action.clamp(0, 1)
