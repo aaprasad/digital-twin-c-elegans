c647f72abb8fc70302d6dfb251b1258ce905412f
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-02-09 16:59:13 +0800

    weathervane: add muscle arrangement 2

--- virtual_nematode/networks/snn/output.py
+++ virtual_nematode/networks/snn/output.py
@@ -24,13 +24,24 @@ def muscle_band_arrangement1(band_length, kernel_size):
     return weight, bias, mask
 
 
+def muscle_band_arrangement2(band_length, kernel_size):
+    mask = torch.zeros((band_length, band_length), dtype=torch.bool)
+    kernel_width = int(kernel_size / 2)
+    for i in range(band_length):
+        mask[max(i-kernel_width, 0):min(i+kernel_width+1, band_length), i] = True
+    init_range = 1 / kernel_size
+    weight = torch.zeros((band_length, band_length)).uniform_(0, init_range)
+    bias = torch.zeros(band_length).uniform_(0, init_range)
+    return weight, bias, mask
+
+
 class MuscleArrangement(torch.nn.Module):
     def __init__(self, band_lengths, kernel_size):
         super(MuscleArrangement, self).__init__()
         self.band_lengths = band_lengths
         self.band_range = torch.cumsum(torch.tensor([0] + band_lengths), dim=0)
         # band 0
-        w0, b0, m0 = muscle_band_arrangement1(band_lengths[0], kernel_size)
+        w0, b0, m0 = muscle_band_arrangement2(band_lengths[0], kernel_size)
         self.w0 = torch.nn.Parameter(w0)
         self.b0 = torch.nn.Parameter(b0)
         self.m0 = torch.nn.Parameter(m0, requires_grad=False)
