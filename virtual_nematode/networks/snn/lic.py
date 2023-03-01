b0bad5009cf5b707e0066f7cb94ec4eb150ab30a
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-03-01 21:26:50 +0800

    weathervane: add lic21

--- virtual_nematode/networks/snn/lic.py
+++ virtual_nematode/networks/snn/lic.py
@@ -769,3 +769,116 @@ class LIC11(torch.nn.Module):
         action = activation[:, self.output_index]
         action = self.muscle_arrangement(action)
         return state, activation, action
+
+
+class Activation(torch.nn.Module):
+    def __init__(self, k=12.5, b=3.):
+        super(Activation, self).__init__()
+        self.k = k
+        self.b = b
+
+    def forward(self, input):
+        return torch.sigmoid(self.k * input + self.b)
+
+
+class LIC21(torch.nn.Module):
+    def __init__(self, dt, steps, n, m, p, w_c_mask, w_g_mask, w_p_mask, output_index, s, w_s_mask, scale_init=0.25, scale_max=1.):
+        super(LIC21, self).__init__()
+        self.dt = dt
+        self.steps = steps
+        self.n = n
+        self.m = m
+        self.p = p
+        self.tau = torch.nn.Parameter(torch.zeros(n).uniform_(0.01, 0.2))  # (n, )
+        self.bias = -0.35 + torch.nn.Parameter(torch.zeros(n).normal_(0, 0.01))  # (n, )
+        w_c = torch.zeros((n, n)).uniform_(0, 1) * scale_init
+        w_c *= w_c_mask.any(dim=0)
+        self.w_c = torch.nn.Parameter(w_c)  # (n, n)
+        e_c = torch.zeros((n, n)).normal_(mean=0, std=0.01)
+        e_c = (-0.24 + e_c) * w_c_mask[0] + (0. + e_c) * w_c_mask[1] + (-0.45 + e_c) * w_c_mask[2]
+        self.e_c = torch.nn.Parameter(e_c)  # (n, n)
+        self.w_c_mask = torch.nn.Parameter(w_c_mask.any(dim=0), requires_grad=False)  # (n, n), bool
+        w_c_n = w_c_mask.sum(dim=[0, 1])
+        w_c_n[w_c_n == 0] = 1
+        self.w_c_n = torch.nn.Parameter(w_c_n, requires_grad=False)  # (n, )
+        w_g = torch.zeros((n, n)).uniform_(0, 1) * scale_init
+        w_g *= w_g_mask
+        self.w_g = torch.nn.Parameter(w_g)  # (n, n)
+        self.w_g_mask = torch.nn.Parameter(w_g_mask, requires_grad=False)  # (n, n), bool
+        w_g_n = w_g_mask.sum(dim=0)
+        w_g_n[w_g_n == 0] = 1
+        self.w_g_n = torch.nn.Parameter(w_g_n, requires_grad=False)  # (n, )
+        w_p = torch.zeros((p, n)).uniform_(-1, 1) * scale_init
+        w_p *= w_p_mask.any(dim=0)
+        self.w_p = torch.nn.Parameter(w_p)  # (p, n)
+        self.w_p_mask = torch.nn.Parameter(w_p_mask.any(dim=0), requires_grad=False)  # (p, n), bool
+        w_p_n = w_p_mask.sum(dim=[0, 1])
+        w_p_n[w_p_n == 0] = 1
+        self.w_p_n = torch.nn.Parameter(w_p_n, requires_grad=False)  # (n, )
+        self.output_index = torch.nn.Parameter(output_index, requires_grad=False)  # (n, ), bool
+        self.activation_func = Activation(k=37.5, b=9.)
+        self.s = s  # sensory size
+        self.w_s = torch.nn.Parameter(torch.ones(s) * scale_init)  # (3, )
+        self.w_s_mask = torch.nn.Parameter(w_s_mask, requires_grad=False)  # (2, ), long
+        self.scale_max = scale_max
+
+    @property
+    def init_state(self):
+        """ initial state and activation """
+        bias = self.bias.clone().detach()
+        state = bias
+        activation = self.activation_func(state)
+        return state, activation  # (n, ), (n, )
+
+    def _external_input_proprioception(self, stimuli):
+        """ proprioception input """
+        w_p = self.w_p * self.w_p_mask
+        external_input = torch.mm(stimuli, w_p) / self.w_p_n
+        return external_input
+
+    def _external_input(self, stimuli):
+        """ receive proprioception input and sensory input
+        https://doi.org/10.1038/nature06927
+        https://doi.org/10.1038/s41598-018-35157-1
+        """
+        # proprioception input
+        external_input = self._external_input_proprioception(stimuli[:, 0:self.p])
+        # sensory input
+        gradient = stimuli[:, self.p:self.p + 1]  # (batch_size, 1)
+        up_step_index = gradient > 0
+        down_step_index = gradient <= 0
+        w_s = self.w_s.abs()
+        asel_input = torch.zeros_like(gradient)
+        asel_input[up_step_index] = w_s[0] * gradient[up_step_index]
+        aser_input = torch.zeros_like(gradient)
+        aser_input[up_step_index] = -w_s[1] * gradient[up_step_index]
+        aser_input[down_step_index] = -w_s[2] * gradient[down_step_index]
+        sensory_input = torch.cat((asel_input, aser_input), dim=1)  # ASEL/ASER, (batch_size, 2)
+        external_input[:, self.w_s_mask] += sensory_input
+        return external_input
+
+    def forward(self, state, activation, stimuli):
+        # chemical synapse weight
+        w_c = self.w_c.clamp(0, self.scale_max) * self.w_c_mask
+        e_c = self.e_c.clamp(-0.5, 0.05)
+        # gap junction weight
+        w_g = self.w_g.clamp(0, self.scale_max)
+        w_g = (w_g.tril() + w_g.tril(diagonal=-1).T) * self.w_g_mask
+        # external input + bias
+        external_input = self._external_input(stimuli) + self.bias
+        # dt / tau
+        dt_tau = self.dt / self.steps / self.tau.clamp(0.01, 0.2)
+        for i in range(self.steps):
+            # chemical synapse input
+            synapse_input = (torch.mm(activation, w_c * e_c) - torch.mm(activation, w_c) * state) / self.w_c_n
+            # gap junction input
+            delta_state = state.unsqueeze(dim=2).repeat(1, 1, self.n) - state.unsqueeze(dim=1).repeat(1, self.n, 1)
+            gap_input = torch.sum(delta_state * w_g, dim=1) / self.w_g_n
+            # total input
+            total_input = synapse_input + gap_input + external_input
+            # cell state and activation
+            state = (1 - dt_tau) * state + dt_tau * total_input
+            activation = self.activation_func(state)
+        # muscle output
+        action = activation[:, self.output_index]
+        return state, activation, action
