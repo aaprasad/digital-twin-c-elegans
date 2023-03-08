3c02e98155b5c36f82835dd47b670156e50bfe73
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-03-08 18:56:07 +0800

    weathervane: train lic31, steps=10, seed=48

--- scripts/muscle_ellipsoid2d/weathervane/train.py
+++ scripts/muscle_ellipsoid2d/weathervane/train.py
@@ -18,6 +18,7 @@ def train(model_name):
         # device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 288, 0.05, 0, 1000  # steps=5
         # device_ids, batch_size, lr, weight_decay, epochs = [4, 5, 6, 7], 288, 0.05, 0, 1000  # steps=5
         device_ids, batch_size, lr, weight_decay, epochs = [0, 1, 2, 3], 160, 0.05, 0, 1000  # steps=10
+        # device_ids, batch_size, lr, weight_decay, epochs = [4, 5, 6, 7], 160, 0.05, 0, 1000  # steps=10
         suffix, suffix_params = None, None
         # suffix, suffix_params = ['e_c'], {'lr': 5e-4}
         kwargs = {
