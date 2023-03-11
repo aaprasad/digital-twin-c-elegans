6c1d589ea1dbc6546a2bc1cba624921119d7d948
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2023-03-11 20:01:26 +0800

    weathervane: add lic40, state use sigmoidclamp(-0.8,0.35)

--- virtual_nematode/networks/snn/lic.py
+++ virtual_nematode/networks/snn/lic.py
@@ -1483,7 +1483,7 @@ class LIC40(torch.nn.Module):
         w_p_n[w_p_n == 0] = 1
         self.w_p_n = torch.nn.Parameter(w_p_n, requires_grad=False)  # (n, )
         self.output_index = torch.nn.Parameter(output_index, requires_grad=False)  # (n, ), bool
-        self.input_func = SigmoidClamp(a=-0.8, b=0.35)
+        self.state_func = SigmoidClamp(a=-0.8, b=0.35)
         self.activation_func = Activation(k=37.5, b=9.)
         self.s = s  # sensory size
         self.w_s = torch.nn.Parameter(torch.ones(s))  # (3, )
@@ -1545,6 +1545,7 @@ class LIC40(torch.nn.Module):
             total_input = self.input_func(total_input)
             # cell state and activation
             state = (1 - dt_tau) * state + dt_tau * total_input
+            state = self.state_func(state)
             activation = self.activation_func(state)
         # muscle output
         action = activation[:, self.output_index]
